2835c0c0352906ee0389cf1069dfb2029e1eee5f
Thomas McNiven <hello@vevix.net>
2016-10-04 22:19:25 +1000

    Turn off logging as default

--- config/environment.js
+++ config/environment.js
@@ -51,10 +51,10 @@ module.exports = function(environment) {
 
   if (environment === 'development') {
     ENV.APP.LOG_RESOLVER = false;
-    ENV.APP.LOG_ACTIVE_GENERATION = true;
-    ENV.APP.LOG_TRANSITIONS = true;
-    ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV.APP.LOG_ACTIVE_GENERATION = false;
+    ENV.APP.LOG_TRANSITIONS = false;
+    ENV.APP.LOG_TRANSITIONS_INTERNAL = false;
+    ENV.APP.LOG_VIEW_LOOKUPS = false;
   }
 
   if (environment === 'test') {
