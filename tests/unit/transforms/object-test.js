3057931b6ab7dfa5c808c0344f460161063238db
Thomas McNiven <hello@vevix.net>
2016-10-12 06:08:31 +1000

    Test the changes in object transform

--- tests/unit/transforms/object-test.js
+++ tests/unit/transforms/object-test.js
@@ -6,19 +6,16 @@ moduleFor('transform:object', 'Unit | Transform | object', {
 });
 
 test('#serialize', function(assert) {
-  assert.expect(3);
   const transform = this.subject();
-
   assert.deepEqual(transform.serialize(null), {});
   assert.deepEqual(transform.serialize(undefined), {});
   assert.deepEqual(transform.serialize({ foo: 'bar' }), { foo: 'bar' });
 });
 
 test('#deserialize', function(assert) {
-  assert.expect(3);
   const transform = this.subject();
-
   assert.deepEqual(transform.deserialize(null), {});
   assert.deepEqual(transform.deserialize(undefined), {});
   assert.deepEqual(transform.deserialize({ foo: 'bar' }), { foo: 'bar' });
+  assert.equal(transform.deserialize(null, { defaultValue: 'hello' }), 'hello');
 });
