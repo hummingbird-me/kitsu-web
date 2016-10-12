75eb1b2cd03310f6a9d09ad7d18c6b1d3656363a
Thomas McNiven <hello@vevix.net>
2016-10-13 06:44:42 +1000

    Fix opening of edit modal

--- app/routes/users/controller.js
+++ app/routes/users/controller.js
@@ -2,10 +2,12 @@ import Controller from 'ember-controller';
 import computed, { alias } from 'ember-computed';
 import service from 'ember-service/inject';
 import get from 'ember-metal/get';
+import set from 'ember-metal/set';
 import IsOwnerMixin from 'client/mixins/is-owner';
 import { image } from 'client/helpers/image';
 
 export default Controller.extend(IsOwnerMixin, {
+  isEditing: false,
   user: alias('model'),
   session: service(),
 
@@ -14,5 +16,11 @@ export default Controller.extend(IsOwnerMixin, {
       const coverImage = image([get(this, 'user.coverImage')]);
       return `background-image: url("${coverImage}")`.htmlSafe();
     }
-  })
+  }),
+
+  actions: {
+    toggleEditing(value) {
+      set(this, 'isEditing', value);
+    }
+  }
 });
