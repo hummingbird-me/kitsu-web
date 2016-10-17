9b35163c76555817091f627f696165259edfe850
Sven Gehring <cbrxde@gmail.com>
2016-10-17 14:07:44 +0200

    Add tests for component; Remove example data

--- app/components/compatibility-indicator/component.js
+++ app/components/compatibility-indicator/component.js
@@ -1,5 +1,4 @@
 import Ember from 'ember';
-import libraryStatus from 'client/utils/library-status';
 import { isEmberArray as isArray } from 'ember-array/utils';
 
 export default Ember.Component.extend({
@@ -11,26 +10,8 @@ export default Ember.Component.extend({
 
     if (this.get('similarity') !== undefined) return;
 
-    // DEVNOTE: It is unclear, how data will get into this component
-    // The following calculations assumes library-entry objects with
-    // the keys id, status and rating.
-          this.set('source', [
-            {id: 1, status: 'watching', rating: 3},
-            {id: 2, status: 'watching', rating: 2},
-            {id: 3, status: 'watching', rating: 5},
-            {id: 4, status: 'planned', rating: 8},
-            {id: 5, status: 'dropped', rating: 8}
-          ]);
-          this.set('target', [
-            {id: 1, status: 'watching', rating: 3},
-            {id: 2, status: 'watching', rating: 10},
-            {id: 3, status: 'watching', rating: 5},
-            {id: 4, status: 'planned', rating: 8},
-            {id: 5, status: 'dropped', rating: 8}
-          ]);
-
     if (!isArray(this.get('source')) || !isArray(this.get('target'))) {
-      this.set('similarity', 0);
+      this.set('similarity', '?');
       return;
     }
 
--- tests/integration/components/compatibility-indicator/component-test.js
+++ tests/integration/components/compatibility-indicator/component-test.js
@@ -5,6 +5,28 @@ moduleForComponent('compatibility-indicator', 'Integration | Component | compati
   integration: true
 });
 
+const dataset1 = [
+  { id: 237, rating: 5.0 },
+  { id: 456, rating: 3.5 },
+  { id: 329, rating: 1.0 },
+  { id: 123, rating: 4.0 },
+  { id: 384, rating: 4.0 },
+  { id: 982, rating: 2.5 },
+  { id: 234, rating: 4.5 },
+  { id: 598, rating: 3.0 }
+];
+
+const dataset2 = [
+  { id: 237, rating: 2.0 },
+  { id: 345, rating: 3.5 },
+  { id: 329, rating: 1.0 },
+  { id: 123, rating: 4.0 },
+  { id: 765, rating: 4.0 },
+  { id: 982, rating: 2.5 },
+  { id: 454, rating: 4.5 },
+  { id: 598, rating: 3.0 }
+];
+
 test('it does not render wrong arguments', function(assert) {
   this.set('title', 'Test');
   this.set('source', 'some_string');
@@ -15,6 +37,34 @@ test('it does not render wrong arguments', function(assert) {
     target=target
   }}`);
 
-  let $el = this.$('[data-test-selector="similarity-result"]');
-  assert.equal($el.eq(0).text().trim(), 'Test similarity is 0%');
+  let $el = this.$('[class="similarity-result"]');
+  assert.equal($el.eq(0).text().trim(), 'Test similarity is ?%');
+});
+
+test('it does calculate perfect match for identical datasets', function(assert) {
+  this.set('title', 'Test');
+  this.set('source', dataset1);
+  this.set('target', dataset1);
+  this.render(hbs`{{compatibility-indicator
+    title=title
+    source=source
+    target=target
+  }}`);
+
+  let $el = this.$('[class="similarity-result"]');
+  assert.equal($el.eq(0).text().trim(), 'Test similarity is 100%');
+});
+
+test('it does calculate proper similarity', function(assert) {
+  this.set('title', 'Test');
+  this.set('source', dataset1);
+  this.set('target', dataset2);
+  this.render(hbs`{{compatibility-indicator
+    title=title
+    source=source
+    target=target
+  }}`);
+
+  let $el = this.$('[class="similarity-result"]');
+  assert.equal($el.eq(0).text().trim(), 'Test similarity is 81.3%');
 });
