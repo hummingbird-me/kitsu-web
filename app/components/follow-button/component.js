96630536045e7863836de6f7132d92df42abb312
Sven <sgehring@kochag.ch>
2016-10-23 23:11:32 +0200

    Refactor following-button to ember-concurrency

--- app/components/follow-button/component.js
+++ app/components/follow-button/component.js
@@ -2,6 +2,7 @@ import Component from 'ember-component';
 import get from 'ember-metal/get';
 import set from 'ember-metal/set';
 import service from 'ember-service/inject';
+import { task } from 'ember-concurrency';
 
 export default Component.extend({
   session: service(),
@@ -9,17 +10,19 @@ export default Component.extend({
 
   init() {
     this._super(...arguments);
+    get(this, 'prepare').perform();
+  },
 
+  prepare: task(function* () {
     set(this, 'follower', get(this, 'session.account'));
     set(this, 'followed', get(this, 'user'));
-    set(this, 'loading', false);
 
     if (get(this, 'follower') === null || get(this, 'followed') === null) {
       set(this, 'following', false);
-      return;
+      yield;
     }
 
-    get(this, 'store').query('follow', {
+    yield get(this, 'store').query('follow', {
       filter: {
         follower: get(this, 'follower.id'),
         followed: get(this, 'followed.id')
@@ -28,28 +31,22 @@ export default Component.extend({
       set(this, 'relationship', get(e, 'firstObject'));
       set(this, 'following', (get(e, 'firstObject') !== undefined));
     });
-  },
+  }),
 
-  actions: {
-    toggleFollow() {
-      set(this, 'loading', true);
-
-      if (get(this, 'following')) {
-        get(this, 'relationship').destroyRecord().then(() => {
-          set(this, 'relationship', null);
-          set(this, 'loading', false);
-          set(this, 'following', true);
-        }, () => { set(this, 'loading', false); });
-      } else {
-        get(this, 'store').createRecord('follow', {
-          follower: get(this, 'follower'),
-          followed: get(this, 'followed')
-        }).save().then((e) => {
-          set(this, 'relationship', e);
-          set(this, 'loading', false);
-          set(this, 'following', false);
-        }, () => { set(this, 'loading', false); });
-      }
+  toggle: task(function* () {
+    if (get(this, 'following')) {
+      yield get(this, 'relationship').destroyRecord().then(() => {
+        set(this, 'relationship', null);
+        set(this, 'following', false);
+      });
+    } else {
+      yield get(this, 'store').createRecord('follow', {
+        follower: get(this, 'follower'),
+        followed: get(this, 'followed')
+      }).save().then((e) => {
+        set(this, 'relationship', e);
+        set(this, 'following', true);
+      });
     }
-  }
+  }).restartable()
 });
