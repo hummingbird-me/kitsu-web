eb23e5de2182f0da800060d7658d0d86913d3004
Thomas McNiven <hello@vevix.net>
2016-10-26 03:14:22 +1000

    Use truncate helper over jquery

--- app/components/truncate-text/component.js
+++ app/components/truncate-text/component.js
@@ -1,16 +1,8 @@
 import Component from 'ember-component';
-import get from 'ember-metal/get';
-import getter from 'client/utils/getter';
-import jQuery from 'jquery';
 
 const TruncateTextComponent = Component.extend({
   isExpanded: false,
-  max: 200,
-
-  truncated: getter(function() {
-    const text = jQuery.trim(get(this, 'text')).substring(0, get(this, 'max'));
-    return `${text}...`;
-  })
+  max: 200
 });
 
 TruncateTextComponent.reopenClass({
