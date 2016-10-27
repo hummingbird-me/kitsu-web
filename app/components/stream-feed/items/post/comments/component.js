5bd0bdc4a59b473107135f6688136dcd1448f1d8
Thomas McNiven <hello@vevix.net>
2016-10-27 23:40:28 +1000

    Open sign up modal for guests trying to comment

--- app/components/stream-feed/items/post/comments/component.js
+++ app/components/stream-feed/items/post/comments/component.js
@@ -1,11 +1,18 @@
 import Component from 'ember-component';
+import get from 'ember-metal/get';
+import service from 'ember-service/inject';
 import { invokeAction } from 'ember-invoke-action';
 
 export default Component.extend({
   classNames: ['stream-item-comments'],
+  session: service(),
 
   actions: {
     createComment(content) {
+      if (get(this, 'readOnly') === true) {
+        return get(this, 'session.signUpModal')();
+      }
+
       this.$('.add-comment').val('');
       invokeAction(this, 'onCreate', content);
     }
