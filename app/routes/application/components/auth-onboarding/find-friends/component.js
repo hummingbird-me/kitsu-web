53cab397ddd5ad8c20a1e33c24f1f7ffa02b293c
Thomas McNiven <hello@vevix.net>
2016-11-11 15:05:27 +1000

    Fix a number of bugs (see ext.)
    - Facebook error message on `find-friends` modal
    - Facebook erroring should not longer break your rating counter
    - Pagination + page increase on `rate-content` modal
    - Don't show global/following switch for guests

--- app/routes/application/components/auth-onboarding/find-friends/component.js
+++ app/routes/application/components/auth-onboarding/find-friends/component.js
@@ -1,23 +1,27 @@
 import Component from 'ember-component';
 import get from 'ember-metal/get';
+import set from 'ember-metal/set';
 import service from 'ember-service/inject';
 import { isPresent } from 'ember-utils';
 import { invokeAction } from 'ember-invoke-action';
 import { task, taskGroup } from 'ember-concurrency';
 
+// TODO: i18n errors
 export default Component.extend({
+  errorMessage: undefined,
+
   facebook: service(),
   session: service(),
   tasks: taskGroup().drop(),
 
   connectFacebook: task(function* () {
     yield get(this, 'facebook').connect(get(this, 'session.account'))
-      .catch(() => { /* TODO: Feedback */ });
+      .catch(() => set(this, 'errorMessage', 'There is already an account on Kitsu with your Facebook account connected.'));
   }).group('tasks'),
 
   disconnectFacebook: task(function* () {
     yield get(this, 'facebook').disconnect(get(this, 'session.account'))
-      .catch(() => { /* TODO: Feedback */ });
+      .catch(() => set(this, 'errorMessage', 'There was an issue disconnecting your Facebook account.'));
   }).group('tasks'),
 
   importFriends: task(function* () {
