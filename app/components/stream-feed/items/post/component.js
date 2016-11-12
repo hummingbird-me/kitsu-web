ff5422db6c82d291022df4a8afb7435358725f3d
Thomas McNiven <hello@vevix.net>
2016-11-12 12:56:10 +1000

    don't use feedId on dedicated post page

--- app/components/stream-feed/items/post/component.js
+++ app/components/stream-feed/items/post/component.js
@@ -74,7 +74,9 @@ export default Component.extend(ClipboardMixin, InViewportMixin, {
     }
     const post = get(this, 'post');
     set(this, 'isHidden', get(post, 'nsfw') === true || get(post, 'spoiler') === true);
-    set(this, 'userId', get(this, 'feedId').split(':')[1]);
+    if (get(this, 'feedId') !== undefined) {
+      set(this, 'userId', get(this, 'feedId').split(':')[1]);
+    }
   },
 
   actions: {
