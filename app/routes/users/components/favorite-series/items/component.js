c827d34ac58e3ea9731edc2828eea4750c805735
Thomas McNiven <hello@vevix.net>
2016-11-16 04:15:15 +1000

    show favorite characters on profile

--- app/routes/users/components/favorite-series/items/component.js
+++ app/routes/users/components/favorite-series/items/component.js
@@ -1,7 +1,13 @@
 import Component from 'ember-component';
+import get from 'ember-metal/get';
 import { invokeAction } from 'ember-invoke-action';
+import getter from 'client/utils/getter';
 
 export default Component.extend({
+  isCharacter: getter(function() {
+    return get(this, 'type') === 'character';
+  }),
+
   actions: {
     updateNextPage(...args) {
       invokeAction(this, 'updateNextPage', ...args);
