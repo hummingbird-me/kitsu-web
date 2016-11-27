2bebfc237c837a711bb7099e9d7708026898954c
Thomas McNiven <hello@vevix.net>
2016-11-28 08:20:22 +1000

    handle navbar hover state with JS

--- app/mixins/routes/cover-page.js
+++ app/mixins/routes/cover-page.js
@@ -13,30 +13,35 @@ export default Mixin.create({
     set(this, 'scrollBinding', binding);
     jQuery(document).on('scroll', binding);
 
-    scheduleOnce('afterRender', () => {
-      jQuery('.primary-nav').addClass('transparent');
-    });
+    const hoverBinding = bind(this, '_handleHover');
+    set(this, 'hoverBinding', hoverBinding);
+
+    scheduleOnce('afterRender', () => this._handleScroll());
   },
 
   deactivate() {
     this._super(...arguments);
     jQuery('body').removeClass('cover-page');
     jQuery(document).off('scroll', get(this, 'scrollBinding'));
+    jQuery('.primary-nav').off('mouseenter', get(this, 'hoverBinding'));
   },
 
   _handleScroll() {
-    if (jQuery(window).scrollTop() < 200) {
+    if (jQuery(window).scrollTop() >= 210) {
       jQuery('.primary-nav').removeClass('transparent');
-      const timer = later(() => {
-        if (jQuery(window).scrollTop() < 200) {
-          jQuery('.primary-nav').addClass('transparent');
-        }
-      }, 1000);
-
-      if (get(this, 'timer') !== undefined) {
-        cancel(get(this, 'timer'));
-      }
-      set(this, 'timer', timer);
+      jQuery('.primary-nav').off('mouseenter', get(this, 'hoverBinding'));
+    } else {
+      jQuery('.primary-nav').addClass('transparent');
+      jQuery('.primary-nav').on('mouseenter', get(this, 'hoverBinding'));
     }
+  },
+
+  _handleHover() {
+    jQuery('.primary-nav').removeClass('transparent');
+    cancel(get(this, 'timer'));
+    jQuery('.primary-nav').one('mouseleave', () => {
+      const timer = later(() => jQuery('.primary-nav').addClass('transparent'), 1000);
+      set(this, 'timer', timer);
+    });
   }
 });
