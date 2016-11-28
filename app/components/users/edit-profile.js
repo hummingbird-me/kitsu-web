6c7095975b0d65fdf9ee29180a7ab340802578fc
Thomas McNiven <hello@vevix.net>
2016-11-29 02:19:13 +1000

    Can have husbando

--- app/components/users/edit-profile.js
+++ app/components/users/edit-profile.js
@@ -24,7 +24,7 @@ export default Component.extend({
 
   coverImageStyle: computed('user.coverImage', {
     get() {
-      const coverImage = image([get(this, 'user.coverImage')]);
+      const coverImage = image(get(this, 'user.coverImage'));
       return `background-image: url("${coverImage}")`.htmlSafe();
     }
   }).readOnly(),
