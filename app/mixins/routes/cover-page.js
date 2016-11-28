7941fabb92331890113e86ee3b2239836c39c63a
Thomas McNiven <hello@vevix.net>
2016-11-28 15:55:02 +1000

    fixes for nav header hover state

--- app/mixins/routes/cover-page.js
+++ app/mixins/routes/cover-page.js
@@ -1,47 +1,46 @@
 import Mixin from 'ember-metal/mixin';
 import get from 'ember-metal/get';
 import set from 'ember-metal/set';
+import { isEmpty } from 'ember-utils';
 import { bind, later, cancel, scheduleOnce } from 'ember-runloop';
 import jQuery from 'jquery';
 
+const DISTANCE = 210;
+
 export default Mixin.create({
+  isHovered: false,
+
   activate() {
     this._super(...arguments);
     jQuery('body').addClass('cover-page');
-
-    const binding = bind(this, '_handleScroll');
-    set(this, 'scrollBinding', binding);
-    jQuery(document).on('scroll', binding);
-
-    const hoverBinding = bind(this, '_handleHover');
-    set(this, 'hoverBinding', hoverBinding);
-
-    scheduleOnce('afterRender', () => this._handleScroll());
+    jQuery(document).on('scroll.cover', bind(this, '_handleScroll'));
+    scheduleOnce('afterRender', bind(this, '_handleScroll'));
+    scheduleOnce('afterRender', () => {
+      jQuery('.primary-nav').hover(() => {
+        cancel(get(this, 'timer'));
+        jQuery('.primary-nav').removeClass('transparent');
+        set(this, 'isHovered', true);
+      }, () => {
+        if (jQuery(window).scrollTop() < DISTANCE && isEmpty(jQuery('#search').val()) === true) {
+          const timer = later(() => jQuery('.primary-nav').addClass('transparent'), 500);
+          set(this, 'timer', timer);
+        }
+        set(this, 'isHovered', false);
+      });
+    });
   },
 
   deactivate() {
     this._super(...arguments);
     jQuery('body').removeClass('cover-page');
-    jQuery(document).off('scroll', get(this, 'scrollBinding'));
-    jQuery('.primary-nav').off('mouseenter', get(this, 'hoverBinding'));
+    jQuery(document).off('scroll.cover');
   },
 
   _handleScroll() {
-    if (jQuery(window).scrollTop() >= 210) {
+    if (jQuery(window).scrollTop() >= DISTANCE) {
       jQuery('.primary-nav').removeClass('transparent');
-      jQuery('.primary-nav').off('mouseenter', get(this, 'hoverBinding'));
-    } else {
+    } else if (get(this, 'isHovered') === false && isEmpty(jQuery('#search').val()) === true) {
       jQuery('.primary-nav').addClass('transparent');
-      jQuery('.primary-nav').on('mouseenter', get(this, 'hoverBinding'));
     }
-  },
-
-  _handleHover() {
-    jQuery('.primary-nav').removeClass('transparent');
-    cancel(get(this, 'timer'));
-    jQuery('.primary-nav').one('mouseleave', () => {
-      const timer = later(() => jQuery('.primary-nav').addClass('transparent'), 1000);
-      set(this, 'timer', timer);
-    });
   }
 });
