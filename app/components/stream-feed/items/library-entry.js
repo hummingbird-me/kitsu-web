af53f0c5324cd03a4eb686819a21e1e3187549ac
Thomas McNiven <hello@vevix.net>
2016-11-29 11:29:01 +1000

    performance changes on feeds

--- app/components/stream-feed/items/library-entry.js
+++ app/components/stream-feed/items/library-entry.js
@@ -1,7 +1,6 @@
 import Component from 'ember-component';
 import get from 'ember-metal/get';
 import service from 'ember-service/inject';
-import computed from 'ember-computed';
 import getter from 'client/utils/getter';
 import moment from 'moment';
 
@@ -14,30 +13,28 @@ export default Component.extend({
     return get(this, 'group.activities.firstObject.media');
   }),
 
-  groupByTime: computed('group.activities.[]', {
-    get() {
-      const temp = {};
-      get(this, 'group.activities').forEach((activity) => {
-        const time = get(activity, 'time');
-        const calendar = moment(time).calendar(null, {
-          sameDay: '[Today]',
-          lastDay: '[Yesterday]',
-          lastWeek: 'dddd',
-          sameElse: 'dddd',
-        });
-        const key = `${calendar}-${moment(time).format('DD')}`;
-        if (get(temp, key) === undefined) {
-          temp[key] = {
-            calendar,
-            date: moment(time).format('MMM Do'),
-            activities: []
-          };
-        }
-        temp[key].activities.push(activity);
+  groupByTime: getter(function() {
+    const temp = {};
+    get(this, 'group.activities').forEach((activity) => {
+      const time = get(activity, 'time');
+      const calendar = moment(time).calendar(null, {
+        sameDay: '[Today]',
+        lastDay: '[Yesterday]',
+        lastWeek: 'dddd',
+        sameElse: 'dddd',
       });
-      return Object.values(temp);
-    }
-  }).readOnly(),
+      const key = `${calendar}-${moment(time).format('DD')}`;
+      if (get(temp, key) === undefined) {
+        temp[key] = {
+          calendar,
+          date: moment(time).format('MMM Do'),
+          activities: []
+        };
+      }
+      temp[key].activities.push(activity);
+    });
+    return Object.values(temp);
+  }),
 
   actions: {
     trackEngagement(label) {
