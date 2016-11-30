6e0092ad91e8139eb966f1db38cefeba5d066f51
Thomas McNiven <hello@vevix.net>
2016-11-30 23:41:52 +1000

    add `settings-page` class

--- app/routes/settings.js
+++ app/routes/settings.js
@@ -3,6 +3,7 @@ import get from 'ember-metal/get';
 import service from 'ember-service/inject';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 import DataErrorMixin from 'client/mixins/routes/data-error';
+import jQuery from 'jquery';
 
 export default Route.extend(AuthenticatedRouteMixin, DataErrorMixin, {
   session: service(),
@@ -11,5 +12,15 @@ export default Route.extend(AuthenticatedRouteMixin, DataErrorMixin, {
     return get(this, 'store').query('list-import', {
       filter: { user_id: get(this, 'session.account.id') }
     });
+  },
+
+  activate() {
+    this._super(...arguments);
+    jQuery('body').addClass('settings-page');
+  },
+
+  deactivate() {
+    this._super(...arguments);
+    jQuery('body').removeClass('settings-page');
   }
 });
