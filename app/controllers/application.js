2f1b4906cbd5d6d4d6de61e45d556270ad548502
Thomas McNiven <hello@vevix.net>
2016-12-01 09:49:36 +1000

    add banner for staging only

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,3 +1,7 @@
 import Controller from 'ember-controller';
+import Config from 'client/config/environment';
+import getter from 'client/utils/getter';
 
-export default Controller.extend();
+export default Controller.extend({
+  isStaging: getter(() => Config.isStaging)
+});
