d21f4351343aa3198f61ec30dcf59e303d7b2864
Thomas McNiven <hello@vevix.net>
2016-12-02 00:20:49 +1000

    Update site-header.js

--- app/components/application/site-header.js
+++ app/components/application/site-header.js
@@ -1,11 +1,14 @@
 import Component from 'ember-component';
 import get from 'ember-metal/get';
 import service from 'ember-service/inject';
+import Config from 'client/config/environment';
+import getter from 'client/utils/getter';
 
 export default Component.extend({
   authOpened: false,
   authComponent: 'social-auth',
   session: service(),
+  isStaging: getter(() => Config.isStaging),
 
   actions: {
     invalidateSession() {
