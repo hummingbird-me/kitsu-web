a37d8143b1c5659907bb5b71ae02db1f876339c3
Thomas McNiven <hello@vevix.net>
2016-12-02 00:21:19 +1000

    Fix staging banner (#58)
    * Update application.js
    
    * Update site-header.js

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,7 +1,3 @@
 import Controller from 'ember-controller';
-import Config from 'client/config/environment';
-import getter from 'client/utils/getter';
 
-export default Controller.extend({
-  isStaging: getter(() => Config.isStaging)
-});
+export default Controller.extend();
