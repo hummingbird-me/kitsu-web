d066a31ce392336139b83518f78e02839818380f
Thomas McNiven <hello@vevix.net>
2016-12-03 05:17:08 +1000

    check that clipboard exists

--- app/mixins/clipboard.js
+++ app/mixins/clipboard.js
@@ -12,6 +12,8 @@ export default Mixin.create({
 
   willDestroyElement() {
     this._super(...arguments);
-    get(this, 'clipboard').destroy();
+    if (get(this, 'clipboard') !== undefined) {
+      get(this, 'clipboard').destroy();
+    }
   }
 });
