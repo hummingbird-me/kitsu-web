5a2a79cbbf1f98dc08bac328f23c2172e6410ce7
Thomas McNiven <hello@vevix.net>
2016-12-03 19:03:07 +1000

    fix reset password payload

--- app/controllers/password-reset.js
+++ app/controllers/password-reset.js
@@ -14,10 +14,12 @@ export default Controller.extend(Validations, {
   email: null,
   password: null,
   passwordConfirm: null,
-
   ajax: service(),
   notify: service(),
 
+  /**
+   * Password passes validation and matches the password confirmation.
+   */
   passwordValid: computed('password', 'passwordConfirm', {
     get() {
       return get(this, 'validations.attrs.password.isValid') &&
@@ -25,28 +27,39 @@ export default Controller.extend(Validations, {
     }
   }).readOnly(),
 
+  /**
+   * Tell the server to send off an email to the supplied email/username account.
+   */
   sendEmail: task(function* () {
     yield get(this, 'ajax').request('/users/_recover', {
       method: 'POST',
-      data: JSON.stringify(get(this, 'email')),
-      contentType: 'application/json'
+      data: JSON.stringify({ username: get(this, 'email') }),
+      contentType: 'application/vnd.api+json'
     }).then(() => {
-      get(this, 'notify').success('Your password reset email has been sent! Please check your email.', { closeAfter: 5000 });
+      const message = 'Your password reset email has been sent! Please check your email.';
+      get(this, 'notify').success(message, { closeAfter: 5000 });
       this.transitionToRoute('dashboard');
     }).catch(err => get(this, 'notify').error(errorMessages(err)));
   }).drop(),
 
+  /**
+   * Send the user's password change request upstream
+   */
   resetPassword: task(function* () {
+    // get the current user based off the token supplied as a query param
     let user = yield get(this, 'ajax').request('/users?filter[self]=true', {
       headers: {
         Authorization: `Bearer ${get(this, 'usableToken')}`
       }
     }).catch(err => get(this, 'notify').error(errorMessages(err)));
 
+    // if we didn't get a response than it is most likely that the token wasn't associated
+    // with a user.
     if (isEmpty(get(user, 'data')) === true) {
       return get(this, 'notify').error('The token is either invalid or expired.');
     }
 
+    // send the request upstream to change the user's password
     user = get(user, 'data.firstObject');
     yield get(this, 'ajax').request(`/users/${get(user, 'id')}`, {
       method: 'PATCH',
@@ -62,7 +75,8 @@ export default Controller.extend(Validations, {
       }),
       contentType: 'application/vnd.api+json'
     }).then(() => {
-      get(this, 'notify').success('Your password was successfully reset. Login using your new password.', { closeAfter: 5000 });
+      const message = 'Your password was successfully reset. Login using your new password.';
+      get(this, 'notify').success(message, { closeAfter: 5000 });
       this.transitionToRoute('dashboard');
     }).catch(err => get(this, 'notify').error(errorMessages(err)));
   }).drop()
