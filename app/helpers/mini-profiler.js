4919c8c3574b0f2dd34afe21954f021dbf5280f9
Thomas McNiven <hello@vevix.net>
2016-12-03 23:23:48 +1000

    don't include script tags into handlebars template

---
+++ app/helpers/mini-profiler.js
@@ -0,0 +1,41 @@
+import Helper from 'ember-helper';
+import service from 'ember-service/inject';
+import get from 'ember-metal/get';
+import { isEmpty } from 'ember-utils';
+import observer from 'ember-metal/observer';
+
+export default Helper.extend({
+  session: service(),
+
+  compute() {
+    if (isEmpty(get(this, 'session.account'))) {
+      return;
+    }
+
+    if (get(this, 'session.account').hasRole('admin')) {
+      const element = document.createElement('script');
+      element.async = true;
+      element.src = '/mini-profiler-resources/includes.js?v=12b4b45a3c42e6e15503d7a03810ff33';
+      element.id = 'mini-profiler';
+      element.dataset.version = '12b4b45a3c42e6e15503d7a03810ff33';
+      element.dataset.path = '/mini-profiler-resources/';
+      element.dataset.currentId = 'redo66j4g1077kto8uh3';
+      element.dataset.ids = 'redo66j4g1077kto8uh3';
+      element.dataset.position = 'left';
+      element.dataset.trivial = false;
+      element.dataset.children = false;
+      element.dataset.maxTraces = 10;
+      element.dataset.controls = false;
+      element.dataset.authorized = true;
+      element.dataset.toggleShortcut = 'Alt+P';
+      element.dataset.startHidden = false;
+      element.dataset.collapseResults = true;
+      const script = document.getElementsByTagName('script')[0];
+      script.parentNode.insertBefore(element, script);
+    }
+  },
+
+  didAuthenticate: observer('session.account', function() {
+    this.recompute();
+  })
+});
