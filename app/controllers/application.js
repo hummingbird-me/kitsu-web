8e24476d8d72dfd3c5424c8702399609a717b4fb
Thomas McNiven <hello@vevix.net>
2016-12-05 00:47:50 +1000

    remove free previews from staging, need tokens

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,3 +1,9 @@
 import Controller from 'ember-controller';
+import service from 'ember-service/inject';
+import Config from 'client/config/environment';
+import getter from 'client/utils/getter';
 
-export default Controller.extend();
+export default Controller.extend({
+  session: service(),
+  isStaging: getter(() => Config.isStaging),
+});
