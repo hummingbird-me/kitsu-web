9b37a101bb6b9109962ece20591bcfab3f122483
James Harris <wopiancitizen123@gmail.com>
2016-12-05 09:41:29 +0000

    Expand years filter 2

--- app/controllers/media/index.js
+++ app/controllers/media/index.js
@@ -14,7 +14,7 @@ export default Controller.extend({
   averageRating: [0.5, 5.0],
   genres: [],
   text: undefined,
-  year: [1914, 2018],
+  year: [1907, 2018],
 
   // Buffer values so we don't mutate the query params every update
   dirtyYear: [1907, 2018],
