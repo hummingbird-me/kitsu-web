e2324a01db3d5e8587ebe056b4c2c2dc21e27f94
Thomas McNiven <hello@vevix.net>
2016-12-09 08:08:16 +1000

    close and show loader on block modal

--- app/components/modals/block-user.js
+++ app/components/modals/block-user.js
@@ -3,6 +3,7 @@ import get from 'ember-metal/get';
 import { assert } from 'ember-metal/utils';
 import service from 'ember-service/inject';
 import { isPresent } from 'ember-utils';
+import { task } from 'ember-concurrency';
 import errorMessages from 'client/utils/error-messages';
 
 export default Component.extend({
@@ -10,23 +11,22 @@ export default Component.extend({
   session: service(),
   store: service(),
 
+  blockUser: task(function* () {
+    const block = get(this, 'store').createRecord('block', {
+      user: get(this, 'session.account'),
+      blocked: get(this, 'user')
+    });
+
+    yield block.save().then(() => {
+      this.$('.modal').modal('hide');
+      get(this, 'notify').info(`You have blocked ${get(this, 'user.name')}`);
+    }).catch(err => (
+      get(this, 'notify').error(errorMessages(err))
+    ));
+  }).drop(),
+
   init() {
     this._super(...arguments);
     assert('You must pass a user into the `block-user` component.', isPresent(get(this, 'user')));
-  },
-
-  actions: {
-    blockUser() {
-      const block = get(this, 'store').createRecord('block', {
-        user: get(this, 'session.account'),
-        blocked: get(this, 'user')
-      });
-
-      block.save().then(() => {
-        get(this, 'notify').info(`You have blocked ${get(this, 'user.name')}`);
-      }).catch(err => (
-        get(this, 'notify').error(errorMessages(err))
-      ));
-    },
   }
 });
