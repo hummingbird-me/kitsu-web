5312867aa5ea608c628098147e6589354daf244a
Thomas McNiven <hello@vevix.net>
2016-12-10 01:27:50 +1000

    don't serialize unit relations on library-entry

--- app/serializers/library-entry.js
+++ app/serializers/library-entry.js
@@ -2,6 +2,8 @@ import ApplicationSerializer from 'client/serializers/application';
 
 export default ApplicationSerializer.extend({
   attrs: {
-    review: { serialize: false }
+    review: { serialize: false },
+    unit: { serialize: false },
+    nextUnit: { serialize: false }
   }
 });
