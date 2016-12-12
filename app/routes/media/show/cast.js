e649b05d034b82379d6f942e51e871a9556c49b6
Thomas McNiven <hello@vevix.net>
2016-12-12 14:46:03 +1000

    sort by mediaType on casting page

--- app/routes/media/show/cast.js
+++ app/routes/media/show/cast.js
@@ -1,15 +1,21 @@
 import Route from 'ember-route';
 import get from 'ember-metal/get';
+import { capitalize } from 'ember-string';
+import PaginationMixin from 'client/mixins/routes/pagination';
 
-export default Route.extend({
+export default Route.extend(PaginationMixin, {
   templateName: 'media/show/cast',
 
   model() {
     const [mediaType] = get(this, 'routeName').split('.');
     const media = this.modelFor(`${mediaType}.show`);
     return get(this, 'store').query('casting', {
-      filter: { media_id: get(media, 'id') },
-      include: 'character,person'
+      filter: {
+        media_type: capitalize(mediaType),
+        media_id: get(media, 'id')
+      },
+      include: 'character,person',
+      sort: 'featured'
     });
   }
 });
--- app/routes/media/show/episodes.js
+++ app/routes/media/show/episodes.js
@@ -1,7 +1,6 @@
 import Route from 'ember-route';
 import get from 'ember-metal/get';
 import { capitalize } from 'ember-string';
-import { modelType } from 'client/helpers/model-type';
 import PaginationMixin from 'client/mixins/routes/pagination';
 
 export default Route.extend(PaginationMixin, {
@@ -12,7 +11,7 @@ export default Route.extend(PaginationMixin, {
     const media = this.modelFor(`${mediaType}.show`);
     return get(this, 'store').query('episode', {
       filter: {
-        media_type: capitalize(modelType([media])),
+        media_type: capitalize(mediaType),
         media_id: get(media, 'id')
       }
     });
