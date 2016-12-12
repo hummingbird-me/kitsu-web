d62f107757e6676c00a4cf02a07822e88aeb9ec6
Thomas McNiven <hello@vevix.net>
2016-12-12 13:56:18 +1000

    add pagination to episodes page

--- app/routes/media/show/episodes.js
+++ app/routes/media/show/episodes.js
@@ -2,8 +2,9 @@ import Route from 'ember-route';
 import get from 'ember-metal/get';
 import { capitalize } from 'ember-string';
 import { modelType } from 'client/helpers/model-type';
+import PaginationMixin from 'client/mixins/routes/pagination';
 
-export default Route.extend({
+export default Route.extend(PaginationMixin, {
   templateName: 'media/show/episodes',
 
   model() {
