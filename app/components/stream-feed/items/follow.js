7f98a3668617d854737808d620174eb7a801fb71
Thomas McNiven <hello@vevix.net>
2016-12-13 03:53:12 +1000

    add basic styling to follow stories

--- app/components/stream-feed/items/follow.js
+++ app/components/stream-feed/items/follow.js
@@ -1,9 +1,11 @@
 import Component from 'ember-component';
 import get from 'ember-metal/get';
+import service from 'ember-service/inject';
 import getter from 'client/utils/getter';
 
 export default Component.extend({
   classNames: ['stream-item', 'row'],
+  metrics: service(),
 
   activity: getter(function() {
     return get(this, 'group.activities.firstObject');
@@ -11,5 +13,19 @@ export default Component.extend({
 
   follow: getter(function() {
     return get(this, 'activity.subject');
-  })
+  }),
+
+  actions: {
+    trackEngagement(label) {
+      const data = {
+        label,
+        content: { foreign_id: `Follow:${get(this, 'follow.id')}` },
+        position: get(this, 'positionInFeed') || 0
+      };
+      if (get(this, 'feedId') !== undefined) {
+        data.feed_id = get(this, 'feedId');
+      }
+      get(this, 'metrics').invoke('trackEngagement', 'Stream', data);
+    },
+  }
 });
