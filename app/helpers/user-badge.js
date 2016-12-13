c13cb312476edac83f799c2d515e4ce242022a27
Thomas McNiven <hello@vevix.net>
2016-12-14 05:04:02 +1000

    use title for user-badge

--- app/helpers/user-badge.js
+++ app/helpers/user-badge.js
@@ -1,27 +1,15 @@
-import Helper from 'ember-helper';
+import { helper } from 'ember-helper';
 import get from 'ember-metal/get';
-import set from 'ember-metal/set';
-import { isEmpty } from 'ember-utils';
+import { isPresent } from 'ember-utils';
 import { htmlSafe } from 'ember-string';
-import observer from 'ember-metal/observer';
 
-export default Helper.extend({
-  compute([user]) {
-    set(this, 'user', user);
-    const isPro = get(user, 'isPro');
-    const userRoulette = user.isFulfilled !== undefined ? get(user, 'content') : user;
-    if (isEmpty(userRoulette)) {
-      return;
-    }
-    const isStaff = userRoulette.hasRole('admin');
-    if (isStaff) {
-      return htmlSafe('<span class="tag tag-default role-tag">STAFF</span>');
-    } else if (isPro) {
-      return htmlSafe('<span class="tag tag-default role-tag">PRO</span>');
-    }
-  },
+export function userBadge([user]) {
+  const isPro = get(user, 'isPro');
+  if (isPresent(get(user, 'title'))) {
+    return htmlSafe('<span class="tag tag-default role-tag">STAFF</span>');
+  } else if (isPro) {
+    return htmlSafe('<span class="tag tag-default role-tag">PRO</span>');
+  }
+}
 
-  gotRoles: observer('user.userRoles.@each.role', function() {
-    this.recompute();
-  })
-});
+export default helper(userBadge);
