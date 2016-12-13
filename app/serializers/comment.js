ce1869bed653e3c3d090a0c50f232ffa9fed1766
Thomas McNiven <hello@vevix.net>
2016-12-13 13:43:47 +1000

    don't serialize contentFormatted

--- app/serializers/comment.js
+++ app/serializers/comment.js
@@ -2,6 +2,7 @@ import ApplicationSerializer from 'client/serializers/application';
 
 export default ApplicationSerializer.extend({
   attrs: {
+    contentFormatted: { serialize: false },
     likesCount: { serialize: false },
     repliesCount: { serialize: false },
     createdAt: { serialize: false },
--- app/serializers/post.js
+++ app/serializers/post.js
@@ -2,6 +2,7 @@ import ApplicationSerializer from 'client/serializers/application';
 
 export default ApplicationSerializer.extend({
   attrs: {
+    contentFormatted: { serialize: false },
     commentsCount: { serialize: false },
     postLikesCount: { serialize: false },
     topLevelCommentsCount: { serialize: false },
