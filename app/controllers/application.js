08c76fce3f02dd2c02270e1f49c9af0a34de6527
Sven Gehring <cbrxde@gmail.com>
2016-12-15 16:33:04 +0100

    Query notifications (#93)
    * Use query-parameters to generate notification link
    
    * Mark notification as read, if query-param is present
    
    * Final cleanup
    
    * Mark notification as read locally if it is in store
    
    * Final cleanup

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,8 +2,36 @@ import Controller from 'ember-controller';
 import service from 'ember-service/inject';
 import Config from 'client/config/environment';
 import getter from 'client/utils/getter';
+import observer from 'ember-metal/observer';
+import get from 'ember-metal/get';
+import set from 'ember-metal/set';
 
 export default Controller.extend({
+  queryParams: ['notification'],
+  notification: null,
+
+  ajax: service(),
+  store: service(),
   session: service(),
   isStaging: getter(() => Config.isStaging),
+
+  notificationTracker: observer('notification', function() {
+    const notification = get(this, 'notification');
+    if (notification === null || !get(this, 'session.hasUser')) return;
+
+    const feedUrl = `/feeds/notifications/${get(this, 'session.account.id')}/_read`;
+
+    get(this, 'ajax').request(feedUrl, {
+      method: 'POST',
+      data: JSON.stringify([notification]),
+      contentType: 'application/json'
+    }).then(() => {
+      set(this, 'notification', null);
+      const trackedItems = get(this, 'store').peekAll('notification').filterBy('streamId', notification);
+      if (trackedItems.length === 1) {
+        const [trackedItem] = trackedItems;
+        set(trackedItem, 'isRead', true);
+      }
+    }).catch(() => {});
+  })
 });
