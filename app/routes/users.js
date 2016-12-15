458c4064fb53ebd34e9367467f6bcc8aa0eeebd0
Thomas McNiven <hello@vevix.net>
2016-12-16 01:01:29 +1000

    don't request user API when visiting local user page

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,16 +1,23 @@
 import Route from 'ember-route';
 import get from 'ember-metal/get';
 import set from 'ember-metal/set';
+import service from 'ember-service/inject';
 import DataErrorMixin from 'client/mixins/routes/data-error';
 import CanonicalRedirectMixin from 'client/mixins/routes/canonical-redirect';
 import CoverPageMixin from 'client/mixins/routes/cover-page';
 
 export default Route.extend(DataErrorMixin, CanonicalRedirectMixin, CoverPageMixin, {
+  session: service(),
+
   model({ name }) {
     if (name.match(/\D+/)) {
+      const isSelf = get(this, 'session.account.name') === name;
+      if (isSelf) { return get(this, 'session.account'); }
       return get(this, 'store').query('user', { filter: { name }, include: 'pinnedPost' })
         .then(records => get(records, 'firstObject'));
     }
+    const isSelf = get(this, 'session.account.id') === name;
+    if (isSelf) { return get(this, 'session.account'); }
     return get(this, 'store').findRecord('user', name, { include: 'pinnedPost' });
   },
 
