c03e3ed98646943c1983f227f52516012c7fd085
Thomas McNiven <hello@vevix.net>
2016-12-17 09:27:27 +1000

    404 on missing comment

--- app/routes/comments.js
+++ app/routes/comments.js
@@ -2,8 +2,9 @@ import Route from 'ember-route';
 import get from 'ember-metal/get';
 import set from 'ember-metal/set';
 import service from 'ember-service/inject';
+import DataErrorMixin from 'client/mixins/routes/data-error';
 
-export default Route.extend({
+export default Route.extend(DataErrorMixin, {
   i18n: service(),
 
   model({ id }) {
