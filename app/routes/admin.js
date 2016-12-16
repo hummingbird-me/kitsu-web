b1e771f37b82e45b625410ec0c6ff93e319c5686
Thomas McNiven <hello@vevix.net>
2016-12-16 22:02:52 +1000

    admin route no longer needs to RSVP roles

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,7 +1,6 @@
 import Route from 'ember-route';
 import get from 'ember-metal/get';
 import service from 'ember-service/inject';
-import RSVP from 'rsvp';
 import DataErrorMixin from 'client/mixins/routes/data-error';
 
 export default Route.extend(DataErrorMixin, {
@@ -9,18 +8,8 @@ export default Route.extend(DataErrorMixin, {
 
   beforeModel() {
     const user = get(this, 'session.account');
-    if (user === undefined) {
+    if (user === undefined || !user.hasRole('admin')) {
       return this.transitionTo('dashboard');
     }
-
-    return new RSVP.Promise((resolve) => {
-      get(user, 'userRoles').then(roles => resolve(roles));
-    }).then(roles => (
-      RSVP.all(roles.map(role => get(role, 'role'))).then(() => {
-        if (user.hasRole('admin') === false) {
-          this.transitionTo('dashboard');
-        }
-      })
-    ));
   }
 });
