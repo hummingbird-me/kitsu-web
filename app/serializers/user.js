bbe708d884dbce03d90e6a2cc677dca39a0126f0
Thomas McNiven <hello@vevix.net>
2016-12-20 00:06:18 +1000

    add new onboarding model properties

--- app/serializers/user.js
+++ app/serializers/user.js
@@ -4,10 +4,12 @@ export default ApplicationSerializer.extend({
   attrs: {
     commentsCount: { serialize: false },
     favoritesCount: { serialize: false },
+    feedCompleted: { serialize: false },
     followingCount: { serialize: false },
     followersCount: { serialize: false },
     likesGivenCount: { serialize: false },
     postsCount: { serialize: false },
+    profileCompleted: { serialize: false },
     waifuDirtyHack: { serialize: false }
   }
 });
