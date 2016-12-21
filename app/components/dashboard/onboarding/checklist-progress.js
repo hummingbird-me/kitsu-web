1a2658abf5f46175a7fc6563429740d424fb464f
Thomas McNiven <hello@vevix.net>
2016-12-21 21:14:19 +1000

    hide checklists by their completed flag

--- app/components/dashboard/onboarding/checklist-progress.js
+++ app/components/dashboard/onboarding/checklist-progress.js
@@ -1,24 +1,12 @@
 import Component from 'ember-component';
 import computed, { gte } from 'ember-computed';
 import get from 'ember-metal/get';
-import service from 'ember-service/inject';
-import observer from 'ember-metal/observer';
 
 export default Component.extend({
-  classNameBindings: ['isCompleted'],
-  metrics: service(),
-
   hasRatings: gte('user.ratingsCount', 5),
   hasFollows: gte('user.followingCount', 5),
   hasComments: gte('user.commentsCount', 1),
   hasLikes: gte('user.likesGivenCount', 3),
-
-  isCompleted: computed('hasRatings', 'hasFollows', 'hasComments', 'hasLikes', {
-    get() {
-      return get(this, 'hasRatings') === true && get(this, 'hasFollows') === true &&
-        get(this, 'hasComments') === true && get(this, 'hasLikes') === true;
-    }
-  }).readOnly(),
 
   ratingsLeft: computed('user.ratingsCount', {
     get() {
@@ -36,11 +24,5 @@ export default Component.extend({
     get() {
       return 3 - get(this, 'user.likesGivenCount');
     }
-  }).readOnly(),
-
-  _track: observer('isCompleted', function() {
-    if (get(this, 'isCompleted') === true) {
-      get(this, 'metrics').trackEvent({ category: 'onboarding', action: 'complete', label: 'dashboard' });
-    }
-  })
+  }).readOnly()
 });
--- app/components/users/onboarding/checklist-progress.js
+++ app/components/users/onboarding/checklist-progress.js
@@ -1,8 +1,6 @@
 import Component from 'ember-component';
 import computed, { alias, gte } from 'ember-computed';
 import get from 'ember-metal/get';
-import service from 'ember-service/inject';
-import observer from 'ember-metal/observer';
 import { isObject } from 'client/helpers/is-object';
 
 const isObjectComputed = property => (
@@ -14,33 +12,15 @@ const isObjectComputed = property => (
 );
 
 export default Component.extend({
-  classNameBindings: ['isCompleted'],
-
-  metrics: service(),
   hasRatings: gte('user.ratingsCount', 5),
   hasAvatar: isObjectComputed('user.avatar'),
   hasCover: isObjectComputed('user.coverImage'),
   hasAbout: alias('user.about'),
   hasFavorites: gte('user.favoritesCount', 1),
 
-  isCompleted: computed('hasRatings', 'hasAvatar', 'hasCover', 'hasAbout', 'hasFavorites', {
-    get() {
-      return get(this, 'hasRatings') === true && get(this, 'hasAvatar') === true &&
-        get(this, 'hasCover') === true &&
-        (get(this, 'hasAbout') !== undefined || get(this, 'hasAbout') !== null) &&
-        get(this, 'hasFavorites') === true;
-    }
-  }).readOnly(),
-
   ratingsLeft: computed('user.ratingsCount', {
     get() {
       return 5 - get(this, 'user.ratingsCount');
     }
-  }).readOnly(),
-
-  _track: observer('isCompleted', function() {
-    if (get(this, 'isCompleted') === true) {
-      get(this, 'metrics').trackEvent({ category: 'onboarding', action: 'complete', label: 'users' });
-    }
-  })
+  }).readOnly()
 });
