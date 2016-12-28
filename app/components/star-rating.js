158498a4da930746594d6d36479e9d3dc9ea2097
Thomas McNiven <hello@vevix.net>
2016-12-29 02:10:53 +1000

    reset rating if selecting the same value (#113)

--- app/components/star-rating.js
+++ app/components/star-rating.js
@@ -1,6 +1,7 @@
 import StarRatingComponent from 'ember-star-rating/components/star-rating';
 import service from 'ember-service/inject';
 import get from 'ember-metal/get';
+import { invokeAction } from 'ember-invoke-action';
 
 export default StarRatingComponent.extend({
   session: service(),
@@ -8,5 +9,22 @@ export default StarRatingComponent.extend({
   click() {
     get(this, 'session.account').incrementProperty('ratingsCount');
     this._super(...arguments);
-  }
+  },
+
+  /**
+   * Override `_update` from `ember-star-rating` to support setting rating to `null`
+   * if the value is the same.
+   */
+  _update(event) {
+    if (get(this, 'readOnly')) {
+      return;
+    }
+    const pageX = event.pageX;
+    const target = this._getTarget(pageX);
+    let rating = Math.floor(target * 2) / 2;
+    if (rating === get(this, 'rating')) {
+      rating = null;
+    }
+    invokeAction(this, 'onClick', rating);
+  },
 });
