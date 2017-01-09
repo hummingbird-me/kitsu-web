882c8ed34c26ec93d6039648503200a799c304af
Thomas McNiven <hello@vevix.net>
2017-01-09 17:09:03 +1000

    Library sorting (#96)
    * Implement library sorting UI
    
    * handle sort being in the URL on initial load
    
    * Support filtering title by users preference
    
    * rotate svg icon for ascending sort
    
    * Save the last used sort

---
+++ app/components/users/library/library-sorting.js
@@ -0,0 +1,42 @@
+import Component from 'ember-component';
+import get from 'ember-metal/get';
+import set from 'ember-metal/set';
+import { invokeAction } from 'ember-invoke-action';
+
+export default Component.extend({
+  didReceiveAttrs() {
+    this._super(...arguments);
+    this._resetActiveSort();
+    const sort = get(this, 'sort');
+    if (sort !== undefined) {
+      const type = sort.charAt(0) === '-' ? sort.slice(1) : sort;
+      const direction = sort.charAt(0) === '-' ? 'desc' : 'asc';
+      set(this, 'active', { type, direction });
+    }
+  },
+
+  _updateActiveSort(type) {
+    const active = get(this, 'active');
+    if (get(active, 'type') === type) {
+      // same type, toggle direction
+      if (get(active, 'direction') === 'asc') {
+        set(active, 'direction', 'desc');
+      } else if (get(active, 'direction') === 'desc') {
+        this._resetActiveSort();
+      }
+    } else {
+      set(this, 'active', { type, direction: 'asc' });
+    }
+  },
+
+  _resetActiveSort() {
+    set(this, 'active', {});
+  },
+
+  actions: {
+    sort(type) {
+      this._updateActiveSort(type);
+      invokeAction(this, 'onSortChange', get(this, 'active'));
+    }
+  }
+});
