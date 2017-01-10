56270cf6fb7fcf00bdf052ccf2a76fdff0faa6ab
Sven Gehring <sven@kitsu.io>
2017-01-10 16:35:32 +0100

    Add modal contents for export setup

---
+++ app/components/application/auth-onboarding/export-basicauth.js
@@ -0,0 +1,55 @@
+import Component from 'ember-component';
+import get from 'ember-metal/get';
+import set from 'ember-metal/set';
+import service from 'ember-service/inject';
+import { classify } from 'ember-string';
+import { invokeAction } from 'ember-invoke-action';
+import { task } from 'ember-concurrency';
+import errorMessages from 'client/utils/error-messages';
+import computed from 'ember-computed';
+
+export default Component.extend({
+  username: undefined,
+  password: undefined,
+  notify: service(),
+  session: service(),
+  store: service(),
+
+  token: computed('username', 'password', {
+    get() {
+      const username = get(this, 'username');
+      const password = get(this, 'password');
+      return btoa(`${username}:${password}`);
+    }
+  }),
+
+  createExport: task(function* () {
+    const exporter = get(this, 'store').createRecord('linked-account', {
+      externalUserId: get(this, 'username'),
+      token: get(this, 'token'),
+      type: 'MyAnimeList',
+
+      shareFrom: false,
+      shareTo: false,
+      syncTo: true,
+      user: get(this, 'session.account')
+    });
+    return yield exporter.save();
+  }).drop(),
+
+  init() {
+    this._super(...arguments);
+    set(this, 'siteName', get(this, 'componentData.siteName'));
+  },
+
+  actions: {
+    setupExport() {
+      get(this, 'createExport').perform()
+        .then(() => {
+          // NOTE: Probably a cleaner solution to add a "completed!" tab
+          invokeAction(this, 'close');
+        })
+        .catch(err => get(this, 'notify').error(errorMessages(err)));
+    }
+  }
+});
