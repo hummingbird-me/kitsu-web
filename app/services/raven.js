c583101e9f51f964e58220e3e3095fc47f4d664c
Thomas McNiven <hello@vevix.net>
2017-01-11 01:08:20 +1000

    always report to Sentry

--- app/services/raven.js
+++ app/services/raven.js
@@ -2,23 +2,10 @@ import RavenLogger from 'ember-cli-sentry/services/raven';
 import get from 'ember-metal/get';
 
 export default RavenLogger.extend({
-  errorsToIgnore: [
-    'TransitionAborted'
-  ],
+  errorsToIgnore: ['TransitionAborted'],
 
   ignoreError(reason) {
-    if (!this.shouldReportError()) {
-      return true;
-    }
     const { message } = reason;
     return get(this, 'errorsToIgnore').any(error => message.includes(error));
   },
-
-  /**
-   * Sentry recommends only reporting a small subset of the actual
-   * frontend errors. This can get *very* noisy otherwise.
-   */
-  shouldReportError() {
-    return (Math.random() * 100 <= 10);
-  }
 });
