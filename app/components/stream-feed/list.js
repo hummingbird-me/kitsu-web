f25a487b5bd80c9c1d63af585c14c8348479dae8
Thomas McNiven <hello@vevix.net>
2017-01-12 18:55:04 +1000

    Feed Filtering (#103)
    * support filtered feeds
    
    * filter realtime activity by active filter
    
    * remember the last filter used

--- app/components/stream-feed/list.js
+++ app/components/stream-feed/list.js
@@ -2,18 +2,19 @@ import Component from 'ember-component';
 import { task } from 'ember-concurrency';
 import get, { getProperties } from 'ember-metal/get';
 import set from 'ember-metal/set';
+import observer from 'ember-metal/observer';
 import service from 'ember-service/inject';
 import { isEmpty } from 'ember-utils';
 import { capitalize } from 'ember-string';
 import computed from 'ember-computed';
 import EmberObject from 'ember-object';
+import { storageFor } from 'ember-local-storage';
 import getter from 'client/utils/getter';
 import { modelType } from 'client/helpers/model-type';
 import errorMessages from 'client/utils/error-messages';
 import { prependObjects } from 'client/utils/array-utils';
 
 export default Component.extend({
-  filter: 'all',
   readOnly: false,
 
   ajax: service(),
@@ -24,6 +25,7 @@ export default Component.extend({
   store: service(),
   metrics: service(),
   streamRealtime: service(),
+  lastUsed: storageFor('last-used'),
 
   feedId: getter(function() {
     return `${get(this, 'streamType')}:${get(this, 'streamId')}`;
@@ -35,31 +37,8 @@ export default Component.extend({
     }
   }).readOnly(),
 
-  filteredFeed: computed('feed.[]', 'filter', {
-    get() {
-      const feed = get(this, 'feed');
-      if (feed === undefined) {
-        return [];
-      }
-      let result = feed;
-      const filter = get(this, 'filter');
-      if (filter === 'media') {
-        result = result.filter((group) => {
-          const [type] = get(group, 'activities.firstObject.foreignId').split(':');
-          return type === 'LibraryEntry' || type === 'Review';
-        });
-      } else if (filter === 'user') {
-        result = result.reject((group) => {
-          const [type] = get(group, 'activities.firstObject.foreignId').split(':');
-          return type === 'LibraryEntry' || type === 'Review';
-        });
-      }
-      return result;
-    }
-  }).readOnly(),
-
-  getFeedData: task(function* (type, id, limit = 10) {
-    return yield get(this, 'store').query('feed', {
+  getFeedData: task(function* (type, id, limit = 10, kind = null) {
+    const options = {
       type,
       id,
       include: [
@@ -74,7 +53,11 @@ export default Component.extend({
         'subject.library_entry'
       ].join(','),
       page: { limit }
-    });
+    };
+    if (!isEmpty(kind) && kind !== 'all') {
+      options.filter = { kind: kind === 'user' ? 'posts' : kind };
+    }
+    return yield get(this, 'store').query('feed', options);
   }).restartable(),
 
   createPost: task(function* (content, options) {
@@ -118,37 +101,53 @@ export default Component.extend({
     return yield get(this, 'ajax').delete(feedUrl);
   }).enqueue(),
 
+  handleFilter: observer('filter', function() {
+    this._getFeedData(10, get(this, 'filter'));
+  }),
+
+  init() {
+    this._super(...arguments);
+    set(this, 'filter', get(this, 'lastUsed.feedFilter') || 'all');
+  },
+
   didReceiveAttrs() {
     this._super(...arguments);
     get(this, 'headTags').collectHeadTags();
 
     // cancel any previous subscriptions
     this._cancelSubscription();
+    this._getFeedData(10, get(this, 'filter')).then((data) => {
+      if (isEmpty(data)) { return; }
+      // realtime
+      const { streamType, streamId } = getProperties(this, 'streamType', 'streamId');
+      const { readonlyToken } = get(data, 'meta');
+      const subscription = get(this, 'streamRealtime')
+        .subscribe(streamType, streamId, readonlyToken, object => this._handleRealtime(object));
+      set(this, 'subscription', subscription);
+    });
+  },
 
+  willDestroyElement() {
+    this._super(...arguments);
+    this._cancelSubscription();
+  },
+
+  _getFeedData(limit = 10, kind = null) {
     const { streamType, streamId } = getProperties(this, 'streamType', 'streamId');
     if (isEmpty(streamType) || isEmpty(streamId)) {
       return;
     }
     set(this, 'feed', []);
     set(this, 'newItems', EmberObject.create({ length: 0, cache: [] }));
-    get(this, 'getFeedData').perform(streamType, streamId).then((data) => {
+    return get(this, 'getFeedData').perform(streamType, streamId, limit, kind).then((data) => {
       get(this, 'feed').addObjects(data);
       set(this, 'feed.links', get(data, 'links'));
 
-      // realtime
-      const { readonlyToken } = get(data, 'meta');
-      const subscription = get(this, 'streamRealtime').subscribe(streamType, streamId, readonlyToken,
-        object => this._handleRealtime(object));
-      set(this, 'subscription', subscription);
-
       // stream analytics
       this._trackImpressions(data);
-    }).catch(() => {});
-  },
 
-  willDestroyElement() {
-    this._super(...arguments);
-    this._cancelSubscription();
+      return data;
+    }).catch(() => {});
   },
 
   /**
@@ -187,24 +186,31 @@ export default Component.extend({
 
   _handleRealtime(object) {
     const groupCache = get(this, 'newItems.cache');
+    const filter = get(this, 'filter');
 
     get(this, 'newItems').beginPropertyChanges();
     get(object, 'new').forEach((activity) => {
       const type = get(activity, 'foreign_id').split(':')[0];
-      if (type === 'Post' || type === 'Comment') {
-        // look for unknown post at first object by session user
-        if (get(activity, 'actor').split(':')[1] === get(this, 'session.account.id')) {
-          if (type === 'post') {
-            const top = get(this, 'feed.firstObject.activities.firstObject');
-            if (get(top, 'foreignId') === 'Post:<unknown>' ||
-              get(top, 'foreignId') === get(activity, 'foreign_id')) {
-              return;
-            }
-          } else {
-            return; // Don't show new activity and bump post for own users comments
-          }
+
+      // filter out content not apart of the current filter
+      if (filter === 'media') {
+        if (type === 'Post' || type === 'Comment') {
+          return;
+        }
+      } else if (filter === 'user') {
+        if (type !== 'Post' && type !== 'Comment') {
+          return;
         }
       }
+
+      // don't show a new activity action if the actor is the sessioned user
+      if (type === 'Post' || type === 'Comment') {
+        if (get(activity, 'actor').split(':')[1] === get(this, 'session.account.id')) {
+          return;
+        }
+      }
+
+      // add to new activities cache
       if (groupCache.indexOf(get(activity, 'group')) === -1) {
         set(this, 'newItems.length', get(this, 'newItems.length') + 1);
         groupCache.addObject(get(activity, 'group'));
@@ -239,6 +245,11 @@ export default Component.extend({
 
     removeGroup(group) {
       get(this, 'feed').removeObject(group);
+    },
+
+    updateFilter(option) {
+      set(this, 'filter', option);
+      set(this, 'lastUsed.feedFilter', option);
     },
 
     /**
