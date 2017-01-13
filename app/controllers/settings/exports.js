ad1130ee94c198e255970397f39ce9540f3f43f4
Sven Gehring <sven@kitsu.io>
2017-01-13 16:14:09 +0100

    Fix STI type; add remove button; fix pagination

--- app/controllers/settings/exports.js
+++ app/controllers/settings/exports.js
@@ -1,3 +1,9 @@
 import Controller from 'ember-controller';
 
-export default Controller.extend();
+export default Controller.extend({
+  actions: {
+    removeExport(exporter) {
+      exporter.destroyRecord();
+    }
+  }
+});
