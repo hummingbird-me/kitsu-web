5de79ed3f934a2c499248906cffa538da2beca34
Thomas McNiven <hello@vevix.net>
2017-01-14 17:34:26 +1000

    Hookup the networked trending section

--- app/components/trending/trending-banner.js
+++ app/components/trending/trending-banner.js
@@ -1,9 +1,34 @@
 import Component from 'ember-component';
 import get from 'ember-metal/get';
+import set from 'ember-metal/set';
 import service from 'ember-service/inject';
-import { task, timeout } from 'ember-concurrency';
+import { task } from 'ember-concurrency';
 
 export default Component.extend({
   tagName: 'section',
   classNames: ['trending-banner'],
+  ajax: service(),
+  store: service(),
+
+  getTrendingMedia: task(function* () {
+    return yield get(this, 'ajax').request('/trending/anime?in_network=true').then((response) => {
+      // push into store since this is a regular AJAX request
+      const records = [];
+      response.data.forEach((data) => {
+        const normalize = get(this, 'store').normalize('anime', data);
+        const record = get(this, 'store').push(normalize);
+        records.addObject(record);
+      });
+      // TODO: Remove when server API limit works
+      return records.slice(0, 8);
+    }).catch((error) => {
+      get(this, 'raven').logException(error);
+    });
+  }).drop(),
+
+  init() {
+    this._super(...arguments);
+    const taskInstance = get(this, 'getTrendingMedia').perform();
+    set(this, 'taskInstance', taskInstance);
+  }
 });
---
+++ app/components/trending/trending-banner/participants.js
@@ -0,0 +1,28 @@
+import Component from 'ember-component';
+import get from 'ember-metal/get';
+import set from 'ember-metal/set';
+import service from 'ember-service/inject';
+import { task } from 'ember-concurrency';
+
+export default Component.extend({
+  session: service(),
+  store: service(),
+
+  getParticipants: task(function* () {
+    const type = get(this, 'itemType');
+    return yield get(this, 'store').query('library-entry', {
+      filter: {
+        [`${type}_id`]: get(this, 'item.id'),
+        following: get(this, 'session.account.id')
+      },
+      include: 'user',
+      page: { limit: 5 }
+    });
+  }).drop(),
+
+  init() {
+    this._super(...arguments);
+    const taskInstance = get(this, 'getParticipants').perform();
+    set(this, 'taskInstance', taskInstance);
+  },
+});
