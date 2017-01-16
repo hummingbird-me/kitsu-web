90652e9d3ffa8c04d190b832c13d02569755e969
Josh <contact@joshfabian.com>
2017-01-16 15:04:36 -0800

    Add static people page

--- app/router.js
+++ app/router.js
@@ -80,6 +80,8 @@ RouterInstance.map(function() {
   // These must remain at the bottom of the RouterInstance map
   this.route('server-error', { path: '/500' });
   this.route('not-found', { path: '/*path' });
+  this.route('people');
+  this.route('characters');
 });
 
 export default RouterInstance;
