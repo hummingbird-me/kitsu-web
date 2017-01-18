d124ce80dcc5073d4dc2ab2d23cd766dc05ca773
Sven Gehring <sven@kitsu.io>
2017-01-18 10:05:25 +0100

    Hide export options if they are in use

--- app/components/application/auth-onboarding/export-select.js
+++ app/components/application/auth-onboarding/export-select.js
@@ -1,8 +1,21 @@
 import Component from 'ember-component';
 import get from 'ember-metal/get';
 import { invokeAction } from 'ember-invoke-action';
+import service from 'ember-service/inject';
+import getter from 'client/utils/getter';
 
 export default Component.extend({
+  store: service(),
+
+  hasExporterForMal: getter(function() {
+    return this.hasExporterFor('MyAnimeList');
+  }),
+
+  hasExporterFor(platform) {
+    const exporter = get(this, 'store').peekAll('linked-account').filterBy('kind', `LinkedAccount::${platform}`);
+    return get(exporter, 'length') !== 0;
+  },
+
   actions: {
     changeComponent(component, siteName) {
       invokeAction(this, 'changeComponent', component, { ...get(this, 'componentData'), siteName });
