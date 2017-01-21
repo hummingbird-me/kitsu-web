a854c0a858aec7484a3bb95b76ca17348c340dd9
Thomas McNiven <hello@vevix.net>
2017-01-21 12:50:51 +1000

    Check that the user exists on the post

--- app/components/stream-feed/items/post.js
+++ app/components/stream-feed/items/post.js
@@ -100,7 +100,7 @@ export default Component.extend(ClipboardMixin, {
       set(this, 'userId', get(this, 'feedId').split(':')[1]);
     }
     const post = get(this, 'post');
-    if (post && get(this, 'session').isCurrentUser(get(post, 'user'))) {
+    if (get(post, 'user') && get(this, 'session').isCurrentUser(get(post, 'user'))) {
       set(this, 'isHidden', get(post, 'nsfw'));
     } else {
       set(this, 'isHidden', get(post, 'nsfw') || get(post, 'spoiler'));
