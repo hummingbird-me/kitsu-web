fd67c7f578e85eaec328637e5ebf57378a31363f
Thomas McNiven <hello@vevix.net>
2017-01-22 15:42:46 +1000

    compute on mediaRelationships rather than a getter

--- app/components/media/media-relationships.js
+++ app/components/media/media-relationships.js
@@ -1,25 +1,27 @@
 import Component from 'ember-component';
 import get from 'ember-metal/get';
-import getter from 'client/utils/getter';
+import computed from 'ember-computed';
 
 export default Component.extend({
   tagName: 'section',
   classNames: ['media--related'],
 
-  relationships: getter(function() {
-    const results = {};
-    const mediaRelationships = get(this, 'media.mediaRelationships');
-    mediaRelationships.forEach((relationship) => {
-      const role = get(relationship, 'role');
-      results[role] = results[role] || [];
-      results[role].addObject(get(relationship, 'destination'));
-    });
-    // sort the results by key
-    const sortedKeys = Object.keys(results).sort();
-    const sortedResults = {};
-    sortedKeys.forEach((key) => {
-      sortedResults[key] = get(results, key);
-    });
-    return sortedResults;
-  })
+  relationships: computed('media.mediaRelationships', {
+    get() {
+      const results = {};
+      const mediaRelationships = get(this, 'media.mediaRelationships');
+      mediaRelationships.forEach((relationship) => {
+        const role = get(relationship, 'role');
+        results[role] = results[role] || [];
+        results[role].addObject(get(relationship, 'destination'));
+      });
+      // sort the results by key
+      const sortedKeys = Object.keys(results).sort();
+      const sortedResults = {};
+      sortedKeys.forEach((key) => {
+        sortedResults[key] = get(results, key);
+      });
+      return sortedResults;
+    }
+  }).readOnly()
 });
