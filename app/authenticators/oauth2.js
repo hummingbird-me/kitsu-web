90bb877f593ab3700ce61c0dbee9f30539ded709
Thomas McNiven <hello@vevix.net>
2017-01-26 15:59:49 +1000

    Support APIHost for ember-simple-auth endpoints

--- app/authenticators/oauth2.js
+++ app/authenticators/oauth2.js
@@ -1,7 +1,9 @@
 import OAuth2PasswordGrant from 'ember-simple-auth/authenticators/oauth2-password-grant';
+import getter from 'client/utils/getter';
+import config from 'client/config/environment';
 
 export default OAuth2PasswordGrant.extend({
   refreshAccessTokens: true,
-  serverTokenEndpoint: '/api/oauth/token',
-  serverTokenRevocationEndpoint: '/api/oauth/revoke'
+  serverTokenEndpoint: getter(() => `${config.APP.APIHost}/api/oauth/token`),
+  serverTokenRevocationEndpoint: getter(() => `${config.APP.APIHost}/api/oauth/revoke`)
 });
