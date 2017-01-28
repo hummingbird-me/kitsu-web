32af1e70ad4c055ceb6cc76f8e6d044f5e1bbe76
Thomas McNiven <hello@vevix.net>
2017-01-28 19:43:48 +1000

    Check that emojione is defined

--- app/helpers/emojify.js
+++ app/helpers/emojify.js
@@ -3,7 +3,7 @@ import { htmlSafe } from 'ember-string';
 /* global emojione */
 
 export function emojify([str]) {
-  const emojified = emojione.toImage(str);
+  const emojified = emojione ? emojione.toImage(str) : str;
   return htmlSafe(emojified);
 }
 
