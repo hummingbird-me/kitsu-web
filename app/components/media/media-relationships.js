a76c89a7f203a10c9091e80f84807c4d783e15a9
Thomas McNiven <hello@vevix.net>
2017-01-30 14:33:08 +1000

    Sort media relationships by their start date

--- app/components/media/media-relationships.js
+++ app/components/media/media-relationships.js
@@ -6,27 +6,40 @@ export default Component.extend({
   tagName: 'section',
   classNames: ['media--related'],
 
-  relationships: computed('media.mediaRelationships', {
-    get() {
-      const results = {};
-      const mediaRelationships = get(this, 'media.mediaRelationships');
-      mediaRelationships.forEach((relationship) => {
-        const role = get(relationship, 'role');
-        results[role] = results[role] || [];
-        results[role].addObject(get(relationship, 'destination'));
-      });
-      // sort the results by key
-      const sortedKeys = Object.keys(results).sort();
-      const sortedResults = {};
-      sortedKeys.forEach((key) => {
-        sortedResults[key] = get(results, key);
-      });
-      return sortedResults;
-    }
+  relationships: computed('media.mediaRelationships', function() {
+    const results = {};
+    const mediaRelationships = get(this, 'media.mediaRelationships');
+    mediaRelationships.forEach((relationship) => {
+      const role = get(relationship, 'role');
+      results[role] = results[role] || [];
+      results[role].addObject(get(relationship, 'destination'));
+    });
+    // sort the results by key
+    return this._sortResults(results);
   }).readOnly(),
 
   didInsertElement() {
     this._super(...arguments);
     this.$('[data-toggle="tooltip"]').tooltip();
+  },
+
+  _sortResults(results) {
+    const sortedKeys = Object.keys(results).sort();
+    const sortedResults = {};
+    sortedKeys.forEach((key) => {
+      sortedResults[key] = get(results, key).sort((a, b) => {
+        const aStart = get(a, 'startDate');
+        const bStart = get(b, 'startDate');
+        if (aStart && bStart) {
+          if (aStart.isBefore(bStart)) {
+            return -1;
+          } else if (aStart.isAfter(bStart)) {
+            return 1;
+          }
+        }
+        return 0;
+      });
+    });
+    return sortedResults;
   }
 });
