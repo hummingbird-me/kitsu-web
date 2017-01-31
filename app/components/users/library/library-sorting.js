76aa5ec2c8ef3088aa07f057ed2a9a5c9d00c812
Thomas McNiven <hello@vevix.net>
2017-02-01 01:26:20 +1000

    Support specific sort direction as the initial sort option on library

--- app/components/users/library/library-sorting.js
+++ app/components/users/library/library-sorting.js
@@ -8,6 +8,7 @@ export default Component.extend({
     this._super(...arguments);
     this._resetActiveSort();
     const sort = get(this, 'sort');
+    // We may already have a sort passed in
     if (sort !== undefined) {
       const type = sort.charAt(0) === '-' ? sort.slice(1) : sort;
       const direction = sort.charAt(0) === '-' ? 'desc' : 'asc';
@@ -15,17 +16,23 @@ export default Component.extend({
     }
   },
 
-  _updateActiveSort(type) {
+  /**
+   * Update the sorting direction of the active sort.
+   */
+  _updateActiveSort(type, defaultSort) {
     const active = get(this, 'active');
+    // Switching the sort of an already active sort option
     if (get(active, 'type') === type) {
-      // same type, toggle direction
-      if (get(active, 'direction') === 'asc') {
-        set(active, 'direction', 'desc');
-      } else if (get(active, 'direction') === 'desc') {
+      const direction = get(active, 'direction');
+      const newDirection = this._getOppositeSort(direction);
+      if (defaultSort === newDirection || (!defaultSort && direction === 'desc')) {
         this._resetActiveSort();
+      } else {
+        set(active, 'direction', newDirection);
       }
     } else {
-      set(this, 'active', { type, direction: 'asc' });
+      // Initial sort option, can be specified for a type or defaults to asc.
+      set(this, 'active', { type, direction: defaultSort || 'asc' });
     }
   },
 
@@ -33,9 +40,13 @@ export default Component.extend({
     set(this, 'active', {});
   },
 
+  _getOppositeSort(sort) {
+    return sort === 'asc' ? 'desc' : 'asc';
+  },
+
   actions: {
-    sort(type) {
-      this._updateActiveSort(type);
+    sort(type, defaultSort) {
+      this._updateActiveSort(type, defaultSort);
       invokeAction(this, 'onSortChange', get(this, 'active'));
     }
   }
