6b34ea9a4cd0016257102a5db09d870541c49d4a
Thomas McNiven <hello@vevix.net>
2017-02-05 16:48:25 +1000

    move FlickityActions to action handlers and set events via ember-flickity

--- app/mixins/flickity-actions.js
+++ app/mixins/flickity-actions.js
@@ -1,7 +1,6 @@
 import Mixin from 'ember-metal/mixin';
 import set from 'ember-metal/set';
 import { later } from 'ember-runloop';
-import canUseDOM from 'client/utils/can-use-dom';
 
 /**
  * Enables the `ember-href-to` `data-href-to-ignore` property if Flickity is in
@@ -9,26 +8,6 @@ import canUseDOM from 'client/utils/can-use-dom';
  */
 export default Mixin.create({
   isFlickityDragging: false,
-
-  didRender() {
-    this._super(...arguments);
-    if (canUseDOM) {
-      this._cleanEvents();
-      this._setupEvents();
-    }
-  },
-
-  _cleanEvents() {
-    this.$('.flickity-enabled')
-      .off('dragStart.flickity')
-      .off('dragEnd.flickity');
-  },
-
-  _setupEvents() {
-    this.$('.flickity-enabled')
-      .on('dragStart.flickity', this._onDragStart())
-      .on('dragEnd.flickity', this._onDragEnd());
-  },
 
   _onDragStart() {
     set(this, 'isFlickityDragging', true);
@@ -41,5 +20,15 @@ export default Mixin.create({
       set(this, 'isFlickityDragging', false);
       this.$('a[href^="/"]').removeAttr('data-href-to-ignore');
     }, 250);
+  },
+
+  actions: {
+    flickityOnDragStart() {
+      this._onDragStart();
+    },
+
+    flickityOnDragEnd() {
+      this._onDragEnd();
+    }
   }
 });
