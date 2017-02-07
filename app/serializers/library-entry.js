36b279bab5000efee7a72a5aecc2a921a5a3b2a6
Thomas McNiven <hello@vevix.net>
2017-02-07 21:08:19 +1000

    call _super for serializeAttribute on library-entry

--- app/serializers/library-entry.js
+++ app/serializers/library-entry.js
@@ -8,14 +8,13 @@ export default ApplicationSerializer.extend({
   },
 
   serializeAttribute(snapshot, json, key) {
+    this._super(...arguments);
     // If progress is changing and the status isn't current -- then we want to update the
     // status
     if (key === 'status' && snapshot.attr(key) !== 'current') {
       if ('progress' in snapshot.changedAttributes()) {
         json.attributes[key] = 'current'; // eslint-disable-line no-param-reassign
-        return;
       }
     }
-    return this._super(...arguments);
   }
 });
