e0706f98f37e8504a1bc461fcb1c73d5bfb5f789
Thomas McNiven <hello@vevix.net>
2017-02-09 00:13:13 +1000

    Only enable ads in production

--- config/environment.js
+++ config/environment.js
@@ -11,7 +11,7 @@ module.exports = function(environment) {
       FEATURES: {}
     },
     APP: {
-      APIHost: 'https://kitsu.io',
+      APIHost: undefined,
       isStaging,
     },
     EXTEND_PROTOTYPES: {
@@ -124,7 +124,7 @@ module.exports = function(environment) {
     google: {
       adwords: environment === 'production',
       ads: {
-        enabled: true,
+        enabled: environment === 'production',
         client: 'ca-pub-1730996169473196',
         pageads: true
       }
