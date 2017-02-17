2eec23583ff1e2f65bb4caf0479790a257bbf624
Thomas McNiven <hello@vevix.net>
2017-02-17 12:16:05 +1000

    StreamAnalytics is already included via npm

--- app/metrics-adapters/stream.js
+++ app/metrics-adapters/stream.js
@@ -2,7 +2,6 @@ import BaseAdapter from 'ember-metrics/metrics-adapters/base';
 import get from 'ember-metal/get';
 import set from 'ember-metal/set';
 import service from 'ember-service/inject';
-import jQuery from 'jquery';
 import Config from 'client/config/environment';
 import canUseDOM from 'client/utils/can-use-dom';
 
@@ -17,8 +16,6 @@ export default BaseAdapter.extend({
     const environment = Config.APP.isStaging ? 'staging' : Config.environment;
     const config = get(this, `config.${environment}`);
     if (canUseDOM) {
-      // eslint-disable-next-line
-      !function(t,e){t("StreamAnalytics","https://d2j1fszo1axgmp.cloudfront.net/2.6.0/stream-analytics.min.js",e)}(function(t,e,n){var s,i,r;n["_"+t]={},n[t]=function(e){n["_"+t].clients=n["_"+t].clients||{},n["_"+t].clients[e.apiKey]=this,this._config=e};var c=function(t){return function(){return this["_"+t]=this["_"+t]||[],this["_"+t].push(arguments),this}};s=["setUser","trackImpression","trackEngagement"];for(var a=0;a<s.length;a++){var o=s[a];n[t].prototype[o]=c(o)}i=document.createElement("script"),i.async=!0,i.src=e,r=document.getElementsByTagName("script")[0],r.parentNode.insertBefore(i,r)},window);
       const client = new window.StreamAnalytics(config);
       set(this, 'client', client);
     }
@@ -50,12 +47,6 @@ export default BaseAdapter.extend({
         location: get(this, 'router.currentRouteName')
       }, data);
       get(this, 'client').trackEngagement(options);
-    }
-  },
-
-  willDestroy() {
-    if (canUseDOM) {
-      jQuery('script[src*="stream-analytics"]').remove();
     }
   }
 });
