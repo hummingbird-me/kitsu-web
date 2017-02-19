5ad62bdda4cae4f7af9349f83a874e8382d3dfaf
Thomas McNiven <hello@vevix.net>
2017-02-19 17:01:15 +1000

    Remove create-link helper

--- tests/unit/helpers/create-link-test.js
+++
@@ -1,9 +0,0 @@
-import { createLink } from 'client/helpers/create-link';
-import { module, test } from 'qunit';
-
-module('Unit | Helper | create link');
-
-test('it returns a HTML link element with the passed in data', function(assert) {
-  const result = createLink('/this/route', 'Hello, World');
-  assert.equal(result.string, '<a href="/this/route">Hello, World</a>');
-});
