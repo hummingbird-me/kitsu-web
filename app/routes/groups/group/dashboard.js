b9e652b69be1d1a038306236fa499f8f421bcd65
Thomas McNiven <hello@vevix.net>
2017-02-26 01:44:14 +1000

    Setup dashboard routes and redirects

---
+++ app/routes/groups/group/dashboard.js
@@ -0,0 +1,45 @@
+import Route from 'ember-route';
+import get from 'ember-metal/get';
+import { CanMixin } from 'ember-can';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import RSVP from 'rsvp';
+import jQuery from 'jquery';
+
+export default Route.extend(AuthenticatedRouteMixin, CanMixin, {
+  authenticationRoute: 'dashboard',
+
+  model() {
+    return RSVP.hash({
+      group: this.modelFor('groups.group'),
+      membership: this._getMembershipStatus()
+    });
+  },
+
+  afterModel(model) {
+    if (!this.can('view dashboard for group', get(model, 'membership'))) {
+      this.transitionTo('groups.group.group-page.index');
+    }
+  },
+
+  activate() {
+    this._super(...arguments);
+    jQuery('body').addClass('settings-page');
+  },
+
+  deactivate() {
+    this._super(...arguments);
+    jQuery('body').removeClass('settings-page');
+  },
+
+  _getMembershipStatus() {
+    if (!get(this, 'session.hasUser')) { return RSVP.resolve(); }
+    const group = this.modelFor('groups.group');
+    return get(this, 'store').query('group-member', {
+      include: 'user,permissions',
+      filter: {
+        group: get(group, 'id'),
+        user: get(this, 'session.account.id')
+      }
+    }).then(records => get(records, 'firstObject'));
+  }
+});
---
+++ app/routes/groups/group/dashboard/index.js
@@ -0,0 +1,16 @@
+import Route from 'ember-route';
+import get from 'ember-metal/get';
+import service from 'ember-service/inject';
+
+export default Route.extend({
+  intl: service(),
+
+  model() {
+    return this.modelFor('groups.group.dashboard');
+  },
+
+  titleToken(model) {
+    const group = get(model, 'name');
+    return get(this, 'intl').t('titles.groups.group.dashboard.index', { group });
+  }
+});
