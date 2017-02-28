e240ffadaed3d7dfec48db5f0db0aece43fad3b7
Thomas McNiven <hello@vevix.net>
2017-02-28 12:26:33 +1000

    Add {leaders,neighbors}Count to group model

--- app/serializers/group.js
+++ app/serializers/group.js
@@ -3,7 +3,9 @@ import ApplicationSerializer from 'client/serializers/application';
 export default ApplicationSerializer.extend({
   attrs: {
     rulesFormatted: { serialize: false },
+    leadersCount: { serialize: false },
     membersCount: { serialize: false },
+    neighborsCount: { serialize: false },
     slug: { serialize: false }
   }
 });
