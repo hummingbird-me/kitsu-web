10a751dc4de31ad10787e83951617a88925be43a
Thomas McNiven <hello@vevix.net>
2017-03-04 03:32:54 +1000

    Hookup latest reports & tickets on dashboard

--- app/models/group-ticket.js
+++ app/models/group-ticket.js
@@ -5,10 +5,11 @@ import { belongsTo, hasMany } from 'ember-data/relationships';
 export default Base.extend({
   createdAt: attr('utc'),
   status: attr('string'),
+  title: attr('string'),
 
   assignee: belongsTo('user'),
   group: belongsTo('group'),
-  sender: belongsTo('user'),
+  user: belongsTo('user'),
 
   messages: hasMany('group-ticket-message', { inverse: 'ticket' })
 });
