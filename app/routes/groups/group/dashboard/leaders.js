5dfa9760ffa7c9228c2a42c3519a412e0832a6b5
Thomas McNiven <hello@vevix.net>
2017-03-05 04:19:11 +1000

    Redirect at the route level for group dashboard for lack of permissions

--- app/routes/groups/group/dashboard/leaders.js
+++ app/routes/groups/group/dashboard/leaders.js
@@ -1,9 +1,18 @@
 import Route from 'ember-route';
 import get from 'ember-metal/get';
 import service from 'ember-service/inject';
+import { CanMixin } from 'ember-can';
 
-export default Route.extend({
+export default Route.extend(CanMixin, {
   intl: service(),
+
+  beforeModel() {
+    const model = this.modelFor('groups.group.dashboard');
+    const membership = get(model, 'membership');
+    if (!this.can('manage leaders for group', { membership })) {
+      return this.transitionTo('groups.group.dashboard.index');
+    }
+  },
 
   model() {
     return this.modelFor('groups.group.dashboard');
