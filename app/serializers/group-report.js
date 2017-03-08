48ff9cab307fcfba28e2efd847804bdb8673e39a
Thomas McNiven <hello@vevix.net>
2017-03-09 09:05:43 +1000

    Hookup group reports

---
+++ app/serializers/group-report.js
@@ -0,0 +1,15 @@
+import ApplicationSerializer from 'client/serializers/application';
+import { isPresent } from 'ember-utils';
+
+export default ApplicationSerializer.extend({
+  attrs: {
+    createdAt: { serialize: false }
+  },
+
+  serializeBelongsTo(snapshot, json, relationship) {
+    const belongsTo = snapshot.belongsTo(relationship.key);
+    if (isPresent(belongsTo)) {
+      this._super(...arguments);
+    }
+  }
+});
