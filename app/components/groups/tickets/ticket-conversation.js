1e0f7e07e29a1df54c27785010844076ef91799f
Thomas McNiven <hello@vevix.net>
2017-03-10 00:29:37 +1000

    Cleanup ticket conversations and scroll to bottom

--- app/components/groups/tickets/ticket-conversation.js
+++ app/components/groups/tickets/ticket-conversation.js
@@ -2,14 +2,27 @@ import Component from 'ember-component';
 import get from 'ember-metal/get';
 import set from 'ember-metal/set';
 import service from 'ember-service/inject';
+import { isEmpty } from 'ember-utils';
+import { scheduleOnce } from 'ember-runloop';
 import { task } from 'ember-concurrency';
+/* global autosize */
 
 export default Component.extend({
   activeTab: 'message',
-  showNav: true,
   store: service(),
 
+  didInsertElement() {
+    this._super(...arguments);
+    this.$('.modal').on('shown.bs.modal', () => {
+      this._scrollToBottom();
+      this.$('.modal').off('shown.bs.modal');
+    });
+  },
+
   postMessageTask: task(function* () {
+    const content = get(this, 'replyContent');
+    if (isEmpty(content)) { return; }
+
     // if the ticket is new then save that first
     if (get(this, 'isNewTicket')) {
       yield get(this, 'ticket').save();
@@ -18,13 +31,18 @@ export default Component.extend({
     const message = get(this, 'store').createRecord('group-ticket-message', {
       ticket: get(this, 'ticket'),
       user: get(this, 'session.account'),
-      content: get(this, 'replyContent'),
+      content,
       kind
     });
     yield message.save().then(() => {
       if (get(this, 'isNewTicket')) {
         this.$('.modal').modal('hide');
       }
+      set(this, 'replyContent', '');
+      scheduleOnce('afterRender', () => {
+        this._scrollToBottom();
+        autosize.update(get(this, 'element').getElementsByClassName('ticket-input'));
+      });
     }).catch(() => {});
   }),
 
@@ -43,5 +61,12 @@ export default Component.extend({
       if (get(this, 'updateStatusTask.isRunning')) { return; }
       get(this, 'updateStatusTask').perform('closed');
     }
+  },
+
+  _scrollToBottom() {
+    const [element] = get(this, 'element').getElementsByClassName('ticket-history');
+    this.$(element).animate({
+      scrollTop: (element.scrollHeight - element.clientHeight)
+    }, 1000);
   }
 });
