2fb13a85e9234e3738fda9e1287530d00126ecac
Thomas McNiven <hello@vevix.net>
2017-03-11 15:49:26 +1000

    Mark a group as read when visiting the page

--- app/routes/groups/group/group-page.js
+++ app/routes/groups/group/group-page.js
@@ -1,14 +1,25 @@
 import Route from 'ember-route';
 import get from 'ember-metal/get';
+import service from 'ember-service/inject';
 import CoverPageMixin from 'client/mixins/routes/cover-page';
 import RSVP from 'rsvp';
 
 export default Route.extend(CoverPageMixin, {
+  ajax: service(),
+
   model() {
     return RSVP.hash({
       group: this.modelFor('groups.group'),
       membership: this._getMembershipStatus()
     });
+  },
+
+  afterModel(model) {
+    this._super(...arguments);
+    if (get(model, 'membership')) {
+      const id = get(model, 'group.id');
+      get(this, 'ajax').post(`/groups/${id}/_read`).catch(() => {});
+    }
   },
 
   _getMembershipStatus() {
