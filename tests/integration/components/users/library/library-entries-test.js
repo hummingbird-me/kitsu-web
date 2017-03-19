f94ba4ccf38eefb7316a7f984ebfec7d77e49622
matthewdias <matthewdias@me.com>
2017-03-19 02:21:28 -0500

    hopefully fix tests this time

--- tests/integration/components/users/library/library-entries-test.js
+++ tests/integration/components/users/library/library-entries-test.js
@@ -1,5 +1,6 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import moment from 'moment';
 
 moduleForComponent('users/library/library-entries', 'Integration | Component | users/library/library entries', {
   integration: true,
@@ -12,7 +13,14 @@ moduleForComponent('users/library/library-entries', 'Integration | Component | u
 test('it renders', function(assert) {
   assert.expect(2);
   this.set('entries', [{
-    media: { episodeCount: 13, episodeLength: 24, posterImage: 'pi', subtype: 'TV', airingStatus: 'current' },
+    media: {
+      episodeCount: 13,
+      episodeLength: 24,
+      posterImage: 'pi',
+      subtype: 'TV',
+      startDate: moment.utc('2008-01-08T00:00:00.000Z'),
+      endDate: moment.utc('2008-03-25T00:00:00.000Z')
+    }
   }]);
 
   this.render(hbs`{{users/library/library-entries
--- tests/integration/components/users/library/library-entry-test.js
+++ tests/integration/components/users/library/library-entry-test.js
@@ -1,5 +1,6 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import moment from 'moment';
 
 moduleForComponent('users/library/library-entry', 'Integration | Component | users/library/library entry', {
   integration: true,
@@ -12,7 +13,13 @@ moduleForComponent('users/library/library-entry', 'Integration | Component | use
 test('it renders', function(assert) {
   assert.expect(2);
   this.set('entry', {
-    media: { computedTitle: 'Spice and Wolf', posterImage: 'pi', subtype: 'TV', airingStatus: 'current' }
+    media: {
+      computedTitle: 'Spice and Wolf',
+      posterImage: 'pi',
+      subtype: 'TV',
+      startDate: moment.utc('2008-01-08T00:00:00.000Z'),
+      endDate: moment.utc('2008-03-25T00:00:00.000Z')
+    }
   });
   this.render(hbs`{{users/library/library-entry entry=entry}}`);
   assert.ok(this.$('[data-test-selector="library-entry"]').length);
