4d9ab84fea74cc58e954877767b57cb2fc949ebd
matthewdias <matthewdias@me.com>
2017-03-21 10:09:25 -0500

    ui components folder

--- app/components/ui/selectable-input.js
+++ app/components/ui/selectable-input.js
@@ -2,7 +2,8 @@ import OneWayNumber from 'ember-one-way-controls/components/one-way-number';
 import get from 'ember-metal/get';
 
 export default OneWayNumber.extend({
-  click() {
+  click(event) {
+    alert(event.target);
     get(this, 'element').select();
   }
 });
