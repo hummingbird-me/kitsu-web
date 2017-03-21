adb25a876629d03bf6f3b776c1599397124defd0
Matthew Dias <matthewdias@me.com>
2017-03-21 13:36:01 -0500

    remove alert

--- app/components/ui/selectable-input.js
+++ app/components/ui/selectable-input.js
@@ -2,8 +2,7 @@ import OneWayNumber from 'ember-one-way-controls/components/one-way-number';
 import get from 'ember-metal/get';
 
 export default OneWayNumber.extend({
-  click(event) {
-    alert(event.target);
+  click() {
     get(this, 'element').select();
   }
 });
