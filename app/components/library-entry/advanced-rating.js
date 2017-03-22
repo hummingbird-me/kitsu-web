9593f194457b0bdd8a6766f80374fdfc9c9837b3
Thomas McNiven <hello@vevix.net>
2017-03-22 22:59:16 +1000

    Implement coloring for advanced slider

--- app/components/library-entry/advanced-rating.js
+++ app/components/library-entry/advanced-rating.js
@@ -1,13 +1,46 @@
 import Component from 'ember-component';
+import get from 'ember-metal/get';
 import { invokeAction } from 'ember-invoke-action';
+import canUseDOM from 'client/utils/can-use-dom';
 
 export default Component.extend({
   tagName: '',
   rating: 1,
 
+  didInsertElement() {
+    this._super(...arguments);
+    if (!canUseDOM) { return; }
+    this._createHandleDOM();
+    this._updateHandle(get(this, 'rating'));
+  },
+
   actions: {
+    onSlide([rating]) {
+      this._updateHandle(rating);
+    },
+
     onClick([rating]) {
       invokeAction(this, 'onClick', rating);
     }
+  },
+
+  _createHandleDOM() {
+    const [slider] = document.getElementsByClassName('advanced-rating-slider');
+    const [element] = slider.getElementsByClassName('noUi-handle');
+    const handleDot = document.createElement('div');
+    handleDot.classList.add('noUi-handle-dot');
+    element.appendChild(handleDot);
+    const handleBackground = document.createElement('span');
+    handleBackground.classList.add('noUi-handle-background');
+    handleBackground.style.width = `${slider.clientWidth}px`;
+    handleDot.appendChild(handleBackground);
+  },
+
+  _updateHandle(rating) {
+    const value = ((rating - 1) * 10);
+    const [element] = document.getElementsByClassName('advanced-rating-slider');
+    const offset = -((value / 100) * element.clientWidth);
+    const [handle] = element.getElementsByClassName('noUi-handle-background');
+    handle.style.left = `${offset}px`;
   }
 });
