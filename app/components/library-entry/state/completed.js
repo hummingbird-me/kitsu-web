f3fd6a498fb5d23a34703fc8566dca25361aaeb5
Thomas McNiven <hello@vevix.net>
2017-03-22 21:35:08 +1000

    Allow rating hover to be closed by clicking elsewhere

--- app/components/library-entry/state/completed.js
+++ app/components/library-entry/state/completed.js
@@ -2,7 +2,9 @@ import Component from 'ember-component';
 import get from 'ember-metal/get';
 import set from 'ember-metal/set';
 import computed from 'ember-computed';
+import { scheduleOnce } from 'ember-runloop';
 import { invokeAction } from 'ember-invoke-action';
+import jQuery from 'jquery';
 
 export default Component.extend({
   isChangingRating: false,
@@ -13,8 +15,24 @@ export default Component.extend({
   }).readOnly(),
 
   actions: {
+    showRating() {
+      set(this, 'isChangingRating', true);
+      // watch for clicks outside the tether element for closure
+      scheduleOnce('afterRender', () => {
+        jQuery(document.body).on('click.library-state-rating', ({ target }) => {
+          const id = '#library-state-completed-rating';
+          const isChild = jQuery(target).is(`${id} *, ${id}`);
+          if (!isChild) {
+            set(this, 'isChangingRating', false);
+            jQuery(document.body).off('click.library-state-rating');
+          }
+        });
+      });
+    },
+
     ratingSelected(rating) {
       set(this, 'isChangingRating', false);
+      jQuery(document.body).off('click.library-state-rating');
       invokeAction(this, 'onRatingChange', rating);
     }
   },
