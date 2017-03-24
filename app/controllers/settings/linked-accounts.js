38dbe7fe849848a14597df2881ddf84715dbc2db
Thomas McNiven <hello@vevix.net>
2017-03-25 02:45:46 +1000

    Initial implementation of Youtube linking

--- app/controllers/settings/linked-accounts.js
+++ app/controllers/settings/linked-accounts.js
@@ -1,21 +1,80 @@
 import Controller from 'ember-controller';
 import get from 'ember-metal/get';
 import service from 'ember-service/inject';
-import { task, taskGroup } from 'ember-concurrency';
+import computed, { alias } from 'ember-computed';
+import { task } from 'ember-concurrency';
+import RSVP from 'rsvp';
+
+const findLinkedAccount = kind => (
+  computed('model.@each.isDeleted', function() {
+    return get(this, 'model').findBy('kind', kind);
+  }).readOnly()
+);
 
 export default Controller.extend({
   facebook: service(),
-  facebookTasks: taskGroup().drop(),
+  torii: service(),
+  youtubeAccount: findLinkedAccount('LinkedAccount::YoutubeChannel'),
+  hasAccounts: alias('youtubeAccount'),
+
+  // eslint-disable-next-line
+  hasDirtyAccounts: computed('youtubeAccount.hasDirtyAttributes', function() {
+    return ['youtubeAccount'].any(account => get(account, 'hasDirtyAttributes'));
+  }).readOnly(),
 
   connectFacebook: task(function* () {
     yield get(this, 'facebook').connect(get(this, 'session.account')).catch(() => {
       get(this, 'session.account').rollbackAttributes();
     });
-  }).group('facebookTasks'),
+  }),
 
   disconnectFacebook: task(function* () {
     yield get(this, 'facebook').disconnect(get(this, 'session.account')).catch(() => {
       get(this, 'session.account').rollbackAttributes();
     });
-  }).group('facebookTasks'),
+  }),
+
+  createLink: task(function* (kind) {
+    // get token
+    const provider = this._getProviderType(kind);
+    if (!provider) { return; }
+    const token = yield get(this, 'torii').open('google-oauth2-bearer').then(response => (
+      response.authorizationToken.access_token
+    )).catch(() => {});
+    if (!token) { return; }
+
+    // create linked account
+    const account = get(this, 'store').createRecord('linked-account', {
+      kind,
+      token,
+      user: get(this, 'session.account')
+    });
+    yield account.save().then(() => {
+      get(this, 'model').addObject(account);
+    }).catch(() => {});
+  }),
+
+  destroyLink: task(function* (kind) {
+    const account = get(this, 'model').findBy('kind', kind);
+    yield account.destroyRecord().catch(() => {
+      account.rollbackAttributes();
+    });
+  }),
+
+  saveChangesTask: task(function* () {
+    const accounts = [];
+    ['youtubeAccount'].filterBy('hasDirtyAttributes').forEach((account) => {
+      accounts.push(account);
+    });
+    yield RSVP.all(accounts);
+  }),
+
+  _getProviderType(kind) {
+    switch (kind) {
+      case 'LinkedAccount::YoutubeChannel':
+        return 'google-oauth2-bearer';
+      default:
+        return null;
+    }
+  }
 });
