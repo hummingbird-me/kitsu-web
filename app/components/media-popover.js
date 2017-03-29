2cb60e595db53cd1e454d9181a94955cb6ead12b
Thomas McNiven <hello@vevix.net>
2017-03-30 00:17:00 +1000

    Update media popover to use average rating and drop library button

--- app/components/media-popover.js
+++ app/components/media-popover.js
@@ -1,98 +1,23 @@
 import Component from 'ember-component';
-import service from 'ember-service/inject';
 import get from 'ember-metal/get';
-import set from 'ember-metal/set';
+import computed from 'ember-computed';
 import HoverIntentMixin from 'client/mixins/hover-intent';
 
 export default Component.extend(HoverIntentMixin, {
-  cache: service('local-cache'),
-  store: service(),
-
-  _onMouseEnter() {
-    this._super(...arguments);
-    this._getLibraryEntry();
-  },
-
-  _getLibraryEntry() {
-    // user isn't logged in.
-    if (!get(this, 'session.hasUser')) {
-      return;
+  /** Determines what class to use based on the percentage value. */
+  percentageClass: computed('media.averageRating', function() {
+    if (!get(this, 'media.averageRating')) {
+      return '';
     }
-
-    // check if we already have this entry in store.
-    const entry = this._checkLocalStore();
-    if (entry) {
-      set(this, 'entry', entry);
-    } else {
-      this._requestLibraryEntry();
+    const rating = get(this, 'media.averageRating');
+    if (rating <= 25) {
+      return 'percent-quarter-1';
+    } else if (rating <= 50) {
+      return 'percent-quarter-2';
+    } else if (rating <= 75) {
+      return 'percent-quarter-3';
+    } else if (rating <= 100) {
+      return 'percent-quarter-4';
     }
-  },
-
-  _checkLocalStore() {
-    const mediaId = get(this, 'media.id');
-    const mediaType = get(this, 'media.modelType');
-    const lookupKey = `${mediaType}-${mediaId}`;
-    const id = get(this, 'cache').getFromCache('library-entry', lookupKey);
-    return get(this, 'store').peekRecord('library-entry', id);
-  },
-
-  _requestLibraryEntry() {
-    const media = get(this, 'media');
-    const type = get(media, 'modelType');
-    const promise = get(this, 'store').query('library-entry', {
-      filter: {
-        kind: type,
-        [`${type}_id`]: get(media, 'id'),
-        user_id: get(this, 'session.account.id')
-      }
-    }).then((results) => {
-      if (get(this, 'isDestroying') || get(this, 'isDestroyed')) { return; }
-      const entry = get(results, 'firstObject');
-      set(this, 'entry', entry);
-      if (entry !== undefined) {
-        set(this, `entry.${type}`, media);
-        this._addToStore();
-      }
-    });
-    set(this, 'entry', promise);
-  },
-
-  _addToStore() {
-    const type = get(this, 'media.modelType');
-    const id = get(this, 'media.id');
-    const lookupKey = `${type}-${id}`;
-    const entryId = get(this, 'entry.id');
-    get(this, 'cache').addToCache('library-entry', lookupKey, entryId);
-  },
-
-  actions: {
-    createEntry(status) {
-      const user = get(this, 'session.account');
-      const type = get(this, 'media.modelType');
-      const entry = get(this, 'store').createRecord('library-entry', {
-        status,
-        user,
-        [type]: get(this, 'media')
-      });
-      return entry.save().then(() => {
-        set(this, 'entry', entry);
-        this._addToStore();
-      });
-    },
-
-    updateEntry(status) {
-      set(this, 'entry.status', status);
-      return get(this, 'entry').save().catch(() => {
-        get(this, 'entry').rollbackAttributes();
-      });
-    },
-
-    deleteEntry() {
-      return get(this, 'entry').destroyRecord().then(() => {
-        set(this, 'entry', undefined);
-      }).catch(() => {
-        get(this, 'entry').rollbackAttributes();
-      });
-    }
-  }
+  }).readOnly()
 });
