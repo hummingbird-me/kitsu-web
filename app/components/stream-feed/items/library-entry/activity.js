dd423a3adca99ca550dcca67cf583731f045d04d
Thomas McNiven <hello@vevix.net>
2017-04-09 11:14:16 +1000

    Handle feed ratings that are prior to rating change

--- app/components/stream-feed/items/library-entry/activity.js
+++ app/components/stream-feed/items/library-entry/activity.js
@@ -1,8 +1,18 @@
 import Component from 'ember-component';
+import get from 'ember-metal/get';
 import set from 'ember-metal/set';
+import computed from 'ember-computed';
 import { strictInvokeAction } from 'ember-invoke-action';
 
 export default Component.extend({
+  rating: computed('activity.{rating,nineteenScale}', function() {
+    const rating = get(this, 'activity.rating');
+    if (get(this, 'activity.nineteenScale')) {
+      return rating;
+    }
+    return rating * 4;
+  }).readOnly(),
+
   actions: {
     deleteActivity(activity) {
       set(this, 'isDeleteing', true);
