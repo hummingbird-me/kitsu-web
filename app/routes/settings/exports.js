e3552274f782ddff55ab362c571abfcc53b7ce6d
matthewdias <matthewdias@me.com>
2017-04-10 15:01:24 -0500

    display logs

--- app/routes/settings/exports.js
+++ app/routes/settings/exports.js
@@ -6,7 +6,9 @@ import Pagination from 'client/mixins/pagination';
 
 export default Route.extend(Pagination, {
   modelTask: task(function* () {
-    const results = yield get(this, 'store').findAll('linked-account');
+    const results = yield get(this, 'store').findAll('linked-account', {
+      include: 'libraryEntryLogs.media'
+    });
     const controller = this.controllerFor(get(this, 'routeName'));
     set(controller, 'taskValue', results);
   }),
