a30a2b19aeec6af32997ea2b0fad3b00b3711195
matthewdias <matthewdias@me.com>
2017-04-10 15:01:24 -0500

    display logs

--- app/models/linked-account.js
+++ app/models/linked-account.js
@@ -1,6 +1,6 @@
 import Base from 'client/models/-base';
 import attr from 'ember-data/attr';
-import { belongsTo } from 'ember-data/relationships';
+import { belongsTo, hasMany } from 'ember-data/relationships';
 
 export default Base.extend({
   externalUserId: attr('string'),
@@ -11,5 +11,6 @@ export default Base.extend({
   shareTo: attr('boolean'),
   syncTo: attr('boolean'),
 
-  user: belongsTo('user')
+  user: belongsTo('user'),
+  libraryEntryLogs: hasMany('libraryEntryLog'),
 });
