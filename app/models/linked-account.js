b3976311a16b1a91ba63223127f9fbf381eee783
matthewdias <matthewdias@me.com>
2017-04-11 01:32:22 -0500

    sort and group by date

--- app/models/linked-account.js
+++ app/models/linked-account.js
@@ -1,6 +1,9 @@
 import Base from 'client/models/-base';
 import attr from 'ember-data/attr';
 import { belongsTo, hasMany } from 'ember-data/relationships';
+import get from 'ember-metal/get';
+import computed from 'ember-computed';
+import groupBy from 'ember-group-by';
 
 export default Base.extend({
   externalUserId: attr('string'),
@@ -13,4 +16,8 @@ export default Base.extend({
 
   user: belongsTo('user'),
   libraryEntryLogs: hasMany('libraryEntryLog'),
+  sortedLogs: computed('libraryEntryLogs', function() {
+    return get(this, 'libraryEntryLogs').sortBy('createdAt').toArray();
+  }).readOnly(),
+  logsByDate: groupBy('sortedLogs', 'date'),
 });
