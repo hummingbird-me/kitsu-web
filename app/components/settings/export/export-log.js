8b605f61fdebaf2dc76a51075e34c8cdbe6c51dd
matthewdias <matthewdias@me.com>
2017-04-11 20:05:26 -0500

    sort descending

--- app/components/settings/export/export-log.js
+++ app/components/settings/export/export-log.js
@@ -3,16 +3,36 @@ import get from 'ember-metal/get';
 import set from 'ember-metal/set';
 import computed from 'ember-computed';
 import groupBy from 'ember-group-by';
+import moment from 'moment';
+
+const dateFormat = 'YYYY-MM-DD';
 
 export default Component.extend({
-  sortedLogs: computed(function() {
-    const exp = get(this, 'export');
-    const logs = get(exp, 'libraryEntryLogs').sortBy('createdAt').toArray();
+  datedLogs: computed('export.libraryEntryLogs', function() {
+    const logs = get(this, 'export.libraryEntryLogs');
     logs.forEach((log) => {
       const created = get(log, 'createdAt');
-      set(log, 'date', created.format('MMMM Do YYYY'));
+      set(log, 'date', created.format(dateFormat));
     });
-    return logs;
+    return logs
   }).readOnly(),
-  logsByDate: groupBy('sortedLogs', 'date'),
+  logsByDate: groupBy('datedLogs', 'date'),
+  sortedLogs: computed('logsByDate', function() {
+    const dates = get(this, 'logsByDate');
+    const sDates = dates.sort((a, b) => {
+      const ad = moment(get(a, 'value'), dateFormat);
+      const bd = moment(get(b, 'value'), dateFormat);
+      return bd.diff(ad);
+    });
+    sDates.forEach((date) => {
+      const logs = get(date, 'items');
+      const sLogs = logs.sort((a, b) => {
+        return get(b, 'createdAt') - get(a, 'createdAt');
+      });
+      set(date, 'items', sLogs);
+      const d = moment(get(date, 'value'), dateFormat);
+      set(date, 'value', d.format('MMMM Do YYYY'));
+    });
+    return sDates;
+  }).readOnly(),
 });
