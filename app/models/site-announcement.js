f6fc53227655afd4a20d1be8a8c710f9dadaf08a
Thomas McNiven <hello@vevix.net>
2017-04-25 21:30:59 +1000

    Add the site-announcement model

---
+++ app/models/site-announcement.js
@@ -0,0 +1,28 @@
+import Base from 'client/models/-base';
+import attr from 'ember-data/attr';
+import { belongsTo } from 'ember-data/relationships';
+import { validator, buildValidations } from 'ember-cp-validations';
+
+const Validations = buildValidations({
+  description: [
+    validator('presence', true)
+  ],
+  image: [
+    validator('format', { type: 'url', allowBlank: true })
+  ],
+  link: [
+    validator('format', { type: 'url', allowBlank: true })
+  ],
+  title: [
+    validator('presence', true)
+  ]
+});
+
+export default Base.extend(Validations, {
+  description: attr('string'),
+  image: attr('string'),
+  link: attr('string'),
+  title: attr('string'),
+
+  user: belongsTo('user')
+});
