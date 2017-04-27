151f3e99dfa9c6e4c8aee5089f1e71d939cdd298
Thomas McNiven <hello@vevix.net>
2017-04-28 00:40:35 +1000

    Remove ember-group-by

--- app/components/settings/export/export-logs.js
+++ app/components/settings/export/export-logs.js
@@ -1,44 +1,11 @@
 import Component from 'ember-component';
 import get from 'ember-metal/get';
-import set from 'ember-metal/set';
-import computed from 'ember-computed';
-import groupBy from 'ember-group-by';
 import { task } from 'ember-concurrency';
 import { concat } from 'client/utils/computed-macros';
 import Pagination from 'client/mixins/pagination';
-import moment from 'moment';
-
-const DATE_FORMAT = 'YYYY-MM-DD';
 
 export default Component.extend(Pagination, {
   exportLogs: concat('getExportLogsTask.last.value', 'paginatedRecords'),
-  groupedExportLogs: groupBy('exportLogs', 'formattedDate'),
-
-  /**
-   * Take the grouped logs which are in a format of:
-   *   [{ property: 'formattedDate', value: 'YYYY-MM-DD', items: [...] }, ...]
-   * Sort the list by the value property, then sort the items of each element
-   * by their createdAt property.
-   */
-  sortedLogs: computed('groupedExportLogs', function() {
-    const dates = get(this, 'groupedExportLogs');
-    const sortedDates = dates.sort((a, b) => {
-      const ad = moment(get(a, 'value'), DATE_FORMAT);
-      const bd = moment(get(b, 'value'), DATE_FORMAT);
-      return bd.diff(ad);
-    });
-
-    sortedDates.forEach((date) => {
-      const logs = get(date, 'items');
-      const sortedLogs = logs.sort((a, b) => (
-        get(b, 'createdAt').diff(get(a, 'createdAt'))
-      ));
-      set(date, 'items', sortedLogs);
-      const d = moment(get(date, 'value'), DATE_FORMAT);
-      set(date, 'value', d.format('MMMM Do YYYY'));
-    });
-    return sortedDates;
-  }).readOnly(),
 
   didReceiveAttrs() {
     this._super(...arguments);
@@ -58,5 +25,11 @@ export default Component.extend(Pagination, {
       get(this, 'pollForChangesTask').perform();
     }
     return logs;
-  }).keepLatest()
+  }).keepLatest(),
+
+  actions: {
+    sortLogs(first, second) {
+      return get(second, 'createdAt').diff(get(first, 'createdAt'));
+    }
+  }
 });
