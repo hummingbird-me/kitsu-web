c7982595587079b4debefc95fcc8a2f94120398b
Thomas McNiven <hello@vevix.net>
2017-04-29 10:20:18 +1000

    Cleanup dashboard controller

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -2,7 +2,6 @@ import Controller from 'ember-controller';
 import computed from 'ember-computed';
 import get from 'ember-metal/get';
 import set from 'ember-metal/set';
-import observer from 'ember-metal/observer';
 import { storageFor } from 'ember-local-storage';
 
 const MAGIC_NUMBER = 7;
@@ -10,30 +9,19 @@ const MAGIC_NUMBER = 7;
 export default Controller.extend({
   lastUsed: storageFor('last-used'),
 
-  streamId: computed('streamType', 'session.hasUser', function() {
+  streamId: computed('streamType', function() {
     return get(this, 'streamType') === 'global' ? 'global' : get(this, 'session.account.id');
   }).readOnly(),
-
-  updateStreamType: observer('session.hasUser', 'session.account.followingCount', function() {
-    const defaultType = this._getDefaultType();
-    set(this, 'streamType', get(this, 'lastUsed.feedType') || defaultType);
-  }),
 
   init() {
     this._super(...arguments);
     const defaultType = this._getDefaultType();
-    if (get(this, 'session.hasUser')) {
-      set(this, 'streamType', get(this, 'lastUsed.feedType') || defaultType);
-    } else {
-      set(this, 'streamType', defaultType);
-    }
+    set(this, 'streamType', get(this, 'lastUsed.feedType') || defaultType);
   },
 
   _getDefaultType() {
-    if (get(this, 'session.hasUser')) {
-      if (get(this, 'session.account.followingCount') >= MAGIC_NUMBER) {
-        return 'timeline';
-      }
+    if (get(this, 'session.account.followingCount') >= MAGIC_NUMBER) {
+      return 'timeline';
     }
     return 'global';
   },
