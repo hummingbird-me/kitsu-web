9baed6456a9fc2908adc064d8c2da1cf1c3fbf10
Thomas McNiven <hello@vevix.net>
2017-05-07 14:17:39 +1000

    Increase post cut-off size

--- app/mixins/pagination.js
+++ app/mixins/pagination.js
@@ -61,7 +61,7 @@ export default Mixin.create({
   },
 
   actions: {
-    /** Execute a pagination request. This must be overriden by the consumer. */
+    /** Execute a pagination request. */
     onPagination(recordType, customOptions = {}) {
       return this._doPaginationRequest(recordType, customOptions);
     }
