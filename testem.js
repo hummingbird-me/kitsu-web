0e12aaed28f8e5c47c9e86bf10136017b10cc6a1
Thomas McNiven <hello@vevix.net>
2017-05-09 12:44:59 +1000

    Test using headless Chrome

--- testem.js
+++ testem.js
@@ -3,10 +3,16 @@ module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
   launch_in_ci: [
-    'PhantomJS'
+    'Chrome'
   ],
   launch_in_dev: [
-    'PhantomJS',
     'Chrome'
-  ]
+  ],
+  browser_args: {
+    Chrome: [
+      '--headless',
+      '--disable-gpu',
+      '--remote-debugging-port=9222'
+    ]
+  }
 };
