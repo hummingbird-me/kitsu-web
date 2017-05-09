0d5fbd03a581315d909d23e470becdcf570eb0b4
Thomas McNiven <hello@vevix.net>
2017-05-09 14:15:47 +1000

    Update travis config and open normal Chrome in dev

--- testem.js
+++ testem.js
@@ -1,18 +1,22 @@
 /* eslint-env node */
+const isMacOS = /^darwin/.test(process.platform);
+const isWindows = /^win/.test(process.platform);
+
+let browserArgs;
+
+if (isMacOS || isWindows) {
+  browserArgs = [];
+} else {
+  browserArgs = ['--headless', '--disable-gpu', '--remote-debugging-port=9222'];
+}
+
 module.exports = {
+  framework: 'qunit',
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
-  launch_in_ci: [
-    'Chrome'
-  ],
-  launch_in_dev: [
-    'Chrome'
-  ],
+  launch_in_ci: ['Chrome'],
+  launch_in_dev: ['Chrome'],
   browser_args: {
-    Chrome: [
-      '--headless',
-      '--disable-gpu',
-      '--remote-debugging-port=9222'
-    ]
+    Chrome: browserArgs,
   }
 };
