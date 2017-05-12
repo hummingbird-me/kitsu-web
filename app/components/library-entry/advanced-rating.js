b4fc9c5be0926a35fc75004bb8d98d8dd0dc29cf
Thomas McNiven <hello@vevix.net>
2017-05-13 06:32:16 +1000

    Schedule DOM work on advanced rating

--- app/components/library-entry/advanced-rating.js
+++ app/components/library-entry/advanced-rating.js
@@ -3,6 +3,7 @@ import get from 'ember-metal/get';
 import set from 'ember-metal/set';
 import { task, timeout } from 'ember-concurrency';
 import { invokeAction } from 'ember-invoke-action';
+import { scheduleRead, scheduleWork } from 'spaniel';
 import canUseDOM from 'client/utils/can-use-dom';
 
 export default Component.extend({
@@ -41,19 +42,38 @@ export default Component.extend({
     const [element] = slider.getElementsByClassName('noUi-handle');
     const handleDot = document.createElement('div');
     handleDot.classList.add('noUi-handle-dot');
-    element.appendChild(handleDot);
     const handleBackground = document.createElement('span');
     handleBackground.classList.add('noUi-handle-background');
-    handleBackground.style.width = `${slider.clientWidth}px`;
-    handleDot.appendChild(handleBackground);
+    this._scheduleRead(() => {
+      const width = slider.clientWidth;
+      this._scheduleWork(() => {
+        handleBackground.style.width = `${width}px`;
+        element.appendChild(handleDot);
+        handleDot.appendChild(handleBackground);
+      });
+    });
   },
 
   _updateHandle(rating) {
     const value = ((rating - 1) * 10);
     const [element] = document.getElementsByClassName('advanced-rating-slider');
     if (!element) { return; }
-    const offset = -((value / 100) * element.clientWidth);
-    const [handle] = element.getElementsByClassName('noUi-handle-background');
-    handle.style.left = `${offset}px`;
+    this._scheduleRead(() => {
+      const offset = -((value / 100) * element.clientWidth);
+      this._scheduleWork(() => {
+        const [handle] = element.getElementsByClassName('noUi-handle-background');
+        handle.style.left = `${offset}px`;
+      });
+    });
+  },
+
+  _scheduleRead(callback) {
+    if (get(this, 'isDestroyed')) { return; }
+    scheduleRead(callback);
+  },
+
+  _scheduleWork(callback) {
+    if (get(this, 'isDestroyed')) { return; }
+    scheduleWork(callback);
   }
 });
