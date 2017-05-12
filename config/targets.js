d86ea0246f1c894d079ddb4e00c3728a8a9edc36
Thomas McNiven <hello@vevix.net>
2017-05-12 20:57:25 +1000

    Update targets.js

--- config/targets.js
+++ config/targets.js
@@ -2,9 +2,10 @@
 
 module.exports = {
   browsers: [
-    'ie 11',
+    'last 1 IE versions',
     'last 1 Chrome versions',
     'last 1 Firefox versions',
-    'last 1 Safari versions'
+    'last 1 Safari versions',
+    'last 1 Edge versions'
   ]
 };
