fd1c54f7fcded47dddd3fceecb3ed39b8e066cc7
Thomas McNiven <hello@vevix.net>
2017-05-20 18:00:58 +1000

    Add pagination to notifications page

---
+++ app/controllers/notifications.js
@@ -0,0 +1,6 @@
+import Controller from 'ember-controller';
+import { concat } from 'client/utils/computed-macros';
+
+export default Controller.extend({
+  notifications: concat('model.taskInstance.value', 'model.paginatedRecords')
+});
--- app/routes/notifications.js
+++ app/routes/notifications.js
@@ -2,17 +2,30 @@ import Route from 'ember-route';
 import get from 'ember-metal/get';
 import service from 'ember-service/inject';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import Pagination from 'kitsu-shared/mixins/pagination';
 
-export default Route.extend(AuthenticatedRouteMixin, {
+export default Route.extend(AuthenticatedRouteMixin, Pagination, {
   authenticationRoute: 'dashboard',
   queryCache: service(),
 
   model() {
-    return get(this, 'queryCache').query('feed', {
-      type: 'notifications',
-      id: get(this, 'session.account.id'),
-      include: 'actor,target.post',
-      page: { limit: 30 }
-    });
+    return {
+      taskInstance: this.queryPaginated('feed', {
+        type: 'notifications',
+        id: get(this, 'session.account.id'),
+        include: 'actor,target.post',
+        page: { limit: 30 }
+      }, { cache: false }),
+      paginatedRecords: []
+    };
+  },
+
+  actions: {
+    onPagination() {
+      return this._super('feed', {
+        type: 'notifications',
+        id: get(this, 'session.account.id')
+      });
+    }
   }
 });
