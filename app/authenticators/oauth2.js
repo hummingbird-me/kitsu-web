6a27682b1493ee679db4057350f6b69fd23865a9
Thomas McNiven <hello@vevix.net>
2017-05-24 02:11:50 +1000

    Ship auth header with authenticator requests

--- app/authenticators/oauth2.js
+++ app/authenticators/oauth2.js
@@ -1,9 +1,24 @@
 import OAuth2PasswordGrant from 'ember-simple-auth/authenticators/oauth2-password-grant';
-import getter from 'client/utils/getter';
+import get from 'ember-metal/get';
+import set from 'ember-metal/set';
+import service from 'ember-service/inject';
 import config from 'client/config/environment';
 
 export default OAuth2PasswordGrant.extend({
   refreshAccessTokens: true,
-  serverTokenEndpoint: getter(() => `${config.kitsu.APIHost || ''}/api/oauth/token`),
-  serverTokenRevocationEndpoint: getter(() => `${config.kitsu.APIHost || ''}/api/oauth/revoke`)
+  session: service(),
+
+  init() {
+    this._super(...arguments);
+    const host = config.kitsu.APIHost || '';
+    set(this, 'serverTokenEndpoint', `${host}/api/oauth/token`);
+    set(this, 'serverTokenRevocationEndpoint', `${host}/api/oauth/revoke`);
+  },
+
+  makeRequest(url, data, headers = {}) {
+    get(this, 'session').authorize('authorizer:application', (name, value) => {
+      headers[name] = value; // eslint-disable-line
+    });
+    return this._super(url, data, headers);
+  }
 });
