dfdd5e3d75be2f4880f13db616e7b4cf6000fa43
Thomas McNiven <hello@vevix.net>
2017-06-02 02:24:47 +1000

    Query trending data

--- app/components/explore/explore-section.js
+++ app/components/explore/explore-section.js
@@ -1,26 +1,59 @@
 import Component from 'ember-component';
 import get from 'ember-metal/get';
 import service from 'ember-service/inject';
-import { reads } from 'ember-computed';
+import { or } from 'ember-computed';
 import { task } from 'ember-concurrency';
 
 export default Component.extend({
   classNames: ['explore-section'],
   more: 'explore.more',
   limit: 5,
+  ajax: service(),
+  store: service(),
   queryCache: service(),
-  results: reads('getDataTask.last.value'),
+  results: or('getDataTask.last.value', 'getTrendingDataTask.last.value'),
+
 
   didReceiveAttrs() {
     this._super(...arguments);
-    get(this, 'getDataTask').perform();
+    if (get(this, 'type') === 'trending') {
+      get(this, 'getTrendingDataTask').perform();
+    } else {
+      get(this, 'getDataTask').perform();
+    }
   },
 
   getDataTask: task(function* () {
     const type = get(this, 'mediaType');
     const options = this._getRequestOptions();
     return yield get(this, 'queryCache').query(type, options);
-  }).keepLatest(),
+  }).drop(),
+
+  getTrendingDataTask: task(function* () {
+    const type = get(this, 'mediaType');
+    const categoryId = get(this, 'category.id');
+    const limit = get(this, 'limit');
+    const path = `/trending/${type}?in_category=true&category=${categoryId}&limit=${limit}`;
+
+    // does a cache entry exist?
+    const cachedRecords = yield get(this, 'queryCache').get('trending', path);
+    if (cachedRecords) {
+      return cachedRecords;
+    }
+
+    // query the trending API
+    const response = yield get(this, 'ajax').request(path);
+    const records = [];
+    response.data.forEach((data) => {
+      const normalize = get(this, 'store').normalize(type, data);
+      const record = get(this, 'store').push(normalize);
+      records.addObject(record);
+    });
+
+    // push into cache
+    get(this, 'queryCache').push('trending', path, records);
+    return records;
+  }).drop(),
 
   _getRequestOptions() {
     const options = {
