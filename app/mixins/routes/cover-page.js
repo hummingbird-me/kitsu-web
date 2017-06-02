df0aeb4ff38f48fd265225cdfedd91a9e3b1190a
Thomas McNiven <hello@vevix.net>
2017-06-02 10:40:05 +1000

    Remove jQuery dependency on cover-image mixin

--- app/mixins/routes/cover-page.js
+++ app/mixins/routes/cover-page.js
@@ -2,8 +2,7 @@ import Mixin from 'ember-metal/mixin';
 import get from 'ember-metal/get';
 import set from 'ember-metal/set';
 import { isEmpty } from 'ember-utils';
-import { bind, later, cancel, scheduleOnce } from 'ember-runloop';
-import jQuery from 'jquery';
+import { later, cancel, scheduleOnce } from 'ember-runloop';
 
 const DISTANCE = 210;
 
@@ -12,36 +11,50 @@ export default Mixin.create({
 
   activate() {
     this._super(...arguments);
-    jQuery('body').addClass('cover-page');
-    jQuery(document).on('scroll.cover', bind(this, '_handleScroll'));
-    scheduleOnce('afterRender', bind(this, '_handleScroll'));
+    this._handleScroll = () => { this.handleScroll(); };
+
+    document.body.classList.add('cover-page');
+    document.addEventListener('scroll', this._handleScroll);
     scheduleOnce('afterRender', () => {
-      jQuery('.primary-nav').hover(() => {
-        cancel(get(this, 'timer'));
-        jQuery('.primary-nav').removeClass('transparent');
+      this.handleScroll();
+      const element = document.getElementById('kitsu-navbar');
+
+      this._handleMouseEnter = () => {
+        cancel(this.fadeTimer);
+        element.classList.remove('transparent');
         set(this, 'isHovered', true);
-      }, () => {
-        if (jQuery(window).scrollTop() < DISTANCE && isEmpty(jQuery('#search').val()) === true) {
-          const timer = later(() => jQuery('.primary-nav').addClass('transparent'), 500);
-          set(this, 'timer', timer);
+      };
+      element.addEventListener('mouseenter', this._handleMouseEnter);
+
+      this._handleMouseLeave = () => {
+        const searchElement = document.getElementById('search');
+        if (document.body.scrollTop < DISTANCE && isEmpty(searchElement.value)) {
+          this.fadeTimer = later(() => {
+            element.classList.add('transparent');
+          }, 500);
         }
         set(this, 'isHovered', false);
-      });
+      };
+      element.addEventListener('mouseleave', this._handleMouseLeave);
     });
   },
 
   deactivate() {
     this._super(...arguments);
-    jQuery('body').removeClass('cover-page');
-    jQuery('.primary-nav').unbind('mouseenter').unbind('mouseleave');
-    jQuery(document).off('scroll.cover');
+    const element = document.getElementById('kitsu-navbar');
+    document.body.classList.remove('cover-page');
+    document.removeEventListener('scroll', this._handleScroll);
+    element.removeEventListener('mouseenter', this._handleMouseEnter);
+    element.removeEventListener('mouseleave', this._handleMouseLeave);
   },
 
-  _handleScroll() {
-    if (jQuery(window).scrollTop() >= DISTANCE) {
-      jQuery('.primary-nav').removeClass('transparent');
-    } else if (get(this, 'isHovered') === false && isEmpty(jQuery('#search').val()) === true) {
-      jQuery('.primary-nav').addClass('transparent');
+  handleScroll() {
+    const element = document.getElementById('kitsu-navbar');
+    const searchElement = document.getElementById('search');
+    if (document.body.scrollTop >= DISTANCE) {
+      element.classList.remove('transparent');
+    } else if (!get(this, 'isHovered') && isEmpty(searchElement.value)) {
+      element.classList.add('transparent');
     }
   }
 });
