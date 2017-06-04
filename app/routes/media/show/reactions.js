55341cc35c2c4c7a83ed7d270b337240d2b3c878
matthewdias <matthewdias@me.com>
2017-06-04 03:12:56 -0500

    Add Reactions tab

--- app/routes/media/show/reactions.js
+++ app/routes/media/show/reactions.js
@@ -1,5 +1,44 @@
 import Route from 'ember-route';
+import get from 'ember-metal/get';
+import set from 'ember-metal/set';
+import service from 'ember-service/inject';
+import { capitalize } from 'ember-string';
+import Pagination from 'kitsu-shared/mixins/pagination';
 
-export default Route.extend({
-  templateName: 'media/show/reactions'
+export default Route.extend(Pagination, {
+  templateName: 'media/show/reactions',
+  intl: service(),
+
+  model() {
+    const media = this._getParentModel();
+    const type = get(media, 'modelType');
+    const sort = get(this, 'sort');
+    return {
+      taskInstance: this.queryPaginated('media-reaction', {
+        include: 'user',
+        filter: {
+          [`${type}Id`]: get(media, 'id'),
+        },
+        page: { limit: 6 },
+        sort
+      }),
+      paginatedRecords: []
+    };
+  },
+
+  setupController(controller) {
+    this._super(...arguments);
+    set(controller, 'media', this._getParentModel());
+  },
+
+  titleToken() {
+    const model = this._getParentModel();
+    const title = get(model, 'computedTitle');
+    return get(this, 'intl').t('titles.media.show.reactions', { title });
+  },
+
+  _getParentModel() {
+    const [mediaType] = get(this, 'routeName').split('.');
+    return this.modelFor(`${mediaType}.show`);
+  }
 });
