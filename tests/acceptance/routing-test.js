40f8ded42597197182546c7be186213bc6f97f06
Thomas McNiven <hello@vevix.net>
2017-06-10 05:36:28 +1000

    Update routing tests for new guest redirect

--- tests/acceptance/routing-test.js
+++ tests/acceptance/routing-test.js
@@ -4,44 +4,46 @@ import { authenticateSession, invalidateSession } from 'client/tests/helpers/emb
 
 moduleForAcceptance('Acceptance | Routing');
 
-test('visiting `/dashboard` redirects to `/`', function(assert) {
-  visit('/dashboard');
-  andThen(() => assert.equal(currentURL(), '/'));
-});
-
-test('visiting `/` works when unauthenticated', function(assert) {
-  invalidateSession(this.application);
-  visit('/');
-  andThen(() => assert.equal(currentURL(), '/'));
-});
-
-test('visiting `/admin` redirects when unauthenticated', function(assert) {
-  invalidateSession(this.application);
-  visit('/admin');
-  andThen(() => assert.equal(currentURL(), '/'));
-});
-
-test('visiting `/settings/*` when unauthenticated redirects', function(assert) {
-  invalidateSession(this.application);
-  visit('/settings/profile');
-  andThen(() => assert.equal(currentURL(), '/'));
-});
-
-test('visiting `/notifications` when unauthenticated redirects', function(assert) {
-  invalidateSession(this.application);
-  visit('/notifications');
-  andThen(() => assert.equal(currentURL(), '/'));
-});
-
-test('visiting /password-reset redirects when authenticated', function(assert) {
+test('visiting `/dashboard` redirects to `/`', async function(assert) {
   server.create('user');
   authenticateSession(this.application);
-  visit('/password-reset');
-  andThen(() => assert.equal(currentURL(), '/'));
+  await visit('/dashboard');
+  assert.equal(currentURL(), '/');
 });
 
-test('visiting an unknown route redirects to `/404`', function(assert) {
-  visit('/doesnt-exist');
-  andThen(() => assert.equal(currentRouteName(), 'not-found'));
-  andThen(() => assert.equal(currentURL(), '/404'));
+test('visiting `/` as a guest redirects to explore', async function(assert) {
+  invalidateSession(this.application);
+  await visit('/');
+  assert.equal(currentURL(), '/explore/anime');
+});
+
+test('visiting `/admin` redirects when unauthenticated', async function(assert) {
+  invalidateSession(this.application);
+  await visit('/admin');
+  assert.equal(currentURL(), '/explore/anime');
+});
+
+test('visiting `/settings/*` when unauthenticated redirects', async function(assert) {
+  invalidateSession(this.application);
+  await visit('/settings/profile');
+  assert.equal(currentURL(), '/explore/anime');
+});
+
+test('visiting `/notifications` when unauthenticated redirects', async function(assert) {
+  invalidateSession(this.application);
+  await visit('/notifications');
+  assert.equal(currentURL(), '/explore/anime');
+});
+
+test('visiting /password-reset redirects when authenticated', async function(assert) {
+  server.create('user');
+  authenticateSession(this.application);
+  await visit('/password-reset');
+  assert.equal(currentURL(), '/');
+});
+
+test('visiting an unknown route redirects to `/404`', async function(assert) {
+  await visit('/doesnt-exist');
+  assert.equal(currentRouteName(), 'not-found');
+  assert.equal(currentURL(), '/404');
 });
