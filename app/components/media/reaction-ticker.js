90d815e8bca32ceaef9c44126c9b9717ddff49e6
matthewdias <matthewdias@me.com>
2017-06-15 19:31:04 -0500

    Use task for ticker

--- app/components/media/reaction-ticker.js
+++ app/components/media/reaction-ticker.js
@@ -3,7 +3,7 @@ import service from 'ember-service/inject';
 import get from 'ember-metal/get';
 import set from 'ember-metal/set';
 import computed from 'ember-computed';
-import { task } from 'ember-concurrency';
+import { task, timeout } from 'ember-concurrency';
 
 export default Component.extend({
   classNames: ['reaction-ticker'],
@@ -14,6 +14,13 @@ export default Component.extend({
     const reactions = get(this, 'getReactionsTask.last.value');
     return reactions.objectAt(get(this, 'index'));
   }).readOnly(),
+
+  didReceiveAttrs() {
+    this._super(...arguments);
+    get(this, 'getReactionsTask').perform().then((reactions) => {
+      get(this, 'tickerTask').perform(reactions);
+    });
+  },
 
   getReactionsTask: task(function* () {
     const media = get(this, 'media');
@@ -26,14 +33,12 @@ export default Component.extend({
     });
   }),
 
-  init() {
-    this._super(...arguments);
-    get(this, 'getReactionsTask').perform().then((reactions) => {
-      setInterval(() => {
-        const index = get(this, 'index');
-        const next = index === get(reactions, 'length') - 1 ? 0 : index + 1;
-        set(this, 'index', next);
-      }, 10000);
-    });
-  }
+  tickerTask: task(function* (reactions) {
+    while(true) {
+      yield timeout(10000);
+      const index = get(this, 'index');
+      const next = index === get(reactions, 'length') - 1 ? 0 : index + 1;
+      set(this, 'index', next);
+    }
+  }).drop(),
 });
