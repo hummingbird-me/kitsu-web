2c4f9e1490ef296e4115faec45d86935c7ac0309
Thomas McNiven <hello@vevix.net>
2017-07-07 16:50:10 +1000

    Update to Ember/Ember-CLI 2.14

--- tests/helpers/module-for-acceptance.js
+++ tests/helpers/module-for-acceptance.js
@@ -3,7 +3,7 @@ import startApp from 'client/tests/helpers/start-app';
 import destroyApp from 'client/tests/helpers/destroy-app';
 import RSVP from 'rsvp';
 
-const { Promise } = RSVP;
+const { resolve } = RSVP;
 
 export default function(name, options = {}) {
   module(name, {
@@ -17,7 +17,7 @@ export default function(name, options = {}) {
 
     afterEach() {
       const afterEach = options.afterEach && options.afterEach.apply(this, arguments);
-      return Promise.resolve(afterEach).then(() => destroyApp(this.application));
+      return resolve(afterEach).then(() => destroyApp(this.application));
     }
   });
 }
