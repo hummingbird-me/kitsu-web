6568b66ce1f32fac7b362be15b0b380e4e03a813
matthewdias <matthewdias@me.com>
2017-07-08 00:34:35 -0500

    Add more sorts, separate sort direction

--- app/components/users/library/library-sorting.js
+++ app/components/users/library/library-sorting.js
@@ -1,17 +1,25 @@
 import Component from 'ember-component';
+import get from 'ember-metal/get';
+import set from 'ember-metal/set';
 
 export default Component.extend({
   init() {
     this._super(...arguments);
     this.sortingOptions = [
-      '-watched', // Recent
       'watched', // Oldest
-      '-rating', // Highest
       'rating', // Lowest
       'title', // A - Z
-      '-title', // Z - A
       'length', // Shortest
-      '-length' // Longest
+      'started', // Latest
+      'finished', // Latest
+      'added' // Latest
     ];
+  },
+
+  actions: {
+    toggleSortDirection() {
+      const direction = get(this, 'direction');
+      set(this, 'direction', direction === 'desc' ? 'asc' : 'desc');
+    }
   }
 });
