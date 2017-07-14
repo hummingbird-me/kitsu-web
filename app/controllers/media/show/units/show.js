b8021911b6063ec7fc68aaae509ef6741aa3c626
matthewdias <matthewdias@me.com>
2017-07-14 18:02:36 -0500

    Add unit feeds

--- app/controllers/media/show/units/show.js
+++ app/controllers/media/show/units/show.js
@@ -1,5 +1,6 @@
 import Controller from 'ember-controller';
 import computed from 'ember-computed';
+import get from 'ember-metal/get';
 import service from 'ember-service/inject';
 import humanizeDuration from 'client/utils/humanize-duration';
 import moment from 'moment';
@@ -8,9 +9,9 @@ export default Controller.extend({
   intl: service(),
 
   humanizedLength: computed('model.length', function() {
-    const mediaType = get(this, 'mediaType');
+    const unitType = get(this, 'model.modelType');
     const length = get(this, 'model.length');
-    if (mediaType === 'anime') {
+    if (unitType === 'episode') {
       const time = moment.duration(length, 'minutes');
       return humanizeDuration(time);
     }
