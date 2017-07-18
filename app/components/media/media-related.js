0a77ca429af38952cb704d1a18fa1b0f9658c460
Thomas McNiven <hello@vevix.net>
2017-07-19 06:27:12 +1000

    Drop start_date sort on related media

--- app/components/media/media-related.js
+++ app/components/media/media-related.js
@@ -18,7 +18,6 @@ export default Component.extend({
     const response = yield get(this, 'queryCache').query('media-relationship', {
       filter: { source_id: get(this, 'media.id') },
       include: 'destination',
-      sort: 'destination.start_date',
       page: { limit: 4 }
     });
     if (get(response, 'length') === 0) {
