cad9e2f5767c8199fbb05f49c355868d741eeced
Thomas McNiven <hello@vevix.net>
2017-07-20 20:13:35 +1000

    Initial support for interest feeds on dashboard

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -2,6 +2,7 @@ import Controller from 'ember-controller';
 import computed from 'ember-computed';
 import get from 'ember-metal/get';
 import set from 'ember-metal/set';
+import { capitalize } from 'ember-string';
 import { storageFor } from 'ember-local-storage';
 
 const MAGIC_NUMBER = 7;
@@ -9,14 +10,44 @@ const MAGIC_NUMBER = 7;
 export default Controller.extend({
   lastUsed: storageFor('last-used'),
 
-  streamId: computed('streamType', function() {
-    return get(this, 'streamType') === 'global' ? 'global' : get(this, 'session.account.id');
+  streamId: computed('streamType', 'streamInterest', function() {
+    const streamType = get(this, 'streamType');
+    const interest = get(this, 'streamInterest');
+    console.log(streamType);
+    if (interest && streamType === 'interest_timeline') {
+      return `${get(this, 'session.account.id')}-${capitalize(interest)}`;
+    }
+    return streamType === 'global' ? 'global' : get(this, 'session.account.id');
   }).readOnly(),
 
   init() {
     this._super(...arguments);
     const defaultType = this._getDefaultType();
-    set(this, 'streamType', get(this, 'lastUsed.feedType') || defaultType);
+    const cachedType = get(this, 'lastUsed.feedType');
+    if (this._isCachedTypeOutdated(cachedType)) {
+      set(this, 'lastUsed.feedType', defaultType);
+    }
+    if (cachedType && cachedType.includes('/')) {
+      set(this, 'streamInterest', cachedType.split('/')[1]);
+      set(this, 'streamType', cachedType.split('/')[0]);
+    } else {
+      set(this, 'streamType', cachedType || defaultType);
+    }
+  },
+
+  actions: {
+    switchFeed(type) {
+      window.scrollTo(0, 0);
+      console.log(type);
+      if (type.includes('/')) {
+        set(this, 'streamInterest', type.split('/')[1]);
+        set(this, 'streamType', type.split('/')[0]);
+      } else {
+        set(this, 'streamInterest', null);
+        set(this, 'streamType', type);
+      }
+      set(this, 'lastUsed.feedType', type);
+    }
   },
 
   _getDefaultType() {
@@ -26,11 +57,7 @@ export default Controller.extend({
     return 'global';
   },
 
-  actions: {
-    switchFeed(type) {
-      window.scrollTo(0, 0);
-      set(this, 'streamType', type);
-      set(this, 'lastUsed.feedType', type);
-    }
+  _isCachedTypeOutdated(type) {
+    return type === 'group_timeline';
   }
 });
