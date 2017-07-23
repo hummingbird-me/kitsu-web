ed8874c0ab9ec492315261ec02bc8990dec472f2
matthewdias <matthewdias@me.com>
2017-07-22 19:22:30 -0500

    Don't hide spoilers on interest timeline with no unit

--- app/components/stream-feed/items/post.js
+++ app/components/stream-feed/items/post.js
@@ -18,6 +18,7 @@ export default Component.extend(ClipboardMixin, CanMixin, {
   classNames: ['row'],
   isHidden: false,
   showNsfw: false,
+  showSpoilers: false,
   isFollowingPost: false,
   isOverflowed: false,
   isExpanded: false,
@@ -100,10 +101,11 @@ export default Component.extend(ClipboardMixin, CanMixin, {
 
     const post = get(this, 'post');
     const hideNsfw = get(post, 'nsfw') && !get(this, 'showNsfw');
+    const hideSpoilers = get(post, 'spoiler') && !(get(this, 'showSpoilers') && get(this, 'spoiledUnit'));
     if (get(post, 'user') && get(this, 'session').isCurrentUser(get(post, 'user'))) {
       set(this, 'isHidden', hideNsfw);
     } else {
-      set(this, 'isHidden', hideNsfw || get(post, 'spoiler'));
+      set(this, 'isHidden', hideNsfw || hideSpoilers);
     }
 
     // groups
