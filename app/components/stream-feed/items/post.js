1c3838f3635a4bc7ec2216b3f21fe9ed43255002
Matthew Dias <matthewdias@me.com>
2017-07-23 23:51:06 -0500

    [WIP] gate enhancements (#417)
    * Show comment count when hidden
    
    * Don't hide nsfw posts when viewing nsfw groups
    
    * Don't hide spoilers on interest timeline with no unit
    
    * Don't hide spoilers _with_ a unit
    
    * Put nsfw tag on a diet
    
    * Tag nsfw groups in search

--- app/components/stream-feed/items/post.js
+++ app/components/stream-feed/items/post.js
@@ -17,6 +17,8 @@ export default Component.extend(ClipboardMixin, CanMixin, {
   classNameBindings: ['post.isNew:new-post', 'isPinnedPost:pinned-post', 'post.id:stream-item'],
   classNames: ['row'],
   isHidden: false,
+  showNsfw: false,
+  showSpoilers: false,
   isFollowingPost: false,
   isOverflowed: false,
   isExpanded: false,
@@ -98,10 +100,12 @@ export default Component.extend(ClipboardMixin, CanMixin, {
     }
 
     const post = get(this, 'post');
+    const hideNsfw = get(post, 'nsfw') && !get(this, 'showNsfw');
+    const hideSpoilers = get(post, 'spoiler') && !(get(this, 'showSpoilers') && !get(this, 'spoiledUnit'));
     if (get(post, 'user') && get(this, 'session').isCurrentUser(get(post, 'user'))) {
-      set(this, 'isHidden', get(post, 'nsfw'));
+      set(this, 'isHidden', hideNsfw);
     } else {
-      set(this, 'isHidden', get(post, 'nsfw') || get(post, 'spoiler'));
+      set(this, 'isHidden', hideNsfw || hideSpoilers);
     }
 
     // groups
