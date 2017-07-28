f36df95afab0e1eb83e67880fe12fa072749dbd3
matthewdias <matthewdias@me.com>
2017-07-27 23:30:48 -0500

    Use large poster for permalink

--- app/components/media/media-reaction.js
+++ app/components/media/media-reaction.js
@@ -10,6 +10,7 @@ import { image } from 'client/helpers/image';
 import ClipboardMixin from 'client/mixins/clipboard';
 
 export default Component.extend(ClipboardMixin, {
+  large: false,
   isUpvoted: false,
   queryCache: service(),
   store: service(),
@@ -29,7 +30,7 @@ export default Component.extend(ClipboardMixin, {
   }),
 
   posterImageStyle: computed('media.posterImage', function() {
-    const posterImage = image(get(this, 'media.posterImage'), 'medium');
+    const posterImage = image(get(this, 'media.posterImage'), get(this, 'large') ? 'large' : 'medium');
     return htmlSafe(`background-image: url("${posterImage}")`);
   }).readOnly(),
 
