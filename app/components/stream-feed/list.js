997fd149eff5af5c46055b7fbf994cb253aeeafb
Thomas McNiven <hello@vevix.net>
2017-07-28 18:13:52 +1000

    Use canonicalTitle for subject

--- app/components/stream-feed/list.js
+++ app/components/stream-feed/list.js
@@ -55,7 +55,7 @@ export default Component.extend(Pagination, {
       options.filter = { kind: kind === 'user' ? 'posts' : kind };
     }
     return yield this.queryPaginated('feed', options, { cache: false });
-  }).restartable(),
+  }).keepLatest(),
 
   createPost: task(function* (content, options) {
     const data = { content, user: get(this, 'session.account'), ...options };
