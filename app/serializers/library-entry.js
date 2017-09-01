edb94796643682f07ba655de5902d647a534a784
Matthew Dias <matthewdias@me.com>
2017-09-01 13:05:14 -0500

    Don't force current, deferring to server

--- app/serializers/library-entry.js
+++ app/serializers/library-entry.js
@@ -15,13 +15,6 @@ export default ApplicationSerializer.extend({
 
   serializeAttribute(snapshot, json, key) {
     this._super(...arguments);
-    // If progress is changing and the status isn't current -- then we want to update the
-    // status
-    if (key === 'status' && snapshot.attr(key) !== 'current') {
-      if ('progress' in snapshot.changedAttributes()) {
-        json.attributes[key] = 'current'; // eslint-disable-line no-param-reassign
-      }
-    }
 
     if (key === 'ratingTwenty' && key in snapshot.changedAttributes()) {
       get(this, 'session.account').incrementProperty('ratingsCount');
