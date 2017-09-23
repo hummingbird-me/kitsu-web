13f820fb75dbae59e614e65eb8d62094d9330461
matthewdias <matthewdias@me.com>
2017-09-22 23:40:03 -0500

    Don't remove units when editing posts

--- app/components/stream-feed/edit-post.js
+++ app/components/stream-feed/edit-post.js
@@ -3,7 +3,6 @@ import get from 'ember-metal/get';
 import set from 'ember-metal/set';
 import service from 'ember-service/inject';
 import { task } from 'ember-concurrency';
-import { isEmpty, isPresent } from 'ember-utils';
 import errorMessages from 'client/utils/error-messages';
 
 export default Component.extend({
@@ -16,26 +15,6 @@ export default Component.extend({
     Object.keys(options).forEach((option) => {
       set(post, option, get(options, option));
     });
-
-    // don't update spoiled unit if it already exists
-    if (isEmpty(get(post, 'spoiledUnit.content')) === true) {
-      if (get(post, 'spoiler') === true && isPresent(get(post, 'media.content')) === true) {
-        const type = get(post, 'media.modelType');
-        const entry = yield get(this, 'store').query('library-entry', {
-          filter: {
-            user_id: get(post, 'user.id'),
-            kind: type,
-            [`${type}_id`]: get(post, 'media.id')
-          },
-          include: 'unit'
-        }).then(records => get(records, 'firstObject'));
-        if (entry) {
-          set(post, 'spoiledUnit', get(entry, 'unit'));
-        }
-      }
-    } else if (!get(post, 'spoiler')) {
-      set(post, 'spoiledUnit', null);
-    }
 
     return yield post.save()
       .then(() => this.$('.modal').modal('hide'))
