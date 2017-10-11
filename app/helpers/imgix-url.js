c430368b024d1c4b48a6d46c1c59df838969a6ba
Peter Lejeck <peter.lejeck@gmail.com>
2017-10-10 21:58:24 -0700

    Fix imgix URLs for staging

--- app/helpers/imgix-url.js
+++ app/helpers/imgix-url.js
@@ -5,8 +5,8 @@ export function imgixUrl([src, params = {}]) {
   const options = Object.keys(filledParams).map(key => (
     [key, filledParams[key]].map(encodeURIComponent).join('='))
   ).join('&');
-  const imgixHost = 'https://kitsu.imgix.net/';
-  const url = src.replace(/https:\/\/[^\/]+\//, imgixHost);
+  const url = src.replace('https://media.kitsu.io/', 'https://kitsu.imgix.net/')
+                 .replace('https://media-staging.kitsu.io/', 'https://kitsu-staging.imgix.net/');
   return options ? `${url}&${options}` : url;
 }
 
