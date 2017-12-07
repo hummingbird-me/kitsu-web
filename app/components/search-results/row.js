2c8984a5659fe726e4f04a277a5228cb355b5c96
Peter Lejeck <peter.lejeck@gmail.com>
2017-12-06 21:09:06 -0800

    Break search-results row into separate component

---
+++ app/components/search-results/row.js
@@ -0,0 +1,42 @@
+import Component from '@ember/component';
+import { get, computed } from '@ember/object';
+import { alias, or } from '@ember/object/computed';
+
+export default Component.extend({
+  kind: alias('item.kind'),
+  imageUrl: or('item.avatar', 'item.posterImage'),
+  title: or('item.name', 'item.canonicalTitle'),
+
+  imageClass: computed('item.kind', function () {
+    const kind = get(this, 'item.kind');
+    switch (kind) {
+      case 'user':
+      case 'group':
+        return `is-avatar-${kind}s`;
+      default:
+        return '';
+    }
+  }),
+
+  route: computed('item.kind', function () {
+    const kind = get(this, 'item.kind');
+    switch (kind) {
+      case 'user':
+        return 'users.index';
+      case 'group':
+        return 'groups.group.group-page.index';
+      default:
+        return `${kind}.show`;
+    }
+  }),
+
+  tags: computed('item.kind', 'item.subtype', 'item.nsfw', function () {
+    const kind = get(this, 'item.kind');
+    const subtype = get(this, 'item.subtype');
+    const nsfw = get(this, 'item.nsfw');
+    const out = [];
+    if (subtype) out.push(`media-shared.types.${kind}.${subtype.toLowerCase()}`);
+    if (nsfw) out.push('groups.nsfw');
+    return out;
+  })
+});
