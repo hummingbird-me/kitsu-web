7a14c27730d6d5eb338e7ed1dbb9603d0dfe08a3
Thomas McNiven <hello@vevix.net>
2018-05-08 20:22:57 +1000

    Update `authorize` usage

--- app/authenticators/oauth2.js
+++ app/authenticators/oauth2.js
@@ -1,5 +1,5 @@
 import OAuth2PasswordGrant from 'ember-simple-auth/authenticators/oauth2-password-grant';
-import { get, set } from '@ember/object';
+import { set } from '@ember/object';
 import { inject as service } from '@ember/service';
 import config from 'client/config/environment';
 
@@ -12,12 +12,5 @@ export default OAuth2PasswordGrant.extend({
     const host = config.kitsu.APIHost || '';
     set(this, 'serverTokenEndpoint', `${host}/api/oauth/token`);
     set(this, 'serverTokenRevocationEndpoint', `${host}/api/oauth/revoke`);
-  },
-
-  makeRequest(url, data, headers = {}) {
-    get(this, 'session').authorize('authorizer:application', (name, value) => {
-      headers[name] = value; // eslint-disable-line
-    });
-    return this._super(url, data, headers);
   }
 });
