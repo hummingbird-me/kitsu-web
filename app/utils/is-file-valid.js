5ffab128c0c5aad5e35ae1085f2639888a19ea6a
Thomas McNiven <hello@vevix.net>
2018-06-24 06:05:03 +1000

    Update is-file-valid.js
    - Bump to 15 mb

--- app/utils/is-file-valid.js
+++ app/utils/is-file-valid.js
@@ -7,7 +7,7 @@ export default function isFileValid(file, accept) {
   }
   const size = get(file, 'size');
   const sizeInMb = size / 1000000;
-  if (sizeInMb > 5) {
+  if (sizeInMb > 15) {
     return false;
   }
   return true;
