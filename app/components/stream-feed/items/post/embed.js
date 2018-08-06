92ecd34adfd3677acea301c9c8958e9b1c1c69fc
Thomas McNiven <hello@vevix.net>
2018-08-07 04:41:22 +1000

    Don't manipulate the DOM for embeds.
    - Backport from v4

--- app/components/stream-feed/items/post/embed.js
+++ app/components/stream-feed/items/post/embed.js
@@ -5,6 +5,26 @@ import { htmlSafe } from '@ember/string';
 export default Component.extend({
   classNames: ['stream-content-embed'],
   attributeBindings: ['style'],
+  shouldRenderVideo: false,
+
+  isVideo: computed('embed.{kind,video}', function() {
+    return this.embed.kind && this.embed.kind.includes('video')
+      && (typeof this.embed.video === 'object');
+  }),
+
+  requiresIFrame: computed('isVideo', 'embed.video.type', function() {
+    if (!this.isVideo) { return false; }
+    return this.embed.video.type !== 'video/mp4';
+  }),
+
+  videoSrc: computed('isVideo', 'embed.{site.name,video.url}', function() {
+    if (!this.isVideo) { return null; }
+    const { url } = this.embed.video;
+    if (this.embed.site.name === 'YouTube') {
+      return url.includes('?') ? `${url}&autoplay=1` : `${url}?autoplay=1`;
+    }
+    return url;
+  }),
 
   style: computed('embed.video.{width,height}', function() {
     const kind = this.get('embed.kind');
@@ -36,22 +56,7 @@ export default Component.extend({
 
   actions: {
     revealVideo() {
-      const siteName = this.get('embed.site.name');
-      const video = this.get('embed.video');
-      const { type, url } = video;
-      let src = url;
-      if (siteName === 'YouTube') {
-        src = src.includes('?') ? `${src}&autoplay=1` : `${src}?autoplay=1`;
-      }
-      const attrs = `src=${src} class="embed-video"`;
-      let embed;
-      if (type === 'video/mp4') {
-        embed = `<video controls autoplay ${attrs}></video>`;
-      } else {
-        embed = `<iframe ${attrs}></iframe>`;
-      }
-      const element = this.element.querySelector('.embed-video');
-      element.outerHTML = embed;
+      this.set('shouldRenderVideo', true);
     }
   }
 });
