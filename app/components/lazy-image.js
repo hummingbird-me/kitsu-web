2874d7313392f222b09581d3d1eb4a7c70416a3f
Thomas McNiven <hello@vevix.net>
2018-08-07 05:31:15 +1000

    Use lazysizes for image load
    - v4

--- app/components/lazy-image.js
+++ app/components/lazy-image.js
@@ -1,81 +1,13 @@
 import Component from '@ember/component';
-import { get, set } from '@ember/object';
-import { scheduleOnce, next } from '@ember/runloop';
-import observerManager from '../utils/observer-manager';
+import { scheduleOnce } from '@ember/runloop';
 
 export default Component.extend({
   classNames: ['lazy-image'],
-  classNameBindings: ['isLoaded'],
-  isLoaded: false,
 
-  didReceiveAttrs() {
-    this._super(...arguments);
+  didUpdateAttrs() {
     scheduleOnce('afterRender', () => {
-      this._setupViewport();
+      this.element.querySelector('img').classList.remove('lazyload');
+      this.element.querySelector('img').classList.add('lazyload');
     });
-  },
-
-  willDestroyElement() {
-    this._super(...arguments);
-    this._teardownViewport();
-  },
-
-  /**
-   * Swap `data-*` lazy attributes for their counterpart
-   */
-  _swapAttributes() {
-    const image = this.$('img');
-    const source = image.attr('data-src');
-    if (source) {
-      image.attr('src', source);
-      image.removeAttr('data-src');
-    }
-  },
-
-  /**
-   * Swap the component loading state based on the loaded state of the image itself
-   */
-  _loadImage() {
-    const [image] = this.$('img');
-    if (image.complete) {
-      set(this, 'isLoaded', true);
-    } else {
-      const fn = () => {
-        next(() => {
-          if (get(this, 'isDestroyed') || get(this, 'isDestroying')) { return; }
-          set(this, 'isLoaded', true);
-        });
-      };
-      image.onload = () => { fn(); };
-      image.onerror = () => { fn(); };
-    }
-  },
-
-  _setupViewport() {
-    const options = { root: null, rootMargin: this._getRootMargin(), threshold: 0 };
-    const observe = observerManager(options);
-    this.unobserve = observe(this.element, (entry) => {
-      if (entry && entry.isIntersecting) {
-        if (get(this, 'isDestroyed') || get(this, 'isDestroying')) { return; }
-        this._swapAttributes();
-        this._loadImage();
-        if (this.unobserve) { this.unobserve(); }
-      }
-    });
-  },
-
-  _teardownViewport() {
-    if (this.unobserve) { this.unobserve(); }
-  },
-
-  _getRootMargin() {
-    let rootMargin = get(this, 'rootMargin');
-    rootMargin = Object.assign({
-      top: 0,
-      left: 0,
-      right: 0,
-      bottom: 300
-    }, rootMargin);
-    return `${rootMargin.top}px ${rootMargin.right}px ${rootMargin.bottom}px ${rootMargin.left}px`;
   }
 });
