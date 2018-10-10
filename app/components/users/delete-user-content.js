8de2fbd368ba1935b616193187dde4138dea3032
Emma Lejeck <nuck@kitsu.io>
2018-10-10 13:39:52 -0700

    Add modal for deleting user content

---
+++ app/components/users/delete-user-content.js
@@ -0,0 +1,29 @@
+import Component from '@ember/component';
+import { get } from '@ember/object';
+import { inject as service } from '@ember/service';
+import { task } from 'ember-concurrency';
+
+export default Component.extend({
+  ajax: service(),
+  kinds: {
+    posts: false,
+    comments: false,
+    reactions: false
+  },
+
+  deleteContent: task(function* () {
+    const userId = get(this, 'user.id');
+    const kind = Object.keys(get(this, 'kinds')).filter(k => get(this, `kinds.${k}`));
+    yield get(this, 'ajax').request(`/users/${userId}/_nuke`, {
+      method: 'POST',
+      dataType: 'json',
+      data: { kind }
+    });
+  }).drop(),
+
+  actions: {
+    deleteContent() {
+      get(this, 'deleteContent').perform();
+    }
+  }
+});
