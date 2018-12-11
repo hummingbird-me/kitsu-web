bc3c6692f74cb7d24cb8ba63f721c5cac0369177
Emma Lejeck <nuck@kitsu.io>
2018-12-10 19:47:26 -0800

    Update time-spent component to match proper structure

--- app/components/stats/time-spent.js
+++ app/components/stats/time-spent.js
@@ -2,17 +2,28 @@ import Component from '@ember/component';
 import { get, computed } from '@ember/object';
 import moment from 'moment';
 
-const UNITS = ['years', 'months', 'days', 'hours', 'minutes'];
+const UNITS = ['years', 'months', 'days', 'hours', 'minutes', 'seconds'];
 
 export default Component.extend({
-  time: 0,
+  stat: {},
+  kind: 'anime',
 
-  breakdown: computed('time', function () {
+  breakdown: computed('stat.time', function () {
     // Break down the duration into a list of constituent units
-    const time = moment.duration(get(this, 'time'), 'minutes');
-    const breakdown = UNITS.map(unit => ({ name: unit, count: time[unit]() }));
+    const time = moment.duration(get(this, 'stat.time'), 'seconds');
+    const breakdown = UNITS.map(unit => ({ name: unit, count: time.get(unit) }));
     // Pick the first three non-zero units
-    const nonzeroBreakdown = breakdown.filter(({ count }) => count > 0);
-    return nonzeroBreakdown.slice(0, 3);
+    return breakdown.filter(({ count }) => count > 0).slice(0, 3);
+  }),
+
+  primaryUnit: computed('stat.time', function () {
+    const time = moment.duration(get(this, 'stat.time'), 'seconds');
+    // Ideally we would use for..of but performance is still not good
+    for (let i = 0; i < UNITS.length; i += 1) {
+      const unitTime = time.as(UNITS[i]);
+      if (unitTime > 1) {
+        return { name: UNITS[i], count: unitTime };
+      }
+    }
   })
 });
