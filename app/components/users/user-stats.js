85baa45ba9974f16e019f94c5dce51e4154b68c4
Matthew Dias <matthewdias@me.com>
2018-12-12 16:38:06 -0600

    Hide 0-value stats

--- app/components/users/user-stats.js
+++ app/components/users/user-stats.js
@@ -5,9 +5,22 @@ import { camelize } from '@ember/string';
 export default Component.extend({
   normStats: computed('user.stats', function() {
     const stats = get(this, 'user.stats');
-    return stats.reduce((acc, stat) => {
+    const normed = stats.reduce((acc, stat) => {
       const camelKind = camelize(get(stat, 'kind'));
       return { ...acc, [camelKind]: stat };
     }, {});
+
+    if (normed.animeAmountConsumed && normed.animeAmountConsumed.statsData.time < 1) {
+      delete normed.animeAmountConsumed;
+    }
+    if (normed.mangaAmountConsumed && normed.mangaAmountConsumed.statsData.units < 1) {
+      delete normed.mangaAmountConsumed;
+    }
+    if (normed.animeCategoryBreakdown && normed.animeCategoryBreakdown.statsData.total < 1) {
+      delete normed.animeCategoryBreakdown;
+    }
+    if (normed.mangaCategoryBreakdown && normed.mangaCategoryBreakdown.statsData.total < 1) {
+      delete normed.mangaCategoryBreakdown;
+    }
   }).readOnly()
 });
