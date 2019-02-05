204aca540b26048709b16afa8bb8b54aa10de4cb
Emma Lejeck <nuck@kitsu.io>
2019-02-04 18:15:30 -0800

    Remove test for utils/getter

--- tests/unit/utils/getter-test.js
+++
@@ -1,9 +0,0 @@
-import getter from 'client/utils/getter';
-import { module, test } from 'qunit';
-
-module('Unit | Utility | getter');
-
-test('it returns the value on get', function(assert) {
-  const result = getter(() => 5);
-  assert.equal(5, result.get());
-});
