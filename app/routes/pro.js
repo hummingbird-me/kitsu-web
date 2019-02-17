c7a3ecbb44e20a88e3a7833d7466ca47c2aa6a3a
Emma Lejeck <nuck@kitsu.io>
2019-02-16 20:18:05 -0800

    Set up initial styles for Pro page

--- app/routes/pro.js
+++ app/routes/pro.js
@@ -1,4 +1,19 @@
 import Route from '@ember/routing/route';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import DataErrorMixin from 'client/mixins/routes/data-error';
+import jQuery from 'jquery';
 
-export default Route.extend({
+export default Route.extend(AuthenticatedRouteMixin, DataErrorMixin, {
+  authenticationRoute: 'dashboard',
+  titleToken: 'Pro',
+
+  activate() {
+    this._super(...arguments);
+    jQuery('body').addClass('pro-page');
+  },
+
+  deactivate() {
+    this._super(...arguments);
+    jQuery('body').removeClass('pro-page');
+  }
 });
