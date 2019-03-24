719ed5390fa125608d6b827a5fb89213c5ef2c23
Emma Lejeck <nuck@kitsu.io>
2019-03-24 13:24:22 -0700

    Allow downloading MAL XML files

--- app/controllers/settings/exports.js
+++ app/controllers/settings/exports.js
@@ -1,6 +1,24 @@
 import Controller from '@ember/controller';
 import { alias } from '@ember/object/computed';
+import { computed, get } from '@ember/object';
+import { inject as service } from '@ember/service';
+import config from 'client/config/environment';
 
 export default Controller.extend({
+  session: service('session'),
   account: alias('model.taskInstance.value'),
+
+  downloadUrl: computed('session.isAuthenticated', function () {
+    const token = get(this, 'session.data.authenticated.access_token');
+
+    return `${config.kitsu.APIHost}/api/edge/library-entries/_xml?access_token=${token}`;
+  }),
+
+  animeDownloadUrl: computed('downloadUrl', function () {
+    return `${this.downloadUrl}&kind=anime`;
+  }),
+
+  mangaDownloadUrl: computed('downloadUrl', function () {
+    return `${this.downloadUrl}&kind=manga`;
+  })
 });
