1373f973eeed0e8b431259ff0da795cff674f959
Uri <Uricorn@users.noreply.github.com>
2020-03-11 00:50:04 +0200

    Adding Brazilian Portuguese translation

--- app/utils/languages.js
+++ app/utils/languages.js
@@ -2,5 +2,6 @@ export default [
   { id: 'id-id', text: 'Bahasa Indonesia' },
   { id: 'en-us', text: 'English' },
   { id: 'es-es', text: 'Español' },
-  { id: 'fr-fr', text: 'Français' }
+  { id: 'fr-fr', text: 'Français' },
+  { id: 'pt-br', text: 'Português (Brasileiro)' }
 ];
