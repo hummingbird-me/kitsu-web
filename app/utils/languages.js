484b55845e6fb5af68e66de294486fd428fe0c64
James Harris <wopian@wopian.me>
2020-03-11 02:46:04 +0000

    feat: add British English

--- app/utils/languages.js
+++ app/utils/languages.js
@@ -1,7 +1,8 @@
 export default [
   { id: 'id-id', text: 'Bahasa Indonesia' },
-  { id: 'en-us', text: 'English' },
+  { id: 'en-us', text: 'English (United States)' },
   { id: 'es-es', text: 'Español' },
   { id: 'fr-fr', text: 'Français' },
-  { id: 'pt-br', text: 'Português (Brasileiro)' }
+  { id: 'pt-br', text: 'Português (Brasileiro)' },
+  { id: 'es-gb', text: 'English (United Kingdom)' }
 ];
