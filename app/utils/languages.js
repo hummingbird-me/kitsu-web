62c79e5df912f0df5c91553ddd76d1139c1e185a
Uri <Uricorn@users.noreply.github.com>
2020-03-14 22:58:13 +0200

    Add Russian Language (#952)

--- app/utils/languages.js
+++ app/utils/languages.js
@@ -4,5 +4,6 @@ export default [
   { id: 'es-es', text: 'Español' },
   { id: 'fr-fr', text: 'Français' },
   { id: 'pt-br', text: 'Português (Brasileiro)' },
-  { id: 'en-gb', text: 'English (British)' }
+  { id: 'en-gb', text: 'English (British)' },
+  { id: 'ru-ru', text: 'Русский' }
 ];
