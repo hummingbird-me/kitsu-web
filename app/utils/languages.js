95c5231d1a0c4a895cfd7bf5985af6dcb407ab5c
Uri <Uricorn@users.noreply.github.com>
2020-03-15 21:22:45 +0200

    Add Portuguese Language (#954)
    * Add Portuguese Language
    
    * Removed trailing spaces.

--- app/utils/languages.js
+++ app/utils/languages.js
@@ -3,6 +3,7 @@ export default [
   { id: 'en-us', text: 'English (American)' },
   { id: 'es-es', text: 'Español' },
   { id: 'fr-fr', text: 'Français' },
+  { id: 'pt-pt', text: 'Português' },
   { id: 'pt-br', text: 'Português (Brasileiro)' },
   { id: 'en-gb', text: 'English (British)' },
   { id: 'ru-ru', text: 'Русский' }
