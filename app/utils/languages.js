684db96eb02cb1a6be78f429414021d620604e9e
James Harris <wopian@wopian.me>
2020-03-15 20:56:47 +0000

    Revert "Add Russian/Portuguese Language" (#955)
    * Revert "Add Russian Language (#952)"
    
    This reverts commit 62c79e5df912f0df5c91553ddd76d1139c1e185a.
    
    * Update languages.js

--- app/utils/languages.js
+++ app/utils/languages.js
@@ -3,8 +3,6 @@ export default [
   { id: 'en-us', text: 'English (American)' },
   { id: 'es-es', text: 'Español' },
   { id: 'fr-fr', text: 'Français' },
-  { id: 'pt-pt', text: 'Português' },
   { id: 'pt-br', text: 'Português (Brasileiro)' },
-  { id: 'en-gb', text: 'English (British)' },
-  { id: 'ru-ru', text: 'Русский' }
+  { id: 'en-gb', text: 'English (British)' }
 ];
