ad6aaba0a0a457cdc3f2c01dbd14611a90ac912c
James Harris <wopian@wopian.me>
2020-04-04 21:20:27 +0100

    feat(media): combine sidebar details with main content on tablet/mobile

---
+++ app/breakpoints.js
@@ -0,0 +1,7 @@
+export default {
+  mobileSmall: '(max-width: 432px)',
+  mobile: '(max-width: 600px)',
+  tabletSmall: '(max-width: 768px)',
+  tablet: '(max-width: 991px)',
+  desktop: '(min-width: 992px)'
+};
