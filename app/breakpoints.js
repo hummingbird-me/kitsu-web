a7b2f6351332aa210614173003eb19243db3d3ac
James Harris <wopian@wopian.me>
2020-04-05 17:24:07 +0100

    refactor: better use of space on laptops/tablets

--- app/breakpoints.js
+++ app/breakpoints.js
@@ -4,5 +4,7 @@ export default {
   mobileLarge: '(max-width: 600px)',
   tabletSmall: '(max-width: 768px)',
   tablet: '(max-width: 991px)',
-  desktop: '(min-width: 992px)'
+  desktop: '(min-width: 992px)',
+  profileLeftRightCollapse: '(max-width: 1199px)',
+  profileRightCollapse: '(min-width: 992px) and (max-width: 1199px)'
 };
