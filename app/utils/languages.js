ea2fdbba63416b5bd7f6d01b4da288133b729db6
James Harris <wopian@wopian.me>
2020-04-08 23:23:53 +0100

    chore: label WIP languages with crowdin language name
    Easier to search for when enabling a newly translated language.

--- app/utils/languages.js
+++ app/utils/languages.js
@@ -4,58 +4,58 @@ export default [
   // { id: 'en-ca', text: 'English (Canadian)' },
   { id: 'en-gb', text: 'English (British)' },
   // { id: 'af-za', text: 'Afrikaans' },
-  // { id: 'ar-001', text: 'العربية' }, // rtl
-  // { id: 'bn-bd', text: 'বাংলা (বাংলাদেশ)' },
-  // { id: 'bg-bg', text: 'Български' },
-  // { id: 'ca-es', text: 'Català' },
-  // { id: 'zh-cn', text: '中文 (简体)' },
-  // { id: 'zh-tw', text: '中文 (繁體)' },
-  // { id: 'hr-hr', text: 'Hrvatski' },
-  // { id: 'cs-cz', text: 'Česky' },
-  // { id: 'da-dk', text: 'Dansk' },
-  // { id: 'nl-nl', text: 'Nederlands' },
-  // { id: 'et-ee', text: 'Eesti Keel' },
-  // { id: 'fi-fi', text: 'Suomi' },
+  // { id: 'ar-001', text: 'العربية' }, // Arabic rtl
+  // { id: 'bn-bd', text: 'বাংলা (বাংলাদেশ)' }, // Bengali
+  // { id: 'bg-bg', text: 'Български' }, // Bulgarian
+  // { id: 'ca-es', text: 'Català' }, // Catalan
+  // { id: 'zh-cn', text: '中文 (简体)' }, // Chinese Simplified
+  // { id: 'zh-tw', text: '中文 (繁體)' }, // Chinese Traditional
+  // { id: 'hr-hr', text: 'Hrvatski' }, // Croatian
+  // { id: 'cs-cz', text: 'Česky' }, // Czech
+  // { id: 'da-dk', text: 'Dansk' }, // Danish
+  // { id: 'nl-nl', text: 'Nederlands' }, // Dutch
+  // { id: 'et-ee', text: 'Eesti Keel' }, // Estonian
+  // { id: 'fi-fi', text: 'Suomi' }, // Finnish
   { id: 'fr-fr', text: 'Français' },
   // { id: 'fr-ca', text: 'Français (Canadien)' },
-  // { id: 'de-de', text: 'Deutsch' },
-  // { id: 'el-gr', text: 'Ελληνικά' },
-  // { id: 'he-il', text: 'עברית' }, // rtl
-  // { id: 'hi-in', text: 'हिन्दी' },
-  // { id: 'hu-hu', text: 'Magyar' },
-  // { id: 'is-is', text: 'Íslenska' },
+  // { id: 'de-de', text: 'Deutsch' }, // German
+  // { id: 'el-gr', text: 'Ελληνικά' }, // Greek
+  // { id: 'he-il', text: 'עברית' }, // Hebrew rtl
+  // { id: 'hi-in', text: 'हिन्दी' }, // Hindi
+  // { id: 'hu-hu', text: 'Magyar' }, Hungarian
+  // { id: 'is-is', text: 'Íslenska' }, // Icelandic
   { id: 'id-id', text: 'Bahasa Indonesia' },
-  // { id: 'ga-ie', text: 'Gaeilge' },
+  // { id: 'ga-ie', text: 'Gaeilge' }, // Irish
   { id: 'it-it', text: 'Italiano' },
-  // { id: 'ja-jp', text: '日本語' },
-  // { id: 'kk-kz', text: 'Қазақ' },
-  // { id: 'ko-kr', text: '한국어' },
-  // { id: 'lv-lv', text: 'Latviešu' },
-  // { id: 'lt-lt', text: 'Lietuvių Kalba' },
-  // { id: 'ms-my', text: 'Bahasa Melayu' },
-  // { id: 'mt-mt', text: 'Malti' },
+  // { id: 'ja-jp', text: '日本語' }, // Japanese
+  // { id: 'kk-kz', text: 'Қазақ' }, // Kazakh
+  // { id: 'ko-kr', text: '한국어' }, // Korean
+  // { id: 'lv-lv', text: 'Latviešu' }, // Latvian
+  // { id: 'lt-lt', text: 'Lietuvių Kalba' }, // Lithuanian
+  // { id: 'ms-my', text: 'Bahasa Melayu' }, // Malay
+  // { id: 'mt-mt', text: 'Malti' }, // Maltese
   // { id: 'nb-no', text: 'Norsk (Bokmål)' },
   // { id: 'nn-no', text: 'Norsk (Nynorsk)' },
-  // { id: 'fa-ir', text: 'فارسی' }, // rtl
+  // { id: 'fa-ir', text: 'فارسی' }, // Persian rtl
   // { id: 'pl-pl', text: 'Polski' },
   { id: 'pt-pt', text: 'Português' },
   { id: 'pt-br', text: 'Português (Brasileiro)' },
-  // { id: 'ro-ro', text: 'Română' },
+  // { id: 'ro-ro', text: 'Română' }, // Romanian
   { id: 'ru-ru', text: 'Русский' },
-  // { id: 'sc-it', text: 'Sardu' },
+  // { id: 'sc-it', text: 'Sardu' }, // Sardinian
   // { id: 'sco-gb', text: 'Scots' },
-  // { id: 'sr-cs', text: 'Srpski' },
-  // { id: 'sr-sp', text: 'Српски' },
-  // { id: 'sk-sk', text: 'Slovenčina' },
-  // { id: 'sl-sl', text: 'Slovenščina' },
+  // { id: 'sr-cs', text: 'Srpski' }, // Serbian Latin
+  // { id: 'sr-sp', text: 'Српски' }, // Serbian Cryllic
+  // { id: 'sk-sk', text: 'Slovenčina' }, // Slovak
+  // { id: 'sl-sl', text: 'Slovenščina' }, // Slovenian
   { id: 'es-es', text: 'Español' },
-  // { id: 'sv-se', text: 'Svenska' },
+  // { id: 'sv-se', text: 'Svenska' }, // Swedish
   // { id: 'tl-ph', text: 'Wikang Tagalog' },
-  // { id: 'th-th', text: 'ไทย' },
-  // { id: 'tr-tr', text: 'Türkçe' },
-  // { id: 'uk-ua', text: 'Українська' },
-  // { id: 'ur-in', text: 'اردو (پاکستانی)' },
-  // { id: 'ur-pk', text: 'اردو (ہندوستانی)' },
+  // { id: 'th-th', text: 'ไทย' }, // Thai
+  // { id: 'tr-tr', text: 'Türkçe' }, // Turkish
+  // { id: 'uk-ua', text: 'Українська' }, // Ukrainian
+  // { id: 'ur-in', text: 'اردو (پاکستانی)' }, // Urdu Indian
+  // { id: 'ur-pk', text: 'اردو (ہندوستانی)' }, // Urdu Pakistani
   // { id: 'vi-vn', text: 'Việtnam' },
-  // { id: 'cy-gb', text: 'Cymraeg' }
+  // { id: 'cy-gb', text: 'Cymraeg' } // Welsh
 ];
