a33ea2e7351370488e58aac5b7d54027a392a46c
James Harris <wopian@wopian.me>
2020-05-05 18:44:33 +0100

    Update languages.js

--- app/utils/languages.js
+++ app/utils/languages.js
@@ -3,44 +3,44 @@ export default [
   // { id: 'en-au', text: 'English (Australian)' },
   // { id: 'en-ca', text: 'English (Canadian)' },
   { id: 'en-gb', text: 'English (British)' },
-  // { id: 'af-za', text: 'Afrikaans' },
-  // { id: 'ar-001', text: 'العربية' }, // Arabic rtl
+  // { id: 'af-za', text: 'Afrikaans' }, [1/1]
+  // { id: 'ar-001', text: 'العربية' }, // Arabic rtl [21/20]
   // { id: 'bn-bd', text: 'বাংলা (বাংলাদেশ)' }, // Bengali
-  // { id: 'bg-bg', text: 'Български' }, // Bulgarian
+  // { id: 'bg-bg', text: 'Български' }, // Bulgarian [4/4]
   { id: 'ca-es', text: 'Català' },
-  // { id: 'zh-cn', text: '中文 (简体)' }, // Chinese Simplified
+  // { id: 'zh-cn', text: '中文 (简体)' }, // Chinese Simplified [21/20]
   // { id: 'zh-tw', text: '中文 (繁體)' }, // Chinese Traditional
   // { id: 'hr-hr', text: 'Hrvatski' }, // Croatian
-  // { id: 'cs-cz', text: 'Česky' }, // Czech
+  // { id: 'cs-cz', text: 'Česky' }, // Czech [11/11]
   // { id: 'da-dk', text: 'Dansk' }, // Danish
-  // { id: 'nl-nl', text: 'Nederlands' }, // Dutch
-  // { id: 'et-ee', text: 'Eesti Keel' }, // Estonian
+  // { id: 'nl-nl', text: 'Nederlands' }, // Dutch [80/31 - past 50, waiting on approval]
+  // { id: 'et-ee', text: 'Eesti Keel' }, // Estonian [14/0]
   // { id: 'fi-fi', text: 'Suomi' }, // Finnish
   { id: 'fr-fr', text: 'Français' },
   // { id: 'fr-ca', text: 'Français (Canadien)' },
   { id: 'de-de', text: 'Deutsch' },
-  // { id: 'el-gr', text: 'Ελληνικά' }, // Greek
+  // { id: 'el-gr', text: 'Ελληνικά' }, // Greek [2/1]
   // { id: 'he-il', text: 'עברית' }, // Hebrew rtl
-  // { id: 'hi-in', text: 'हिन्दी' }, // Hindi
+  // { id: 'hi-in', text: 'हिन्दी' }, // Hindi [1/0]
   { id: 'hu-hu', text: 'Magyar' },
   // { id: 'is-is', text: 'Íslenska' }, // Icelandic
   { id: 'id-id', text: 'Bahasa Indonesia' },
   // { id: 'ga-ie', text: 'Gaeilge' }, // Irish
   { id: 'it-it', text: 'Italiano' },
-  // { id: 'ja-jp', text: '日本語' }, // Japanese
+  // { id: 'ja-jp', text: '日本語' }, // Japanese [9/8]
   // { id: 'kk-kz', text: 'Қазақ' }, // Kazakh
   // { id: 'ko-kr', text: '한국어' }, // Korean
   // { id: 'lv-lv', text: 'Latviešu' }, // Latvian
-  // { id: 'lt-lt', text: 'Lietuvių Kalba' }, // Lithuanian
+  // { id: 'lt-lt', text: 'Lietuvių Kalba' }, // Lithuanian [14/0]
   // { id: 'ms-my', text: 'Bahasa Melayu' }, // Malay
   // { id: 'mt-mt', text: 'Malti' }, // Maltese
-  // { id: 'nb-no', text: 'Norsk (Bokmål)' },
+  // { id: 'nb-no', text: 'Norsk (Bokmål)' }, // [24/24]
   // { id: 'nn-no', text: 'Norsk (Nynorsk)' },
   // { id: 'fa-ir', text: 'فارسی' }, // Persian rtl
-  // { id: 'pl-pl', text: 'Polski' },
+  // { id: 'pl-pl', text: 'Polski' }, // [10/7]
   { id: 'pt-pt', text: 'Português' },
   { id: 'pt-br', text: 'Português (Brasileiro)' },
-  // { id: 'ro-ro', text: 'Română' }, // Romanian
+  // { id: 'ro-ro', text: 'Română' }, // Romanian [6/2]
   { id: 'ru-ru', text: 'Русский' },
   // { id: 'sc-it', text: 'Sardu' }, // Sardinian
   // { id: 'sco-gb', text: 'Scots' },
@@ -50,10 +50,10 @@ export default [
   // { id: 'sl-sl', text: 'Slovenščina' }, // Slovenian
   { id: 'es-es', text: 'Español' },
   // { id: 'sv-se', text: 'Svenska' }, // Swedish
-  // { id: 'tl-ph', text: 'Wikang Tagalog' },
+  // { id: 'tl-ph', text: 'Wikang Tagalog' }, // [76/2 - past 50, waiting on approval] 
   // { id: 'th-th', text: 'ไทย' }, // Thai
-  // { id: 'tr-tr', text: 'Türkçe' }, // Turkish
-  // { id: 'uk-ua', text: 'Українська' }, // Ukrainian
+  // { id: 'tr-tr', text: 'Türkçe' }, // Turkish [1/0]
+  // { id: 'uk-ua', text: 'Українська' }, // Ukrainian [14/13]
   // { id: 'ur-in', text: 'اردو (پاکستانی)' }, // Urdu Indian
   // { id: 'ur-pk', text: 'اردو (ہندوستانی)' }, // Urdu Pakistani
   // { id: 'vi-vn', text: 'Việtnam' },
