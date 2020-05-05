ac51b6e60d14a7ea697854cd0f97c994a9437567
James Harris <wopian@wopian.me>
2020-05-05 22:43:30 +0100

    build: update ember-intl to 5.0.6

--- config/ember-intl.js
+++ config/ember-intl.js
@@ -1,7 +1,6 @@
 /* eslint-env node */
 module.exports = function() {
   return {
-    disablePolyfill: true,
     publicOnly: true,
     requiresTranslation(key, locale) {
       return locale === 'en-us';
