c75fdae97e57054dbc051253712fea85f44a8e41
James Harris <wopian@wopian.me>
2020-05-09 20:49:50 +0100

    chore: add notice ember-one-way-controls is deprecated in expanding-textarea

--- app/components/expanding-textarea.js
+++ app/components/expanding-textarea.js
@@ -4,6 +4,15 @@ import { invokeAction } from 'ember-invoke-action';
 import { later } from '@ember/runloop';
 /* global autosize */
 
+/* TODO: ember-one-way-controls is deprecated, use native textarea for expanding-textarea instead.
+Also remove height fix from shame.css (.report-content-input) by using textarea rows attribute
+Migration: https://github.com/DavyJonesLocker/ember-one-way-controls#migrating
+{{! old }}
+{{one-way-textarea myValue update=(action (mut myValue))}}
+{{! new }}
+<textarea value={{myValue}} oninput={{action (mut myValue) value="target.value"}}></textarea>
+*/
+
 export default OneWayTextAreaComponent.extend({
   autocomplete: 'ignore',
 
