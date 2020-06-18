750004b7155415ed19ee883b88865dc6b6ce9b05
James Harris <wopian@wopian.me>
2020-06-18 02:39:34 +0100

    refactor: don't redirect 404 pages
    This UX sin will no longer prevail

--- app/routes/not-found.js
+++ app/routes/not-found.js
@@ -1,13 +1,6 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  // This results in redirecting to `/404` if that isn't the current URL.
-  redirect() {
-    if (window.location.pathname !== '/404') {
-      this.replaceWith('/404');
-    }
-  },
-
   headTags() {
     return [{
       type: 'meta',
