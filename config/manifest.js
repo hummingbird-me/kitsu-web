350b3ddf5e0779a3fc1fb169fd4f9443657ff8e8
James Harris <wopian@wopian.me>
2020-07-08 06:18:09 +0100

    feat: associate client with android and ios apps

--- config/manifest.js
+++ config/manifest.js
@@ -7,19 +7,29 @@ module.exports = function() {
     orientation: 'portrait',
     background_color: '#332532',
     theme_color: '#332532',
-    icons: [{
-      src: '/android-chrome-192x192.png',
-      sizes: '192x192',
-      type: 'image/png'
-    }, {
-      src: '/android-chrome-512x512.png',
-      sizes: '512x512',
-      type: 'image/png'
-    }, {
-      src: '/apple-touch-icon.png',
-      sizes: '180x180',
-      type: 'image/png',
-      targets: ['apple']
-    }]
+    icons: [
+      {
+        src: '/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        src: '/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      },
+      {
+        src: '/apple-touch-icon.png',
+        sizes: '180x180',
+        type: 'image/png',
+        targets: ['apple']
+      }
+    ],
+    related_applications: [
+      {
+        platform: 'play',
+        id: 'com.everfox.animetrackerandroid'
+      }
+    ]
   };
 };
