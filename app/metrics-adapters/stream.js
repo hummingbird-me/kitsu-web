4a147d656e7ad4ad863270beedf4b619450b8d5e
James Harris <wopian@wopian.me>
2020-09-05 01:49:12 +0100

    fix: don't report stream analytics on development
    Stream now puts errors in console for non-kitsu.io requests

--- app/metrics-adapters/stream.js
+++ app/metrics-adapters/stream.js
@@ -10,13 +10,15 @@ export default BaseAdapter.extend({
   },
 
   init() {
-    const environment = Config.kitsu.isStaging ? 'staging' : Config.environment;
-    const config = get(this, `config.${environment}`);
-    if (canUseDOM) {
-      // eslint-disable-next-line
-      !function(t,e){t("StreamAnalytics","https://d2j1fszo1axgmp.cloudfront.net/2.6.0/stream-analytics.min.js",e)}(function(t,e,n){var s,i,r;n["_"+t]={},n[t]=function(e){n["_"+t].clients=n["_"+t].clients||{},n["_"+t].clients[e.apiKey]=this,this._config=e};var c=function(t){return function(){return this["_"+t]=this["_"+t]||[],this["_"+t].push(arguments),this}};s=["setUser","trackImpression","trackEngagement"];for(var a=0;a<s.length;a++){var o=s[a];n[t].prototype[o]=c(o)}i=document.createElement("script"),i.async=!0,i.src=e,r=document.getElementsByTagName("script")[0],r.parentNode.insertBefore(i,r)},window);
-      const client = new window.StreamAnalytics(config);
-      set(this, 'client', client);
+    if (Config.kitsu.isProduction) {
+      const environment = Config.kitsu.isStaging ? 'staging' : Config.environment;
+      const config = get(this, `config.${environment}`);
+      if (canUseDOM) {
+        // eslint-disable-next-line
+        !function(t,e){t("StreamAnalytics","https://d2j1fszo1axgmp.cloudfront.net/2.6.0/stream-analytics.min.js",e)}(function(t,e,n){var s,i,r;n["_"+t]={},n[t]=function(e){n["_"+t].clients=n["_"+t].clients||{},n["_"+t].clients[e.apiKey]=this,this._config=e};var c=function(t){return function(){return this["_"+t]=this["_"+t]||[],this["_"+t].push(arguments),this}};s=["setUser","trackImpression","trackEngagement"];for(var a=0;a<s.length;a++){var o=s[a];n[t].prototype[o]=c(o)}i=document.createElement("script"),i.async=!0,i.src=e,r=document.getElementsByTagName("script")[0],r.parentNode.insertBefore(i,r)},window);
+        const client = new window.StreamAnalytics(config);
+        set(this, 'client', client);
+      }
     }
   },
 
