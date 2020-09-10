ea0cef737e59a4f48ec5319083f6cd09e37709cb
James Harris <wopian@wopian.me>
2020-09-10 02:41:07 +0100

    fix: extract event listeners to a named function

--- app/components/application/nav-search.js
+++ app/components/application/nav-search.js
@@ -12,37 +12,39 @@ export default Component.extend({
     return `site-search__input ${isActive ? 'active' : ''}`;
   }).readOnly(),
 
+  _searchEventListener(event) {
+    const target = get(event, 'target');
+    const id = `#${get(this, 'elementId')}`;
+    const isChild = matches(target, `${id} *, ${id}`);
+    const isPopover = matches(target, '.navbar-search-results *, .navbar-search-results');
+    if (isChild || isPopover) {
+      if (isChild) {
+        if (!get(this, 'isOpened')) {
+          set(this, 'isOpened', true);
+        }
+      }
+      return;
+    }
+    set(this, 'isOpened', false);
+  },
+
+  // Close search results when the user clicks outside of the input or results window
+  _closeSearchEventListener(event) {
+    const searchResults = document.getElementsByClassName('navbar-search-results')[0];
+    const isClickInInput = this.element.contains(event.target);
+    const isClickInResults = searchResults && searchResults.contains(event.target);
+    if (!isClickInInput && !isClickInResults) set(this, 'isOpened', false);
+  },
+
   didInsertElement() {
     this._super(...arguments);
-    this.element.addEventListener('focusin', event => {
-      const target = get(event, 'target');
-      const id = `#${get(this, 'elementId')}`;
-      const isChild = matches(target, `${id} *, ${id}`);
-      const isPopover = matches(target, '.navbar-search-results *, .navbar-search-results');
-      if (isChild || isPopover) {
-        if (isChild) {
-          if (!get(this, 'isOpened')) {
-            set(this, 'isOpened', true);
-          }
-        }
-        return;
-      }
-      set(this, 'isOpened', false);
-    });
-
-    // Close search results when the user clicks outside of the input or results window
-    document.addEventListener('click', event => {
-      const searchResults = document.getElementsByClassName('navbar-search-results')[0];
-      const isClickInInput = this.element.contains(event.target);
-      const isClickInResults = searchResults && searchResults.contains(event.target);
-
-      if (!isClickInInput && !isClickInResults) set(this, 'isOpened', false);
-    });
+    this.element.addEventListener('focusin', this._searchEventListener.bind(this));
+    document.addEventListener('click', this._closeSearchEventListener.bind(this));
   },
 
   willDestroyElement() {
-    this.element.removeEventListener('focusin');
-    document.removeEventListener('click');
+    this.element.removeEventListener('focusin', this._searchEventListener);
+    document.removeEventListener('click', this._closeSearchEventListener);
   },
 
   actions: {
