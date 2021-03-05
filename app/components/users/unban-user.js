80eeab053b9bfb92b3583d8f397d9d33985819d2
Emma Lejeck <nuck@kitsu.io>
2021-03-05 12:41:57 -0800

    Add unban user to admin menu

---
+++ app/components/users/unban-user.js
@@ -0,0 +1,21 @@
+import Component from '@ember/component';
+import { get } from '@ember/object';
+import { inject as service } from '@ember/service';
+import { task } from 'ember-concurrency';
+
+export default Component.extend({
+  ajax: service(),
+  notify: service(),
+
+  unbanUser: task(function* () {
+    const userId = get(this, 'user.id');
+    yield get(this, 'ajax').request(`/users/${userId}/_ban`, { method: 'DELETE' });
+    get(this, 'notify').success('User was unbanned');
+  }).drop(),
+
+  actions: {
+    banUser() {
+      get(this, 'unbanUser').perform();
+    }
+  }
+});
