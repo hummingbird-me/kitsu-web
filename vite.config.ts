ef616f7923f9e498896c8ff30f1522c072407b82
Emma Lejeck <nuck@kitsu.io>
2021-07-04 18:44:40 -0700

    Teach vite about project-absolute imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import path from 'path';
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
-})
+  plugins: [reactRefresh()],
+  resolve: {
+    alias: {
+      app: path.resolve(__dirname, '/src'),
+    },
+  },
+});
