fcc50ea27afa1ab38b55690a86ab617af88e3fa0
Emma Lejeck <nuck@kitsu.io>
2021-07-16 13:46:07 -0700

    Set up Jest with React Testing Library

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,12 @@
 module.exports = {
   preset: 'ts-jest',
   collectCoverage: process.env.COVERAGE === 'true',
+  testEnvironment: 'jsdom',
   testRunner: 'jest-circus/runner',
   moduleNameMapper: {
     '^app/(.*)$': '<rootDir>/src/$1',
+    '\\.(jpe?g|png|gif|webp)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.css$': 'identity-obj-proxy',
   },
   cacheDirectory: '.jest/cache',
 };
