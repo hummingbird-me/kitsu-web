963180b1ab405c78ed30fdd5b9fc6ab8bbc4bce9
Emma Lejeck <nuck@kitsu.io>
2021-07-16 13:54:40 -0700

    Add our first modal to the routing tree

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -4,6 +4,7 @@ import { Switch, Route, useLocation } from 'react-router-dom';
 
 import 'app/styles/index.css';
 import Header from 'app/components/Header';
+import Modal from 'app/components/Modal';
 
 export default function App() {
   const location = useLocation<Location & { background: Location }>();
@@ -14,7 +15,15 @@ export default function App() {
       <Header background="opaque" />
       <Switch location={background || location}>
         <Route path="/users/:id">User Profile</Route>
+        <Route path="/auth/sign-in">
+          <Modal displayMode="page">Sign in</Modal>
+        </Route>
       </Switch>
+      {background && (
+        <Route path="/auth/sign-in">
+          <Modal displayMode="modal">Sign in</Modal>
+        </Route>
+      )}
     </>
   );
 }
