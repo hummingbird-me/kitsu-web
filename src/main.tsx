8e06be37d3a7b8737e3dba9719f540cb3f6c2983
Emma Lejeck <nuck@kitsu.io>
2021-07-16 13:50:40 -0700

    Remove default start page

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import './styles/index.css';
+import App from './pages/App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
-)
+);
