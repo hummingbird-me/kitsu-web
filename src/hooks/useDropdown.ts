0c06e664514e9b606661615cceb90d5ed9ebeac8
Emma Lejeck <nuck@kitsu.io>
2021-07-18 18:20:49 -0700

    Add popper system and wrap their hook

---
+++ src/hooks/useDropdown.ts
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { Options } from '@popperjs/core';
+import { usePopper } from 'react-popper';
+import { useClickAway } from 'react-use';
+
+// TODO: better keyboard navigation
+export default function useDropdown(config: Partial<Options>) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [toggleElement, setToggleElement] = useState<HTMLElement | null>(null);
+  const [arrowElement, setArrowElement] = useState<HTMLElement | null>(null);
+  const [menuElement, setMenuElement] = useState<HTMLElement | null>(null);
+  useClickAway({ current: menuElement }, () => isOpen && setIsOpen(false), [
+    'click',
+  ]);
+  const popper = usePopper(toggleElement, menuElement, {
+    ...config,
+    modifiers: [
+      { name: 'arrow', options: { element: arrowElement } },
+      ...(config.modifiers || []),
+    ],
+  });
+
+  return {
+    isOpen,
+    setIsOpen,
+    toggleProps: {
+      ref: setToggleElement,
+      onClick: () => setIsOpen(!isOpen),
+    },
+    arrowProps: { ref: setArrowElement, style: popper.styles.arrow },
+    menuProps: {
+      role: 'menu',
+      ref: setMenuElement,
+      style: {
+        ...popper.styles.popper,
+        visibility: isOpen ? 'visible' : 'hidden',
+      },
+      ...popper.attributes.popper,
+    },
+    itemProps: { role: 'menuitem' },
+  };
+}
