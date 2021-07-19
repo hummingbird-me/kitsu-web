95a3cfd0ded924c3c4a00612c1fda20ae956c813
Emma Lejeck <nuck@kitsu.io>
2021-07-18 23:11:10 -0700

    Set up unauthenticated header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import { uniqueId } from 'lodash-es';
 import { useWindowScroll } from 'react-use';
+import { Link, useLocation } from 'react-router-dom';
 
 import logo from 'app/assets/logo.svg';
 import { ReactComponent as SearchIcon } from 'app/assets/icons/search.svg';
 import utilStyles from 'app/styles/utils.module.css';
+import { useSession } from 'app/contexts/SessionContext';
 
 import AvatarMenu from './AvatarMenu';
 import styles from './styles.module.css';
@@ -13,17 +15,24 @@ export default function Header({
   background = 'opaque',
   scrollBackground = 'opaque',
 }: {
-  background: 'opaque' | 'transparent';
-  scrollBackground: 'opaque' | 'transparent';
+  background?: 'opaque' | 'transparent';
+  scrollBackground?: 'opaque' | 'transparent';
 }) {
   // We don't expect to have this multiple times per page but we should still be careful
   const [searchId] = useState(() => uniqueId('header-search-'));
+  const location = useLocation();
+  const { session } = useSession();
   const { y } = useWindowScroll();
   const displayBackground = y > 0 ? scrollBackground : background;
 
   return (
     <header className={[styles.header, styles[displayBackground]].join(' ')}>
-      <nav className={[utilStyles.container, styles.container].join(' ')}>
+      <nav
+        className={[
+          utilStyles.container,
+          session ? styles.loggedIn : null,
+          styles.container,
+        ].join(' ')}>
         <a href="/" className={styles.logo}>
           <img src={logo} />
         </a>
@@ -47,10 +56,35 @@ export default function Header({
           </label>
           <input type="search" placeholder="Search Kitsu" id={searchId} />
         </div>
-        <a className={[styles.circular, styles.notificationCount].join(' ')}>
-          3
-        </a>
-        <AvatarMenu className={styles.avatar} />
+        {session ? (
+          <>
+            <a
+              className={[styles.circular, styles.notificationCount].join(' ')}>
+              3
+            </a>
+            <AvatarMenu className={styles.avatar} />
+          </>
+        ) : (
+          <div className={styles.authCta}>
+            <Link
+              to={{
+                pathname: '/auth/sign-up',
+                search: `?returnTo=${location.pathname}`,
+                state: { background: location },
+              }}>
+              Sign Up
+            </Link>
+            {' or '}
+            <Link
+              to={{
+                pathname: '/auth/sign-in',
+                search: `?returnTo=${location.pathname}`,
+                state: { background: location },
+              }}>
+              Sign In
+            </Link>
+          </div>
+        )}
       </nav>
     </header>
   );
