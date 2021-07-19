35c1d9872d0b261114765b2a54a0ba3fb98da098
Emma Lejeck <nuck@kitsu.io>
2021-07-18 22:42:53 -0700

    Clean up AvatarMenu using Dropdown components

--- src/components/Header/AvatarMenu/index.tsx
+++ src/components/Header/AvatarMenu/index.tsx
@@ -1,60 +1,52 @@
-import React, { useState, useRef, useReducer } from 'react';
+import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
-import Spinner from 'app/components/Spinner';
-import useDropdown from 'app/hooks/useDropdown';
-import { useSession } from 'app/contexts/SessionContext';
+import Image from 'app/components/Image';
+import * as Dropdown from 'app/components/Dropdown';
 
 import { useLoadProfileMenuQuery } from './loadProfileMenu-gql';
 import styles from './styles.module.css';
 import headerStyles from '../styles.module.css';
 
 const AvatarMenu: React.FC<{ className?: string }> = ({ className }) => {
-  const session = useSession();
   const { data, loading } = useLoadProfileMenuQuery();
   const profile = data?.currentAccount?.profile;
   const location = useLocation();
-  const { toggleProps, arrowProps, menuProps, itemProps } = useDropdown({
-    placement: 'bottom',
-    modifiers: [{ name: 'offset', options: { offset: [0, 10] } }],
-  });
 
   return (
     <div className={styles.avatar}>
       {loading ? (
         <div className={headerStyles.circular} />
       ) : (
-        <>
-          <img
-            {...toggleProps}
-            src="https://media.kitsu.io/users/avatars/5554/small.jpeg?1597552193"
-            className={headerStyles.circular}
-          />
-          <div {...menuProps} className={styles.menu}>
-            <div {...arrowProps} className={styles.arrow} />
-            <Link
-              {...itemProps}
+        <Dropdown.Wrapper arrow={true} popperOptions={{ placement: 'bottom' }}>
+          <Dropdown.Toggle>
+            {profile?.avatarImage ? (
+              <Image
+                source={profile.avatarImage}
+                height={25}
+                width={25}
+                blurhashSize={6}
+                className={headerStyles.circular}
+              />
+            ) : null}
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.ItemLink to={`/users/${profile?.slug ?? profile?.id}`}>
+              View Profile
+            </Dropdown.ItemLink>
+            <Dropdown.ItemLink
               to={{
                 pathname: '/auth/log-in',
                 search: `?returnTo=${location.pathname}`,
                 state: { background: location },
               }}>
               Sign in
-            </Link>
-            <Link {...itemProps} to={`/users/${profile?.slug ?? profile?.id}`}>
-              View Profile
-            </Link>
-            <Link {...itemProps} to="/">
-              Settings
-            </Link>
-            <Link {...itemProps} to="/">
-              Admin
-            </Link>
-            <Link {...itemProps} to="/">
-              Logout
-            </Link>
-          </div>
-        </>
+            </Dropdown.ItemLink>
+            <Dropdown.ItemLink to="/">Settings</Dropdown.ItemLink>
+            <Dropdown.ItemLink to="/">Admin</Dropdown.ItemLink>
+            <Dropdown.ItemLink to="/">Logout</Dropdown.ItemLink>
+          </Dropdown.Menu>
+        </Dropdown.Wrapper>
       )}
     </div>
   );
