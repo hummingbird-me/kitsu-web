9fbf14bdc30b7993b5d559176eba35327b42de72
Emma Lejeck <nuck@kitsu.io>
2021-08-15 21:34:02 -0700

    Create BannerImage component

---
+++ src/components/BannerImage/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import Image from 'app/components/Image';
+
+import styles from './styles.module.css';
+
+const BannerImage: React.FC<{ background: any }> = function ({
+  background,
+  children,
+}) {
+  return (
+    <div className={styles.container}>
+      <Image
+        source={background}
+        height="100%"
+        width="100%"
+        className={styles.background}
+      />
+      <div className={styles.overlay}>{children}</div>
+    </div>
+  );
+};
+
+export default BannerImage;
