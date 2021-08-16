e4c652d4cb5d6d89a59fa204af10df21c386c466
Emma Lejeck <nuck@kitsu.io>
2021-08-15 21:33:19 -0700

    Add top-level Suspense component

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -8,13 +8,16 @@ import 'app/styles/index.css';
 import { SessionContextProvider } from 'app/contexts/SessionContext';
 import ApolloContextProvider from 'app/contexts/ApolloContext';
 import Router from 'app/Router';
+import Spinner from 'app/components/Spinner';
 
 ReactDOM.render(
   <React.StrictMode>
     <SessionContextProvider>
       <ApolloContextProvider>
         <BrowserRouter>
-          <Router />
+          <Suspense fallback={<Spinner />}>
+            <Router />
+          </Suspense>
         </BrowserRouter>
       </ApolloContextProvider>
     </SessionContextProvider>
