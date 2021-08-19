67cb2750f8cd4ef53ed1b743878bf935ea2cc8f3
Emma Lejeck <nuck@kitsu.io>
2021-08-18 22:59:27 -0700

    Use preferred-locale to replace useLocaleName

--- src/hooks/useLocaleName.ts
+++
@@ -1,7 +0,0 @@
-import { useTranslation } from 'react-i18next';
-
-export default function useLocaleName(): string {
-  const { i18n } = useTranslation();
-  const locale = i18n.languages[0];
-  return locale;
-}
