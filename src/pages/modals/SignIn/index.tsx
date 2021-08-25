85c651130901058cae2019edaf592b5d0d1127e4
Emma Lejeck <nuck@kitsu.io>
2021-08-24 21:32:21 -0700

    Set up localization of sign in modal

--- src/pages/modals/SignIn/index.tsx
+++ src/pages/modals/SignIn/index.tsx
@@ -5,6 +5,7 @@ import {
   FaTwitter as TwitterLogo,
 } from 'react-icons/fa';
 import { useLocation } from 'react-router-dom';
+import { useIntl, FormattedMessage } from 'react-intl';
 
 import ModalLink from 'app/components/ModalLink';
 import Modal from 'app/components/Modal';
@@ -20,6 +21,7 @@ export default function SignInModal({
 }: {
   displayMode: 'page' | 'modal';
 }): JSX.Element {
+  const { formatMessage } = useIntl();
   const { state } =
     useLocation<{ email?: string; password?: string } | undefined>();
   const [email, setEmail] = React.useState(state?.email ?? '');
@@ -32,40 +34,80 @@ export default function SignInModal({
         <TextInput
           type="email"
           autoComplete="email"
-          placeholder="Email"
+          placeholder={formatMessage({
+            defaultMessage: 'Email',
+            description: 'Placeholder for email input on Sign In',
+          })}
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
         <TextInput
           type="password"
           autoComplete="current-password"
-          placeholder="Password"
+          placeholder={formatMessage({
+            defaultMessage: 'Password',
+            description: 'Placeholder for current password input on Sign In',
+          })}
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <Button type="submit" kind={ButtonKind.PRIMARY}>
-          Log in
+        <Button
+          type="submit"
+          kind={ButtonKind.PRIMARY}
+          loading={passwordLoginState.loading}>
+          <FormattedMessage
+            defaultMessage="Log in"
+            description="Submit button for Sign In"
+          />
         </Button>
-        <Rule label="Or log in with" />
+        <Rule
+          label={formatMessage({
+            defaultMessage: 'Or log in with',
+            description: 'Separator text above social media Sign In buttons',
+          })}
+        />
         <div className={styles.socialLoginContainer}>
           <Button
+            type="button"
             kind={ButtonKind.OUTLINE}
             className={styles.socialLoginButton}>
-            <FacebookLogo title="Log in with Facebook" />
+            <FacebookLogo
+              title={formatMessage({
+                defaultMessage: 'Log in with Facebook',
+                description: 'Hover text for Facebook sign in button',
+              })}
+            />
           </Button>
           <Button
+            type="button"
             kind={ButtonKind.OUTLINE}
             className={styles.socialLoginButton}>
-            <TwitterLogo title="Log in with Twitter" />
+            <TwitterLogo
+              title={formatMessage({
+                defaultMessage: 'Log in with Twitter',
+                description: 'Hover text for Twitter sign in button',
+              })}
+            />
           </Button>
           <Button
+            type="button"
             kind={ButtonKind.OUTLINE}
             className={styles.socialLoginButton}>
-            <AppleLogo title="Log in with Apple" />
+            <AppleLogo
+              title={formatMessage({
+                defaultMessage: 'Log in with Apple',
+                description: 'Hover text for Apple sign in button',
+              })}
+            />
           </Button>
         </div>
         <div className={styles.subformLink}>
-          <ModalLink to="/auth/forgot-password">Forgot Password?</ModalLink>
+          <ModalLink to="/auth/forgot-password">
+            <FormattedMessage
+              defaultMessage="Forgot Password?"
+              description="Link to password reset page from sign in form"
+            />
+          </ModalLink>
         </div>
       </form>
     </Modal>
