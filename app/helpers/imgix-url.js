7ad05390f6beb5ee8f9023c03bd469e79ea4879c
Emma Lejeck <nuck@kitsu.io>
2021-09-20 22:16:11 -0700

    Fix naive imgix url utility

--- app/helpers/imgix-url.js
+++ app/helpers/imgix-url.js
@@ -1,12 +1,24 @@
 import { helper } from 'ember-helper';
 
 export function imgixUrl([src, params = {}]) {
-  const filledParams = { auto: 'format', fm: 'png', ch: 'Width,DPR', ...params };
-  const options = Object.keys(filledParams).map(key => (
-    [key, filledParams[key]].map(encodeURIComponent).join('='))).join('&');
-  const url = src.replace('https://media.kitsu.io/', 'https://kitsu.imgix.net/')
-    .replace('https://media-staging.kitsu.io/', 'https://kitsu-staging.imgix.net/');
-  return options ? `${url}&${options}` : url;
+  const url = new URL(
+    src
+      .replace('https://media.kitsu.io/', 'https://kitsu.imgix.net/')
+      .replace(
+        'https://media-staging.kitsu.io/',
+        'https://kitsu-staging.imgix.net/'
+      )
+  );
+  const existingParams = Object.fromEntries(new URLSearchParams(url.search));
+  const options = new URLSearchParams({
+    ...existingParams,
+    auto: 'format',
+    fm: 'png',
+    ch: 'Width,DPR',
+    ...params
+  });
+  url.search = options.toString();
+  return url.toString();
 }
 
 export default helper(imgixUrl);
