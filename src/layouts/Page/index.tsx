a0fd669522d1e16256bfb017f4829a8c2ee97c3b
Emma Lejeck <nuck@kitsu.io>
2021-10-04 09:53:29 -0700

    Move Page component to layouts

--- src/layouts/Page/index.tsx
+++ src/layouts/Page/index.tsx
@@ -1,6 +1,9 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useContext } from 'react';
 
 import { SpinnerBlock } from 'app/components/Spinner';
+import { LayoutSettingsContext } from 'app/contexts/LayoutSettingsContext';
+import Header from 'app/components/Header';
+import Toaster from 'app/components/Toaster/Toaster';
 
 import styles from './styles.module.css';
 
@@ -10,8 +13,12 @@ const Page: React.FC<{ loading: boolean }> = function ({
   loading = false,
   children,
 }) {
+  const { layoutSettings } = useContext(LayoutSettingsContext);
+
   return (
     <>
+      <Header {...layoutSettings.header} />
+      <Toaster />
       <Suspense fallback={<LoadingPage />}>
         {loading ? <LoadingPage /> : children}
       </Suspense>
