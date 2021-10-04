acaab722d444290439635df3c64725c2d41b22ab
Emma Lejeck <nuck@kitsu.io>
2021-10-04 15:12:17 -0700

    Apply formatjs in Babel config

--- babel.config.js
+++ babel.config.js
@@ -9,4 +9,13 @@ module.exports = {
     '@babel/preset-typescript',
     'babel-preset-vite',
   ],
+  plugins: [
+    [
+      'formatjs',
+      {
+        idInterpolationPattern: '[sha512:contenthash:base64:6]',
+        ast: true,
+      },
+    ],
+  ],
 };
