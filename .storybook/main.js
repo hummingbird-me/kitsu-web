a664e7243d02fbb3292f11e6c5dbd155c013546d
Emma Lejeck <nuck@kitsu.io>
2021-10-03 18:27:53 -0700

    Add locale and RTL addons to Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,7 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.tsx'],
   addons: [
+    'storybook-rtl-addon',
     'storybook-addon-themes',
     '@storybook/addon-links',
     {
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import { StaticRouter } from 'react-router';
+import { addParameters } from '@storybook/react';
+import { map } from 'lodash-es';
+import { useLocale } from 'storybook-addon-locale';
+import { withDirection } from 'storybook-rtl-addon';
 
-import UrqlContextProvider from '../src/contexts/UrqlContext';
-import IntlProvider from '../src/contexts/IntlContext';
 import KitsuTheme from './KitsuTheme';
+import UrqlContextProvider from 'app/contexts/UrqlContext';
+import IntlProvider from 'app/contexts/IntlContext';
+import translations from 'app/translations';
+import 'app/styles/index.css';
 
-import '../src/styles/index.css';
+const locales = (() => {
+  const displayNames = new Intl.DisplayNames(['en'], { type: 'language' });
+  return Object.fromEntries(
+    Object.keys(translations).map((key) => [
+      key,
+      {
+        name: displayNames.of(key),
+        text: displayNames.of(key),
+      },
+    ])
+  );
+})();
+
+addParameters({
+  locales,
+  defaultLocale: 'en-US',
+});
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
@@ -28,17 +50,21 @@ export const parameters = {
 };
 
 export const decorators = [
-  (Story) => (
-    <React.StrictMode>
-      <React.Suspense fallback={null}>
-        <StaticRouter location={{}}>
-          <IntlProvider>
-            <UrqlContextProvider>
-              <Story />
-            </UrqlContextProvider>
-          </IntlProvider>
-        </StaticRouter>
-      </React.Suspense>
-    </React.StrictMode>
-  ),
+  withDirection,
+  (Story) => {
+    const locale = useLocale();
+    return (
+      <React.StrictMode>
+        <React.Suspense fallback={null}>
+          <StaticRouter location={{}}>
+            <IntlProvider locale={locale}>
+              <UrqlContextProvider>
+                <Story />
+              </UrqlContextProvider>
+            </IntlProvider>
+          </StaticRouter>
+        </React.Suspense>
+      </React.StrictMode>
+    );
+  },
 ];
