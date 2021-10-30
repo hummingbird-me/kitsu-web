dee4a509ec01b09f89b60d7a846f37f0b18a33db
Emma Lejeck <nuck@kitsu.io>
2021-10-29 19:10:49 -0700

    Allow overriding className on Card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -5,8 +5,11 @@ import styles from './styles.module.css';
 /**
  * A "card" is a component for displaying content over the background of the page
  */
-const Card: React.FC<HTMLProps<HTMLDivElement>> = function (args) {
-  return <div className={styles.card} {...args} />;
+const Card: React.FC<HTMLProps<HTMLDivElement>> = function ({
+  className,
+  ...args
+}) {
+  return <div className={[styles.card, className].join(' ')} {...args} />;
 };
 
 export default Card;
