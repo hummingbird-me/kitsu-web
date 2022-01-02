d7a18439d186fe6528a082ede561e742f00ecb7f
Emma Lejeck <nuck@kitsu.io>
2022-01-02 01:01:08 -0800

    Improve config vars

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,1 +1,2 @@
-export const apiHost = import.meta.env.VITE_API_HOST;
+export const apiHost = (import.meta.env.VITE_API_HOST as string) ?? '';
+export const clientId = (import.meta.env.VITE_CLIENT_ID as string) ?? '';
