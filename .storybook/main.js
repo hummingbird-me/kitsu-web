bdb41f7dd34075665a0f40b52e577496e51fd811
Emma Lejeck <nuck@kitsu.io>
2022-04-28 20:52:36 -0700

    Move storybook webpack config into main.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   stories: [
     '../src/docs/**/*.mdx',
@@ -26,4 +28,38 @@ module.exports = {
     },
     'storybook-css-modules-preset',
   ],
+  async webpackFinal(config) {
+    config.resolve.alias = {
+      app: path.resolve(__dirname, '..', 'src'),
+      // HACK: Webpack is fucking something up and this fixes it idk
+      './app': path.resolve(__dirname, '..', 'src'),
+    };
+
+    const assetRule = config.module.rules.find(({ test }) => test.test('.svg'));
+
+    const assetLoader = {
+      loader: assetRule.loader,
+      options: assetRule.options || assetRule.query,
+    };
+
+    // Merge our rule with existing assetLoader rules
+    config.module.rules.unshift({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            svgoConfig: {
+              plugins: {
+                removeViewBox: false,
+              },
+            },
+          },
+        },
+        assetLoader,
+      ],
+    });
+
+    return config;
+  },
 };
