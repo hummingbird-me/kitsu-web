9719c4236045ce77a67b4a9256b7be1fa7bdbb25
Emma Lejeck <nuck@kitsu.io>
2022-06-18 00:07:57 -0700

    Switch favorites to use Algolia search

--- app/components/users/edit-profile/body/favorites/list.js
+++ app/components/users/edit-profile/body/favorites/list.js
@@ -2,17 +2,43 @@ import Component from '@ember/component';
 import { get, set, computed } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { capitalize } from '@ember/string';
+import { isEmpty } from '@ember/utils';
 import { task, timeout } from 'ember-concurrency';
 import { strictInvokeAction } from 'ember-invoke-action';
 import { concat } from 'client/utils/computed-macros';
 import Pagination from 'kitsu-shared/mixins/pagination';
 
+const ALGOLIA_INDICES = {
+  character: {
+    index: 'characters',
+    fields: ['id', 'slug', 'canonicalName', 'image']
+  },
+  anime: {
+    index: 'media',
+    fields: ['id', 'slug', 'canonicalTitle'],
+    filters: 'kind:anime'
+  },
+  manga: {
+    index: 'media',
+    fields: ['id', 'slug', 'canonicalTitle'],
+    filters: 'kind:manga'
+  }
+};
+
 export default Component.extend(Pagination, {
+  algolia: service(),
   store: service(),
-  favorites: concat('getFavoritesTask.lastSuccessful.value', 'paginatedRecords'),
-  filteredFavorites: computed('favorites.@each.{isDeleted,favRank}', function() {
-    return get(this, 'favorites').rejectBy('isDeleted').sortBy('favRank');
-  }).readOnly(),
+
+  favorites: concat(
+    'getFavoritesTask.lastSuccessful.value',
+    'paginatedRecords'
+  ),
+  filteredFavorites: computed(
+    'favorites.@each.{isDeleted,favRank}',
+    function () {
+      return get(this, 'favorites').rejectBy('isDeleted').sortBy('favRank');
+    }
+  ).readOnly(),
 
   init() {
     this._super(...arguments);
@@ -32,8 +58,8 @@ export default Component.extend(Pagination, {
       sort: 'fav_rank',
       page: { limit: 20 }
     };
-    return yield this.queryPaginated('favorite', options).then(records => {
-      records.forEach(record => {
+    return yield this.queryPaginated('favorite', options).then((records) => {
+      records.forEach((record) => {
         strictInvokeAction(this, 'addRecord', record);
       });
       return records;
@@ -42,44 +68,58 @@ export default Component.extend(Pagination, {
 
   searchTask: task(function* (query) {
     yield timeout(200);
-    const field = get(this, 'isCharacter') ? 'name' : 'text';
-    const type = get(this, 'type');
-    return yield get(this, 'store').query(type, {
-      filter: { [field]: query },
-      fields: this._getFieldsets(type),
-      page: { limit: 6 }
-    }).then(records => records.reject(record => (
-      get(this, 'favorites').map(favorite => get(favorite, 'item.id')).includes(get(record, 'id'))
-    )));
+    const indexInfo = ALGOLIA_INDICES[get(this, 'type')];
+    const index = yield this.get('algolia.getIndex').perform(indexInfo.index);
+    if (isEmpty(index) || isEmpty(query)) {
+      return [];
+    }
+    const response = yield index.search(query, {
+      filters: indexInfo.filters,
+      attributesToRetrieve: indexInfo.fields,
+      attributesToHighlight: [],
+      queryLanguages: ['en', 'ja'],
+      naturalLanguages: ['en', 'ja'],
+      hitsPerPage: 20,
+      responseFields: ['hits'],
+      removeStopWords: false,
+      removeWordsIfNoResults: 'allOptional'
+    });
+    return response.hits;
   }).restartable(),
 
   onPagination(records) {
     this._super(records);
-    records.forEach(record => {
+    records.forEach((record) => {
       strictInvokeAction(this, 'addRecord', record);
     });
   },
 
   actions: {
     reorderItems(orderedItems) {
-      orderedItems.forEach(item => set(item, 'favRank', orderedItems.indexOf(item) + 1));
+      orderedItems.forEach((item) =>
+        set(item, 'favRank', orderedItems.indexOf(item) + 1)
+      );
     },
 
     addFavorite(item) {
-      const record = get(this, 'store').createRecord('favorite', {
-        user: get(this, 'session.account'),
-        item
+      this.store.findRecord(this.get('type'), item.id).then((itemRecord) => {
+        const record = get(this, 'store').createRecord('favorite', {
+          user: get(this, 'session.account'),
+          item: itemRecord
+        });
+        get(this, 'paginatedRecords').addObject(record);
+        strictInvokeAction(this, 'addRecord', record);
+        // @TODO: Should be moved to adapter level
+        get(this, 'session.account').incrementProperty('favoritesCount');
       });
-      get(this, 'paginatedRecords').addObject(record);
-      strictInvokeAction(this, 'addRecord', record);
-      // @TODO: Should be moved to adapter level
-      get(this, 'session.account').incrementProperty('favoritesCount');
     },
 
     removeFavorite(favorite) {
       favorite.deleteRecord();
       const favorites = get(this, 'filteredFavorites');
-      favorites.forEach(item => set(item, 'favRank', favorites.indexOf(item) + 1));
+      favorites.forEach((item) =>
+        set(item, 'favRank', favorites.indexOf(item) + 1)
+      );
     }
   },
 
@@ -87,6 +127,8 @@ export default Component.extend(Pagination, {
     if (type === 'character') {
       return { characters: ['name', 'image'].join(',') };
     }
-    return { [type]: ['slug', 'posterImage', 'canonicalTitle', 'titles'].join(',') };
+    return {
+      [type]: ['slug', 'posterImage', 'canonicalTitle', 'titles'].join(',')
+    };
   }
 });
