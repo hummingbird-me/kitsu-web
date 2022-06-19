06eb3d0d76a30e4974f9821e8ea78f11a1a74fdc
Emma Lejeck <nuck@kitsu.io>
2022-06-18 18:37:28 -0700

    Allow loginWithPassword with null u/p

--- src/utils/login/withPassword.ts
+++ src/utils/login/withPassword.ts
@@ -1,10 +1,13 @@
 import { Session } from 'app/types/session';
 import login from './login';
+import LoginFailed from 'app/errors/LoginFailed';
 
 export default async function loginWithPassword(
-  { username, password }: { username: string; password: string },
+  { username, password }: { username?: string; password?: string },
   init: RequestInit = {}
 ): Promise<NonNullable<Session>> {
+  if (!username || !password) throw LoginFailed;
+
   return login({
     params: {
       grant_type: 'password',
