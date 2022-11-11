8a2322a38319addfe61376b28993ccd25de475fa
Emma Lejeck <nuck@kitsu.io>
2022-11-10 22:02:49 -0800

    Set up global color palette
    Using postcss-custom-properties to compile them so we can use color-mod

--- postcss.config.js
+++ postcss.config.js
@@ -7,6 +7,9 @@ module.exports = {
     require('autoprefixer'),
     require('postcss-easings'),
     require('postcss-preset-env'),
+    require('postcss-custom-properties')({
+      importFrom: './src/styles/globals/colors.css',
+    }),
     require('./postcss/resolveLocalCustomProperties'),
     require('postcss-color-mod-function'),
   ],
