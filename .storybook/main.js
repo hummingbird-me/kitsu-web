4627b1436e5d77745aad99a60f276b338071a2d5
Emma Lejeck <nuck@kitsu.io>
2022-11-18 18:07:28 -0800

    Switch to Storybook 7 alpha

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,4 @@
 const path = require('path');
-
 module.exports = {
   stories: [
     '../src/docs/**/*.mdx',
@@ -11,55 +10,15 @@ module.exports = {
     '@storybook/addon-links',
     {
       name: '@storybook/addon-essentials',
-      options: { backgrounds: false },
+      options: {
+        backgrounds: false,
+      },
     },
     'storybook-addon-designs',
     '@storybook/addon-a11y',
-    {
-      name: '@storybook/addon-postcss',
-      options: {
-        cssLoaderOptions: {
-          importLoaders: 1,
-        },
-        postcssLoaderOptions: {
-          implementation: require('postcss'),
-        },
-      },
-    },
-    'storybook-css-modules-preset',
   ],
-  async webpackFinal(config) {
-    config.resolve.alias = {
-      app: path.resolve(__dirname, '..', 'src'),
-      // HACK: Webpack is fucking something up and this fixes it idk
-      './app': path.resolve(__dirname, '..', 'src'),
-    };
-
-    const assetRule = config.module.rules.find(({ test }) => test.test('.svg'));
-
-    const assetLoader = {
-      loader: assetRule.loader,
-      options: assetRule.options || assetRule.query,
-    };
-
-    // Merge our rule with existing assetLoader rules
-    config.module.rules.unshift({
-      test: /\.svg$/,
-      use: [
-        {
-          loader: '@svgr/webpack',
-          options: {
-            svgoConfig: {
-              plugins: {
-                removeViewBox: false,
-              },
-            },
-          },
-        },
-        assetLoader,
-      ],
-    });
-
-    return config;
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
   },
 };
