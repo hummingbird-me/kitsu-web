2f37af5faaef945aa39452176d9081a8997aec6d
Emma Lejeck <nuck@kitsu.io>
2025-01-24 21:13:44 -0800

    Fix CategoryList graphql stuff

--- src/components/content/CategoryList/index.tsx
+++ src/components/content/CategoryList/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import CategoryTag, {
   CategoryTagFragment,
 } from '@/components/content/CategoryTag';
-import { graphql } from '@/graphql';
+import { graphql, readFragment, type FragmentOf } from '@/graphql';
 
 import styles from './styles.module.css';
 
@@ -12,6 +12,7 @@ export const CategoryListFragment = graphql(
     fragment CategoryListFragment on Media {
       categories(first: 50, sort: [{ on: ANCESTRY, direction: ASCENDING }]) {
         nodes {
+          slug
           ...CategoryTagFragment
         }
       }
@@ -20,21 +21,18 @@ export const CategoryListFragment = graphql(
   [CategoryTagFragment],
 );
 
-const CategoryList: React.FC<{
-  categories: (typeof CategoryListFragment)['categories'];
-}> = function ({ categories }) {
+export type CategoryListProps = {
+  media: FragmentOf<typeof CategoryListFragment>;
+};
+
+export default function CategoryList(props: CategoryListProps) {
+  const { categories } = readFragment(CategoryListFragment, props.media);
   return (
     <div className={styles.tagList}>
       {categories?.nodes?.map(
         (category) =>
-          category && (
-            <CategoryTag key={category.slug} category={category}>
-              {category.title['en']}
-            </CategoryTag>
-          ),
+          category && <CategoryTag key={category.slug} category={category} />,
       )}
     </div>
   );
-};
-
-export default CategoryList;
+}
